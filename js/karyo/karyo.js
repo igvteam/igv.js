f0f6a41aec8d3489adf153f871230564bb107555
Chantal <chantal.roth@lifetech.com>
2014-09-05 09:19:26 +0200

    clickable karyogram

--- js/karyo/karyo.js
+++ js/karyo/karyo.js
@@ -15,7 +15,8 @@ var igv = (function (igv) {
         this.div.style.width = "100%";
 
         this.ideograms = null;
-
+        igv.guichromosomes = [];
+        
         var contentHeight = this.div.clientHeight;
         var contentWidth = this.div.clientWidth - browser.controlPanelWidth;
         var contentDiv = document.createElement("div");
@@ -33,32 +34,11 @@ var igv = (function (igv) {
         canvas.setAttribute('width', contentWidth);    //Must set the width & height of the canvas
         canvas.setAttribute('height', contentHeight);
 
-        var chromosomeNameDiv = document.createElement('div');
-        chromosomeNameDiv.style.position = 'absolute';
-        chromosomeNameDiv.style.height = "100%";
-        chromosomeNameDiv.style.left = "0px";
-        chromosomeNameDiv.style.width = browser.controlPanelWidth + "px";
-        this.div.appendChild(chromosomeNameDiv);
-
-        var chromosomeNameCanvas = document.createElement('canvas');
-        chromosomeNameCanvas.style.position = 'absolute';
-        chromosomeNameCanvas.style.width = "100%";
-        chromosomeNameCanvas.style.height = contentHeight;
-        //chromosomeNameCanvas.style.height = 40;
-        chromosomeNameCanvas.setAttribute('width', browser.controlPanelWidth);
-        chromosomeNameCanvas.setAttribute('height', contentHeight);
-
-
+  
         this.canvas = canvas;
         contentDiv.appendChild(canvas);
 
         this.ctx = canvas.getContext("2d");
-
-        this.chromosomeNameCanvas = chromosomeNameCanvas;
-        chromosomeNameDiv.appendChild(chromosomeNameCanvas);
-
-        this.chromosomeNameCtx = chromosomeNameCanvas.getContext("2d");
-        this.chromosomeNameCtx.font = "bold 10px Arial";
 
         this.canvas.onclick = function (e) {
             var isFirefox = typeof InstallTrigger !== 'undefined';
@@ -80,21 +60,25 @@ var igv = (function (igv) {
             }
 
             this.getContext("2d").fillRect(mouseX, 0, 10, 10);
-
-            igv.navigateKaryo(browser, mouseX);
+            igv.navigateKaryo(browser, mouseX, mouseY);
         }
 
     }
 
     // Move location of the reference panel by clicking on the genome ideogram
-    igv.navigateKaryo = function (browser, pixelPosition) {
-
-        var referenceFrame = browser.referenceFrame,
-            chromosome = browser.genome.getChromosome(browser.referenceFrame.chr),
-            viewportWidth = browser.trackViewportWidth(),
-            bp = chromosome.length * pixelPosition / viewportWidth;
-
-        referenceFrame.start = bp;
+    igv.navigateKaryo = function (browser, mouseX, mouseY) {
+    	// check each chromosome if the coordinates are within its bound
+    	for (var i = 0; i < igv.guichromosomes.length; i++) {    		
+    		var g = igv.guichromosomes[i];
+    		if (g.x < mouseX && g.right > mouseX && g.y < mouseY && g.bottom > mouseY) {
+    			var dy = mouseY - g.y;
+    			var bp = Math.round(g.size * dy / g.h);
+    			log("Going to position "+bp);
+    			browser.search(g.name+":"+bp+"-"+(bp+10000));
+    			break;
+    		}
+    	}
+       
         browser.update();
     };
 
@@ -102,18 +86,12 @@ var igv = (function (igv) {
 
         var contentHeight = this.div.clientHeight,
             contentWidth = this.div.clientWidth,
-            canvas = this.canvas,
-            chromosomeNameCanvas = this.chromosomeNameCanvas;
+            canvas = this.canvas;
         canvas.style.width = "100%";
         canvas.style.height = contentHeight + "px";
         canvas.setAttribute('width', contentWidth);    //Must set the width & height of the canvas
         canvas.setAttribute('height', contentHeight);
-        log("redraw: height is :"+contentHeight);
-        chromosomeNameCanvas.style.width = "100%";
-        chromosomeNameCanvas.style.height = contentHeight + "px";
-        chromosomeNameCanvas.setAttribute('width',50);
-        chromosomeNameCanvas.setAttribute('height', contentHeight)
-
+        log("redraw: height is :"+contentHeight);      
         
         this.repaint();
     }
@@ -138,9 +116,10 @@ var igv = (function (igv) {
         	return;
         }
         var chrwidth = Math.min(30, w / 24);
-        var chrheight = h-20;
+        var chrheight = h-40;
         var longestChr = genome.getChromosome('chr1');
         var cytobands = longestChr.cytobands;
+        var top = 30;
         var maxLen = cytobands[cytobands.length-1].end;
         if (!image || image == null) {
             image = document.createElement('canvas');
@@ -155,7 +134,10 @@ var igv = (function (igv) {
             	nr++;
             	var chromosome = genome.getChromosome(chr);
             	chromosome.x = nr*chrwidth;
-            	drawIdeogram(chromosome.x, 0, chromosome, bufferCtx, chrwidth/2,chrheight, maxLen);
+            	var guichrom = new Object();
+            	guichrom.name = chr;
+            	igv.guichromosomes.push(guichrom); 
+            	drawIdeogram(guichrom, chromosome.x, top, chromosome, bufferCtx, chrwidth/2,chrheight, maxLen);
             	
             }
             this.ideograms= image;           
@@ -171,22 +153,14 @@ var igv = (function (igv) {
         var size = cytobands[cytobands.length-1].end;
         var ideoScale = h / maxLen;
 
-        var boxPY1 = Math.round(this.browser.referenceFrame.start * ideoScale);
+        var boxPY1 = top+Math.round(this.browser.referenceFrame.start * ideoScale);
         //var boxPY2 = Math.round((this.browser.referenceFrame.start+100) * ideoScale);
         this.ctx.strokeStyle = "red";
         this.ctx.lineWidth = 2;
         this.ctx.strokeRect(chromosome.x-2, boxPY1, chrwidth/2+4, 3 );
         this.ctx.restore();
-
-        var chromosomeNameWidth = this.chromosomeNameCanvas.width;
-        var chromosomeNameHeight = this.chromosomeNameCanvas.height;
-
-        this.chromosomeNameCtx.clearRect(0, 0, 100, 100);
-        this.chromosomeNameCtx.fillStyle = "rgb(0, 0, 0)";
-        this.chromosomeNameCtx.fillText(referenceFrame.chr, chromosomeNameWidth / 2, chromosomeNameHeight / 2);
-
-
-        function drawIdeogram(ideogramLeft, ideogramTop, chromosome, bufferCtx, ideogramWidth, ideogramHeight, longestChr) {
+       
+        function drawIdeogram(guichrom, ideogramLeft, top, chromosome, bufferCtx, ideogramWidth, ideogramHeight, longestChr) {
            
             if (!genome) return;            
             if (!chromosome) return;
@@ -194,20 +168,29 @@ var igv = (function (igv) {
             var cytobands = chromosome.cytobands;
 
             var centerx = (ideogramLeft + ideogramWidth / 2);
-
+           
             var xC = [];
             var yC = [];
 
             var len = cytobands.length;
             if (len == 0) return;
-           
             var scale = ideogramHeight / longestChr;
+            
+            guichrom.x = ideogramLeft;
+            guichrom.y = top;
+            guichrom.w = ideogramWidth;
+            guichrom.right = ideogramLeft + ideogramWidth;
+            var last = cytobands[len-1].end;
+            guichrom.h = scale * last;
+            guichrom.size = last;
+            guichrom.longest = longestChr;
+            guichrom.bottom = top +guichrom.h;
             var lastPY = -1;
             for (var i = 0; i < cytobands.length; i++) {
                 var cytoband = cytobands[i];
 
-                var starty = scale * cytoband.start + ideogramTop;
-                var endy = scale * cytoband.end + ideogramTop;
+                var starty = scale * cytoband.start + top;
+                var endy = scale * cytoband.end + top;
                 if (endy > lastPY) {
                     if (cytoband.type == 'c') { // centermere: "acen"
                         if (cytoband.label.charAt(0) == 'p') {
@@ -231,23 +214,26 @@ var igv = (function (igv) {
                        // g2D.fillPolygon(xC, yC, 3);
                     } else {
                     	var dy = endy - starty;
+                    	
                         bufferCtx.fillStyle = getCytobandColor(cytoband); //g2D.setColor(getCytobandColor(cytoband));
                         bufferCtx.fillRect(ideogramLeft, starty,  ideogramWidth, dy);                    
                     }
                 }
-                
-               
-           //      bufferCtx.strokeStyle = "black";
-                // bufferCtx.strokeRect(ideogramLeft, ideogramTop, ideogramWidth, ideogramHeight, 0, ideogramWidth / 2, 1);
-           //      bufferCtx.strokeRect(ideogramLeft, ideogramTop, ideogramWidth, ideogramHeight);
-                //context.strokeRect(margin, y, trackWidth-2*margin, height);                
+                      
                 lastPY = endy;
 
             }
             bufferCtx.fillStyle = null;
             bufferCtx.lineWidth = 1;
             bufferCtx.strokeStyle = "darkgray";
-            bufferCtx.roundRect(ideogramLeft, ideogramTop, ideogramWidth, lastPY, ideogramWidth / 2, 0, 1);
+            var r = ideogramWidth / 2;
+            bufferCtx.roundRect(ideogramLeft, top-r/2, ideogramWidth, lastPY-top+r, ideogramWidth / 2, 0, 1);
+            
+         // draw chromosome name
+            
+            bufferCtx.font = "bold 10px Arial";
+            bufferCtx.fillStyle = "rgb(0, 0, 0)";
+            bufferCtx.fillText(chromosome.name, ideogramLeft +ideogramWidth / 2-10, top-20);
         }
 
         function getCytobandColor(data) {
