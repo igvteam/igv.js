9c71cbf1d0a8da9bdf5d9de7f87b540910ea4214
Douglass Turner <douglass.turner@gmail.com>
2014-09-05 11:28:18 -0400

    trackfilter. radio button ui build out.

--- bootply/track_filter_radio_button/js/track_filter_radio_button.js
+++ bootply/track_filter_radio_button/js/track_filter_radio_button.js
@@ -1,44 +1,24 @@
 
 $(document).ready(function(){
 
-//    var trackFilterTabSet = $('#trackFilterTabSet_GUID'),
-//        enableDisableButtonGroupOnOffFilter = $('#enableDisableButtonGroupOnOffFilter_GUID');
-//
-//    trackFilterTabSet.find('a').click(function (e) {
-//
-//        var that = $(this);
-//
-//        e.preventDefault();
-//
-//        that.tab('show');
-//
-//    });
-//
-//    trackFilterTabSet.find('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-//
-//        var active = e.target,
-//            dormant = e.relatedTarget;
-//
-//        console.log("active " + active.id + " inactive " + dormant.id);
-//
-//    });
-//
-//    enableDisableButtonGroupOnOffFilter.click(function() {
-//
-//        var thang = $(this),
-//            toggleSwitchButtonPair = thang.find('.btn');
-//
-//        toggleSwitchButtonPair.toggleClass('active');
-//
-//        if (thang.find('.btn-primary').size() > 0) {
-//            toggleSwitchButtonPair.toggleClass('btn-primary');
-//        }
-//
-//        toggleSwitchButtonPair.toggleClass('btn-default');
-//
-//        console.log("toggle");
-//
-//    });
+    var enableDisableButtonGroupOnOffFilter = $('#enableDisableButtonGroupOnOffFilter_GUID');
+
+    enableDisableButtonGroupOnOffFilter.click(function() {
+
+        var thang = $(this),
+            toggleSwitchButtonPair = thang.find('.btn');
+
+        toggleSwitchButtonPair.toggleClass('active');
+
+        if (thang.find('.btn-primary').size() > 0) {
+            toggleSwitchButtonPair.toggleClass('btn-primary');
+        }
+
+        toggleSwitchButtonPair.toggleClass('btn-default');
+
+        console.log("toggle");
+
+    });
 
 
 });
