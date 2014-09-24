a6cf2ea52e30e6a2193f781b483e68a8c6a399e2
Douglass Turner <douglass.turner@gmail.com>
2014-09-23 22:12:29 -0400

    Scrollbar. Implementing mouse handlers.

--- js/cursor/horizontalScrollbar.js
+++ js/cursor/horizontalScrollbar.js
@@ -14,13 +14,54 @@ var cursor = (function (cursor) {
 
     cursor.HorizontalScrollbar.prototype.markupWithParentDivObject = function (parentDivObject) {
 
-        var horizontalScrollBarContainer = $('<div class="igv-horizontal-scrollbar-container-div">')[0],
+        var myself = this,
+            horizontalScrollBarContainer = $('<div class="igv-horizontal-scrollbar-container-div">')[0],
             horizontalScrollBar          = $('<div class="igv-horizontal-scrollbar-div">')[0],
             horizontalScrollBarDraggable = $('<div class="igv-horizontal-scrollbar-draggable-div">')[0];
+
+        myself.isMouseDown = undefined;
+        myself.lastMouseX = undefined;
 
         parentDivObject.append(horizontalScrollBarContainer);
         $(horizontalScrollBarContainer).append(horizontalScrollBar);
         $(horizontalScrollBar).append(horizontalScrollBarDraggable);
+
+        $( horizontalScrollBarDraggable ).mousedown(function(e) {
+
+            myself.isMouseDown = true;
+            myself.lastMouseX = e.offsetX;
+
+        });
+
+        $( horizontalScrollBarDraggable ).mousemove(function (e) {
+
+            var dx,
+                left;
+
+            dx = e.offsetX - myself.lastMouseX;
+            left = $( horizontalScrollBarDraggable).position().left;
+
+            if (myself.isMouseDown && myself.lastMouseX) {
+
+                left += dx;
+                $( horizontalScrollBarDraggable).css({
+                    "left": left + "px"
+                });
+
+            }
+
+        });
+
+        $( horizontalScrollBarDraggable ).mouseup(function(e) {
+            myself.isMouseDown = false;
+            myself.lastMouseX = undefined;
+        });
+
+
+        $( horizontalScrollBarDraggable ).mouseout(function(e) {
+            myself.isMouseDown = false;
+            myself.lastMouseX = undefined;
+        });
 
     };
 
