429d24fcfb1c26adc3bd48e1612102d4a9d7b35f
Douglass Turner <douglass.turner@gmail.com>
2014-09-26 17:51:19 -0400

    Now using Font Awesome. Because ... it's awesome.

--- js/popover.js
+++ js/popover.js
@@ -4,7 +4,9 @@
 var igv = (function (igv) {
 
     var popoverDiv,
-        popoverCloseDiv,
+        popoverCloseElement,
+        popoverCloseIcon,
+        popoverPointerDiv,
         popoverContentDiv;
 
     igv.Popover = function (parent) {
@@ -14,30 +16,75 @@ var igv = (function (igv) {
             markupWithParentDiv(parent);
         }
 
-      function markupWithParentDiv (parentDiv) {
+        function markupWithParentDiv (parentDiv) {
 
             // popover
             popoverDiv = document.createElement('div');
             parentDiv.appendChild(popoverDiv);
-
             popoverDiv.id = "popover_" + igv.guid();
             popoverDiv.className = "igv-popover";
 
             // popover content
             popoverContentDiv = document.createElement("div");
             popoverDiv.appendChild(popoverContentDiv);
-
             popoverContentDiv.className = "igv-popoverContent";
-            popoverContentDiv.innerHTML = "blah blah";
+
+
+
+
+
+
+            // popover pointer
+//            <i class="fa fa-location-arrow"></i>
+//            <i class="fa fa-play"></i>
+//            popoverPointerDiv = document.createElement("i");
+//            popoverDiv.appendChild(popoverPointerDiv);
+//            popoverPointerDiv.className = "igv-popoverPointer fa fa-chevron-right fa-2x fa-rotate-90";
+
+
+
+
+
+
+//            // popover close
+//            popoverCloseElement = document.createElement("div");
+//            popoverDiv.appendChild(popoverCloseElement);
+//            popoverCloseElement.className = "igv-popoverClose";
+//            popoverCloseElement.innerHTML = "x";
 
             // popover close
-            popoverCloseDiv = document.createElement("div");
-            popoverDiv.appendChild(popoverCloseDiv);
+            popoverCloseElement = document.createElement("span");
+            popoverDiv.appendChild(popoverCloseElement);
+            popoverCloseElement.className = "igv-popoverCloseElement";
 
-            popoverCloseDiv.className = "igv-popoverClose";
-            popoverCloseDiv.innerHTML = "x";
+            popoverCloseIcon = document.createElement("i");
+            popoverCloseElement.appendChild(popoverCloseIcon);
+            popoverCloseIcon.className = "igv-popoverCloseFontAwesome fa fa-times";
+//            popoverCloseIcon.className = "igv-popoverCloseFontAwesome fa fa-times-circle fa-lg";
 
-            popoverCloseDiv.onclick = function (e) {
+            $(popoverCloseElement).hover(
+
+                function() {
+                    $(popoverCloseIcon).removeClass("fa-times"       );
+                    $(popoverCloseIcon).addClass   ("fa-times-circle fa-lg");
+
+                    $(popoverCloseIcon).css({
+                        "color" : "#f00"
+                    });
+                },
+
+                function() {
+                    $(popoverCloseIcon).removeClass("fa-times-circle fa-lg");
+                    $(popoverCloseIcon).addClass   ("fa-times"       );
+
+                    $(popoverCloseIcon).css({
+                        "color" : "#444"
+                    });
+
+                }
+            );
+
+            popoverCloseElement.onclick = function (e) {
 
                 $(popoverDiv).hide();
 
