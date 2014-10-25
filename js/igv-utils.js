72dbe6cc7923983504fdbde3db10d030f88d0f4b
jrobinso <jrobinso@broadinstitute.org>
2014-10-24 20:53:45 -0400

    Move "cursor-dk" files. Remove spin.js

--- js/igv-utils.js
+++ js/igv-utils.js
@@ -63,32 +63,6 @@ var igv = (function (igv) {
         return formatedNumber.split(",").join().replace(",", "", "g");
     };
 
-    igv.getSpinner = function (target) {
-
-        var opts = {
-            lines: 13, // The number of lines to draw
-            length: 6, // The length of each line
-            width: 2, // The line thickness
-            radius: 8, // The radius of the inner circle
-            corners: 1, // Corner roundness (0..1)
-            rotate: 0, // The rotation offset
-            direction: 1, // 1: clockwise, -1: counterclockwise
-            color: '#000', // #rgb or #rrggbb or array of colors
-            speed: 1, // Rounds per second
-            trail: 60, // Afterglow percentage
-            shadow: false, // Whether to render a shadow
-            hwaccel: false, // Whether to use hardware acceleration
-            className: 'spinner', // The CSS class to assign to the spinner
-            zIndex: 2e9, // The z-index (defaults to 2000000000)
-            top: 'auto',
-            left: 'auto'
-        };
-
-        var spinner = new Spinner(opts).spin(target);
-
-
-        return spinner;
-    };
 
     /**
      * Translate the mouse coordinates for the event to the coordinates for the given target element
