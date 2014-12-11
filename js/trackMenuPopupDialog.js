3046f61544f345cfdbb2c76c43b2551d1b44b399
Douglass Turner <douglass.turner@gmail.com>
2014-12-11 08:24:23 -0500

    Refactoring jquery dialog example to become generic module.

---
+++ js/trackMenuPopupDialog.js
@@ -0,0 +1,86 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Broad Institute
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * Created by turner on 12/11/14.
+ */
+var igv = (function (igv) {
+
+    igv.TrackMenuPopupDialog = function (label, value, ok) {
+
+        var htmlString,
+            myself,
+            thang;
+
+        thang = $( "#dialog-form" );
+
+        if (thang && thang.length !== 0) {
+            thang.remove();
+        }
+
+        htmlString = '<div id="dialog-form" title="Create new user"><form><fieldset><label for="name">Name</label><input type="text" name="name" id="name" value="Jane Smith"></fieldset></form></div>';
+        $( "body" ).append( $.parseHTML( htmlString ) );
+
+        this.dialogForm = $( "#dialog-form" );
+        this.form = this.dialogForm.find( "form" );
+        this.name = $( "#name" );
+
+        myself = this;
+        this.dialogForm.dialog({
+            autoOpen: false,
+            height: 256,
+            width: 300,
+            modal: true,
+            buttons: {
+                "ok": ok,
+                cancel: function() {
+                    console.log("cancel");
+                    myself.dialogForm.dialog( "close" );
+                }
+            },
+            close: function() {
+                console.log("close");
+                myself.form[ 0 ].reset();
+            }
+        });
+
+        this.form.on("submit", function( event ) {
+
+            event.preventDefault();
+
+            $( "#users" ).find(" tbody").append( "<tr>" + "<td>" + myself.name.val() + "</td>" + "</tr>" );
+            myself.dialogForm.dialog( "close" );
+        });
+
+    };
+
+
+    igv.TrackMenuPopupDialog.prototype.doSomethingAmazing = function() {
+
+    };
+
+    return igv;
+
+})(igv || {});
