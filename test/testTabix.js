9bf8f20a2a555abf3faccacdc311b499a7c22133
Douglass Turner <douglass.turner@gmail.com>
2015-01-06 14:27:15 -0500

    Unit tests clean up

--- test/testTabix.js
+++ test/testTabix.js
@@ -6,7 +6,7 @@ function tabixTests() {
         var refID = 14,
             beg = 24375199,
             end = 24378544,
-            indexPath = "../test/data/tabix/TSVC.vcf.gz.tbi",
+            indexPath = "data/tabix/TSVC.vcf.gz.tbi",
             config = {};
 
         igv.loadBamIndex(indexPath, config, function (bamIndex) {
