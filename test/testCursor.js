c17b6a7441556a9d584894a7979a3b02bce22126
Douglass Turner <douglass.turner@gmail.com>
2015-01-10 11:39:50 -0500

    Unit test repair. testCursor - too much coupling in CursorModel to test. Redesign the test?

--- test/testCursor.js
+++ test/testCursor.js
@@ -99,36 +99,47 @@ function runCursorTests() {
 
     });
 
-//    asyncTest("Get allscores ", function () {
-//
-//        var tssUrl, peakURL, tssDataSource, peakDataSource, region, bpStart, bpEnd, len, cursorModel;
-//
-//        tssUrl = "http://www.broadinstitute.org/igvdata/public/test/data/cursor/hg19.tss.bed.gz";
-//        peakURL = "http://www.broadinstitute.org/igvdata/public/test/data/cursor/wgEncodeBroadHistoneH1hescH3k4me3StdPk.broadPeak.gz";
-//
-//        peakDataSource = new igv.BedFeatureSource(peakURL);
-//        tssDataSource = new igv.BedFeatureSource(tssUrl);
-//        cursorModel = new cursor.CursorModel(null);
-//
-//        tssDataSource.allFeatures(function (featureList) {
-//
-//            ok(featureList);
-//
-//            cursorModel.setRegions(featureList);
-//            region = cursorModel.regions[1];
-//
-//
-//            peakDataSource.getFeatureCache(function (featureCache) {
-//
-//                var regionWidth = 1000000;
-//                var score = region.getScore(featureCache, regionWidth);
-//                console.log("Score=" + score);
-//
-//                start();
-//            });
-//        });
-//
-//    });
+    asyncTest("Get All Scores ", function () {
+
+        var url = "data/bed/basic_feature_3_columns.bed.gz",
+             featureSource;
+
+
+
+        featureSource = new igv.BedFeatureSource({ type: 'bed', url: url });
+        featureSource.allFeatures(function (features) {
+
+            var broadPeakFeatureSource,
+                broadPeakURL = "data/peak/test.broadPeak";
+
+            ok(features);
+
+            broadPeakFeatureSource = new igv.BedFeatureSource({ type: 'bed', url: broadPeakURL });
+            ok(broadPeakFeatureSource);
+
+            broadPeakFeatureSource.getFeatureCache(function (featureCache) {
+
+                var cursorModel,
+                    region,
+                    regionWidth = 1000000,
+                    score;
+
+                ok(featureCache);
+
+                cursorModel = new cursor.CursorModel(null);
+                ok(cursorModel);
+
+                cursorModel.setRegions(features);
+                region = cursorModel.regions[ 1 ];
+
+                score = region.getScore(featureCache, regionWidth);
+                console.log("score " + score);
+
+                start();
+            });
+        });
+
+    });
 
 //    asyncTest("Sort ", function () {
 //
