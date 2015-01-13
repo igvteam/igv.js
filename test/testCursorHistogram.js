a63d9b506bc58f6fefe41e2b85e071a04de0624e
Douglass Turner <douglass.turner@gmail.com>
2015-01-13 16:30:06 -0500

    Unit test repair
    Former-commit-id: 7242dac0f7e6289d7d3f8d46c3fd910639315065

--- test/testCursorHistogram.js
+++ test/testCursorHistogram.js
@@ -5,76 +5,85 @@
 
 function runCursorHistogramTests() {
 
-    test( "CursorHistogram Creation Test", 4, function() {
+    test( "CursorHistogram Creation Test", 11, function() {
 
-        var cursorHistogram = new cursor.CursorHistogram(null, 100);
+
+        var score,
+            cursorHistogram = new cursor.CursorHistogram(null, null);
         ok(cursorHistogram);
 
-        equal(0, cursorHistogram.scoreIndex(14));
-        cursorHistogram.bins[cursorHistogram.scoreIndex(14)]++;
+        // maximum track score is 100
+        cursorHistogram.track = { max : 100 };
 
-        equal(1, cursorHistogram.scoreIndex(35));
-        cursorHistogram.bins[cursorHistogram.scoreIndex(35)]++;
+        // default bin count is 100
+        equal(100, cursorHistogram.bins.length);
 
-        equal(2, cursorHistogram.scoreIndex(65));
-        cursorHistogram.bins[cursorHistogram.scoreIndex(65)]++;
+        // we have a "square histogram space"
+        score = 0;
 
-        cursorHistogram.bins.forEach(function(count, index, counts) {
+        [0, 25, 75].forEach(function(score, i, scores){
 
-            console.log("index " + index + " count " + count);
-        });
+            var index;
+            index = cursorHistogram.scoreIndex(score);
+            equal(score, index);
+            ok(index >= 0);
+            ok(index < cursorHistogram.bins.length);
+            cursorHistogram.bins[ index ]++;
 
-    });
-
-    asyncTest("CursorRegion - Lots of scores", function () {
-
-        var peakURL = "http://www.broadinstitute.org/igvdata/public/test/data/cursor/wgEncodeBroadHistoneH1hescH3k4me3StdPk.broadPeak.gz",
-            tssURL = "http://www.broadinstitute.org/igvdata/public/test/test/data/cursor/hg19.tss.bed.gz",
-            featureSource,
-            cursorModel = new cursor.CursorModel(null),
-            bins,
-            maxScore,
-            cursorHistogram;
-
-        featureSource = new igv.BedFeatureSource(peakURL);
-//        featureSource = new igv.BedFeatureSource(tssURL);
-
-        maxScore = 1000;
-        cursorHistogram = new cursor.CursorHistogram(null, maxScore);
-
-        featureSource.allFeatures(function (features) {
-
-            ok(features);
-
-            cursorModel.setRegions(features);
-
-            featureSource.getFeatureCache(function (featureCache) {
-
-                cursorModel.regions.forEach(function (region) {
-
-                    var score = region.getScore(featureCache, region.regionWidth);
-
-                    if (score < 0) {
-
-                        // do nothing
-                    } else {
-
-                        cursorHistogram.bins[ cursorHistogram.scoreIndex(score) ] += 1;
-                    }
-
-                });
-
-                cursorHistogram.bins.forEach(function(count, index, counts) {
-
-                    console.log("bin[" + index + "] = " + count);
-                });
-
-                start();
-            });
         });
 
 
     });
+
+//    asyncTest("CursorRegion - Lots of scores", function () {
+//
+//        var peakURL = "http://www.broadinstitute.org/igvdata/public/test/data/cursor/wgEncodeBroadHistoneH1hescH3k4me3StdPk.broadPeak.gz",
+//            tssURL = "http://www.broadinstitute.org/igvdata/public/test/test/data/cursor/hg19.tss.bed.gz",
+//            featureSource,
+//            cursorModel = new cursor.CursorModel(null),
+//            bins,
+//            maxScore,
+//            cursorHistogram;
+//
+//        featureSource = new igv.BedFeatureSource(peakURL);
+////        featureSource = new igv.BedFeatureSource(tssURL);
+//
+//        maxScore = 1000;
+//        cursorHistogram = new cursor.CursorHistogram(null, maxScore);
+//
+//        featureSource.allFeatures(function (features) {
+//
+//            ok(features);
+//
+//            cursorModel.setRegions(features);
+//
+//            featureSource.getFeatureCache(function (featureCache) {
+//
+//                cursorModel.regions.forEach(function (region) {
+//
+//                    var score = region.getScore(featureCache, region.regionWidth);
+//
+//                    if (score < 0) {
+//
+//                        // do nothing
+//                    } else {
+//
+//                        cursorHistogram.bins[ cursorHistogram.scoreIndex(score) ] += 1;
+//                    }
+//
+//                });
+//
+//                cursorHistogram.bins.forEach(function(count, index, counts) {
+//
+//                    console.log("bin[" + index + "] = " + count);
+//                });
+//
+//                start();
+//            });
+//        });
+//
+//
+//    });
 
 //    asyncTest("CursorRegion - getScore", function () {
 //
