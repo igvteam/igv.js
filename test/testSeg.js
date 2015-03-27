0be5a1b733ab5573ed1a2ed86087d39db5399703
Douglass Turner <douglass.turner@gmail.com>
2015-03-27 17:55:10 -0400

    Unit test. testSeg.js pass

--- test/testSeg.js
+++ test/testSeg.js
@@ -2,17 +2,17 @@ function runSegUnitTests() {
 
     asyncTest("SEG query", function () {
 
-        var url = "http://www.broadinstitute.org/igvdata/test/igv-web/segmented_data_080520.seg.gz";
-
-        var featureSource = new igv.FeatureSource( { type: 'seg', url: url, indexed: false } );
-
-        var chr = "1";
-        var bpStart = 0;
-        var bpEnd = 747751863;
+        var url = "http://www.broadinstitute.org/igvdata/test/igv-web/segmented_data_080520.seg.gz",
+            featureSource = new igv.FeatureSource( { type: 'seg', url: url, indexed: false }),
+            chr = "1",
+            bpStart = 0,
+            bpEnd = 747751863;
 
         featureSource.getFeatures(chr, bpStart, bpEnd, function (features) {
 
             ok(features);
+
+            equal(features.length, 1438);
 
             // Test 1 feature, insure its on chr1
             var c = features[0].chr;
@@ -22,27 +22,5 @@ function runSegUnitTests() {
         }, undefined);
 
     });
-
-    //asyncTest("SEG all features", 2, function () {
-    //
-    //    var url = "http://www.broadinstitute.org/igvdata/test/igv-web/segmented_data_080520.seg.gz";
-    //
-    //    var bedDataSource = new igv.FeatureSource({url: url});
-    //
-    //
-    //    bedDataSource.allFeatures(function (featureList) {
-    //
-    //        ok(featureList);
-    //
-    //        var len = featureList.length;
-    //
-    //        equal(len, 5001);
-    //
-    //
-    //        start();
-    //    });
-    //
-    //});
-
-
+    
 }
