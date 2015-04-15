26084a2e1a243ee79cb805d25583cb8ced2285af
Douglass Turner <douglass.turner@gmail.com>
2015-04-15 09:51:49 -0400

    Grunt file wrangling
    Former-commit-id: c01d891025a23a6a0f98969f98417a892e528fda

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,14 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        //qunit: {
-        //    hello: [ 'test/helloQUnit.html' ],
-        //    all: ['test/**/*.html']
-        //},
+        qunit: {
+            hello: [ 'test/helloQUnit.html' ],
+            all: ['test/**/*.html']
+        },
 
-        //connect: {
-        //    uses_defaults: {}
-        //},
+        connect: {
+            uses_defaults: {}
+        },
 
         concat: {
             igv: {
@@ -60,22 +60,21 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-md2html');
-    //grunt.loadNpmTasks('grunt-contrib-qunit');
-    //grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     //grunt.registerTask('default', ['concat:igvexp', 'uglify:igvexp']);
     //grunt.registerTask('default', ['concat:igv', 'uglify:igv', 'md2html:igv']);
     grunt.registerTask('default', ['concat:igv', 'uglify:igv', 'md2html:igv']);
 
-    //grunt.task.registerTask('unittest', 'Run one unit test.', function(testname) {
-    //
-    //    if(!!testname)
-    //        grunt.config('qunit.all', ['test/' + testname + '.html']);
-    //
-    //    grunt.task.run('qunit:all');
-    //
-    //});
+    grunt.task.registerTask('unittest', 'Run one unit test.', function(testname) {
 
+        if(!!testname)
+            grunt.config('qunit.all', ['test/' + testname + '.html']);
+
+        grunt.task.run('qunit:all');
+
+    });
 };
 
