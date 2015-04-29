35f62d173db7f9bd342dc8b7e09a355abfaac9c5
Douglass Turner <douglass.turner@gmail.com>
2015-04-29 09:22:22 -0400

    Issue 89 - Dialog Improvements. Stub out igv.Dialog behavior.

--- js/dialog.js
+++ js/dialog.js
@@ -28,9 +28,12 @@
  */
 var igv = (function (igv) {
 
-    igv.Dialog = function (parentObject) {
+    igv.Dialog = function (parentObject, inputChange) {
 
         var self = this;
+
+        this.rgb_re = /([012]\d\d|\d\d{0,1})(\s*?,\s*?)([012]\d\d|\d\d{0,1})(\s*?,\s*?)([012]\d\d|\d\d{0,1})/;
+        this.hex_re = new RegExp('^#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})$');
 
         this.container = $('<div class="grid-container-dialog ui-widget-content">');
         parentObject.append( this.container[ 0 ] );
@@ -48,6 +51,90 @@ var igv = (function (igv) {
 
         this.container.append(this.header[ 0 ]);
 
+
+
+        // user colors
+        self.container.append(rowOfTrackName()[ 0 ]);
+
+        function rowOfTrackName() {
+
+            var rowContainer,
+                row,
+                column;
+
+            row = $('<div class="grid-colorpicker">');
+
+            column = $('<div class="col col-4-4">');
+            self.trackNameInput = $('<input class="user-color-input" type="text" value="#000000">');
+            self.trackNameInput.change(function () {
+
+                var raw = $(this).val(),
+                    cooked;
+
+                cooked = raw;
+                if ("" === raw) {
+                    cooked = "untitled";
+                }
+
+                igv.setTrackLabel(self.trackView.track, cooked);
+                self.trackView.update();
+                self.hide();
+
+                //var color = parseColor($(this).val());
+                //
+                //if (undefined !== color) {
+                //    igv.setTrackColor(self.trackView.track, color);
+                //    self.trackView.update();
+                //}
+
+            });
+
+            //self.trackNameInput.keyup(function() {
+            //
+            //    var value = $(this).val(),
+            //        color;
+            //
+            //    if ("" === value) {
+            //        $(this).css( { "color" : "#222222" } );
+            //        return;
+            //    }
+            //
+            //    color = parseColor($(this).val());
+            //    if (undefined !== color) {
+            //        $(this).css( { "color" : color } );
+            //    }
+            //
+            //});
+
+            column.append( self.trackNameInput[ 0 ] );
+            row.append( column[ 0 ] );
+
+            rowContainer = $('<div class="grid-rect">');
+            rowContainer.append( row[ 0 ]);
+
+            function parseColor(value) {
+
+                var rgb,
+                    hex;
+
+                rgb = self.rgb_re.exec(value);
+
+                if (null !== rgb) {
+                    return "rgb(" + rgb[ 0 ] + ")";
+                } else {
+                    hex = self.hex_re.exec(value);
+                    if (null !== hex) {
+                        return igv.hex2Color( hex[ 0 ] );
+                    }
+                }
+
+                return undefined;
+            }
+
+            return rowContainer;
+
+        }
+
     };
 
     igv.Dialog.prototype.hide = function () {
@@ -64,7 +151,7 @@ var igv = (function (igv) {
         $(this.container).offset( { left: (track_size.width - size.width)/2, top: track_origin.top } );
 
         this.headerBlurb.text("Track Name");
-        //this.headerBlurb.text(this.trackView.track.name);
+        this.trackNameInput.val(this.trackView.track.name);
 
         this.container.show();
     };
