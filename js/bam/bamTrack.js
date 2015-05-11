ed4de36c0958da84ab1669a3c968a5239542627e
jrobinso <jrobinso@broadinstitute.org>
2015-05-11 12:56:37 -0400

    Remove div vertical scrollbars to fix horizontal shift problem.

--- js/bam/bamTrack.js
+++ js/bam/bamTrack.js
@@ -49,9 +49,6 @@ var igv = (function (igv) {
 
         this.skippedColor = config.skippedColor || "rgb(150, 170, 170)";
 
-        this.maxHeight = config.maxHeight || 500;
-
-
         // divide the canvas into a coverage track region and an alignment track region
         this.alignmentRowYInset = 1;
 
