8cd39b3e134312da96c08baaacee140ea17bd364
jrobinso <jrobinso@broadinstitute.org>
2015-08-21 17:21:02 -0700

    Improve rendering of zero-quality alignments.   Issue #136

--- js/ga4gh/ga4ghAlignmentReader.js
+++ js/ga4gh/ga4ghAlignmentReader.js
@@ -124,7 +124,6 @@ var igv = (function (igv) {
                                     references.forEach(function (ref) {
                                         var refName = ref.name,
                                             alias = igv.browser.genome.getChromosomeName(refName);
-                                        console.log(refName + "  " + alias);
                                         self.chrNameMap[alias] = refName;
                                     });
                                     continuation(self.chrNameMap);
