2c65e01a19500eff7775c272de46b07591c6945d
Jim Robinson <jrobinso@broadinstitute.org>
2015-12-02 09:28:42 -0800

    ditto
    Former-commit-id: 96abd086b12e3b43e1c107cc8576cf945df5dfaa

--- js/module.js
+++ js/module.js
@@ -23,64 +23,24 @@
  * THE SOFTWARE.
  */
 
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2014 Broad Institute
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 /**
- * Self evalutating function that returns the global igv object, creating it if neccessary.
- * Purpose is to support shims in require.js and browserify.
+ * Support for module definition.  This code should be last in the concatenated "igv.js" file.
  *
  */
-(function igvForAMD(factory) {
-
-    if ( typeof define === 'function' && define.amd ) {
-
-        // AMD. Register as an anonymous module.
 
 
+if ( typeof define === 'function' && define.amd ) {
+    // AMD. Register as an anonymous module.
 
-        define(['jquery'], factory);
-
-
-    } else if (typeof exports === 'object') {
-
+    define(['jquery'], igv);
 
 
-        // Node/CommonJS style for Browserify
+} else if (typeof exports === 'object') {
+    // Node/CommonJS style for Browserify
+
+    module.exports = igv;
 
 
-
-        module.exports = igv;
-
+}
 
 
-    }
-
-})(function (){
-
-    if(igv === undefined) igv = {};
-    return igv;
-
-});
