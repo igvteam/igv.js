9e2ff5bee80e6db6fc9a19a1992ac85d62dc342a
Douglass Turner <douglass.turner@gmail.com>
2015-12-14 11:12:52 -0500

    popover. now presents ok button when showOKButton flag is set to true.

--- js/trackView.js
+++ js/trackView.js
@@ -235,7 +235,7 @@ var igv = (function (igv) {
 
                 description = this.track.description || this.track.name;
                 this.track.labelSpan.onclick = function (e) {
-                    igv.popover.presentTrackPopup(e.pageX, e.pageY, description);
+                    igv.popover.presentTrackPopup(e.pageX, e.pageY, description, false);
                 }
 
                 $(viewportDiv).scroll(function () {
@@ -761,7 +761,7 @@ var igv = (function (igv) {
                             xOrigin = Math.round(referenceFrame.toPixels((trackView.tile.startBP - referenceFrame.start)));
                             popupData = trackView.track.popupData(genomicLocation, canvasCoords.x - xOrigin, canvasCoords.y);
                             if (popupData && popupData.length > 0) {
-                                igv.popover.presentTrackPopup(e.pageX, e.pageY, igv.formatPopoverText(popupData));
+                                igv.popover.presentTrackPopup(e.pageX, e.pageY, igv.formatPopoverText(popupData), false);
                             }
                             mouseDownX = undefined;
                             popupTimer = undefined;
