b98816e48fcc40a5eeb97aa7cc34f30b21d6798a
Jim Robinson <jrobinso@broadinstitute.org>
2016-04-12 15:23:51 -0700

    Add data range axis to CoverageTrack

--- js/wigTrack.js
+++ js/wigTrack.js
@@ -62,68 +62,7 @@ var igv = (function (igv) {
             max: config.max
         };
 
-    };
-
-    igv.WIGTrack.prototype.paintAxis = function (ctx, pixelWidth, pixelHeight) {
-
-        var x1,
-            x2,
-            y1,
-            y2,
-            a,
-            b,
-            reference,
-            shim,
-            font = {
-                'font': 'normal 10px Arial',
-                'textAlign': 'right',
-                'strokeStyle': "black"
-            };
-
-        if (undefined === this.dataRange || undefined === this.dataRange.max || undefined === this.dataRange.min) {
-            return;
-        }
-
-        igv.graphics.fillRect(ctx, 0, 0, pixelWidth, pixelHeight, {'fillStyle': "rgb(255, 255, 255)"});
-
-        reference = 0.95 * pixelWidth;
-        x1 = reference - 8;
-        x2 = reference;
-
-        //shim = 0.5 * 0.125;
-        shim = .01;
-        y1 = y2 = shim * pixelHeight;
-
-        a = {x: x2, y: y1};
-
-        // tick
-        igv.graphics.strokeLine(ctx, x1, y1, x2, y2, font);
-        igv.graphics.fillText(ctx, prettyPrint(this.dataRange.max), x1 + 4, y1 + 12, font);
-
-        //shim = 0.25 * 0.125;
-        y1 = y2 = (1.0 - shim) * pixelHeight;
-
-        b = {x: x2, y: y1};
-
-        // tick
-        igv.graphics.strokeLine(ctx, x1, y1, x2, y2, font);
-        igv.graphics.fillText(ctx, prettyPrint(this.dataRange.min), x1 + 4, y1 - 4, font);
-
-        igv.graphics.strokeLine(ctx, a.x, a.y, b.x, b.y, font);
-
-        function prettyPrint(number) {
-            // if number >= 100, show whole number
-            // if >= 1 show 1 significant digits
-            // if <  1 show 2 significant digits
-
-            if (Math.abs(number) >= 100) {
-                return number.toFixed();
-            } else if (Math.abs(number) >= 1) {
-                return number.toFixed(1);
-            } else {
-                return number.toFixed(2);
-            }
-        }
+        this.paintAxis = igv.paintAxis;
 
     };
 
