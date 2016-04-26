b82aba0c1372313a463d694d1782707106d8ea1d
Jim Robinson <jrobinso@broadinstitute.org>
2016-04-25 22:29:49 -0700

    Fix for input dialog position wandering

--- js/ui/dialog.js
+++ js/ui/dialog.js
@@ -56,6 +56,9 @@ var igv = (function (igv) {
 
     igv.Dialog.dialogConstructor = function (dialog) {
 
+        dialog.$container.removeClass("igv-grid-container-dialog");
+        dialog.$container.addClass("igv-grid-container-alert-dialog");
+
         dialog.$container.append(dialog.rowOfLabel()[ 0 ]);
 
         dialog.$container.append(dialog.rowOfInput()[ 0 ]);
