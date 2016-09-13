fcbe14451f4e8864cf61727f8f6c972813f42286
jrobinso <jrobinso@broadinstitute.org>
2016-09-12 22:05:53 -0400

    Update examples

--- scripts/copyExamples.sh
+++ scripts/copyExamples.sh
@@ -2,6 +2,6 @@
 for file in $1/*.html
 do
   fn=$(basename $file)
-  sed 's%igv.org/web/release%igv.org/web/test%g' $file | sed 's/1.0.2/1.0.3-rc1/g' > $2/$fn
+  sed 's%igv.org/web/release%igv.org/web/test%g' $file | sed 's/1.0.3/1.04/g' > $2/$fn
 done
 
