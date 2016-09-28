309de38dd480418709a9694083bba1c4945cc115
Douglass Turner <douglass.turner@gmail.com>
2016-09-28 14:21:16 -0400

    browser.getKitchenSinkListWithLociAndViewportWidth. parse gene names.

--- js/igv-utils.js
+++ js/igv-utils.js
@@ -25,6 +25,38 @@
 
 var igv = (function (igv) {
 
+    igv.getURLViaPromise = function (url) {
+
+        // Return a new promise.
+        return new Promise(function(resolve, reject) {
+
+            // Do the usual XHR stuff
+            var req = new XMLHttpRequest();
+
+            req.open('GET', url);
+
+            req.onload = function() {
+                // This is called even on 404 etc., so check the status
+                if (req.status == 200) {
+
+                    // Resolve the promise with the response text
+                    resolve(req.response);
+                } else {
+
+                    // Otherwise reject with status text which will hopefully be a meaningful error
+                    reject(Error(req.statusText));
+                }
+            };
+
+            // Handle network errors
+            req.onerror = function() {
+                reject(Error("Network Error"));
+            };
+
+            // Make the request
+            req.send();
+        });
+    };
 
     igv.makeToggleButton = function (buttonOnLabel, buttonOffLabel, configurationKey, get$Target, continuation) {
 
