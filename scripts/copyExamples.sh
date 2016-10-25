e0778e92b406190eafeb982ca66f4c13760753e4
Jim Robinson <jrobinso@broadinstitute.org>
2016-10-25 13:29:10 -0700

    build script hack
    Former-commit-id: 1d0aff25511c2d37562823108e078d3eec5b5aba

--- scripts/copyExamples.sh
+++ scripts/copyExamples.sh
@@ -2,6 +2,6 @@
 for file in $1/*.html
 do
   fn=$(basename $file)
-  sed 's%igv.org/web/release%igv.org/web/test%g' $file | sed 's/1.0.1/1.0.2-rc2/g' > $2/$fn
+  sed 's%igv.org/web/release%igv.org/web/test%g' $file | sed 's/1.0.1/multilocus/g' > $2/$fn
 done
 
