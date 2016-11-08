c3ab000bdb20a5e26f9db1f0bad6ce0cd2922a81
Douglass Turner <douglass.turner@gmail.com>
2016-11-08 10:39:42 -0500

    Ruler track now hosts close button.
    Former-commit-id: 13c517c049f6ab3db5a72bdd66a4e5c179ab766f

--- js/viewport.js
+++ js/viewport.js
@@ -43,9 +43,8 @@ var igv = (function (igv) {
         this.contentDiv = $('<div class="igv-viewport-content-div">')[0];
         this.$viewport.append(this.contentDiv);
 
-        if (trackView.track instanceof igv.RulerTrack || trackView.track instanceof igv.SequenceTrack) {
-            // do nothing
-        } else if (this.genomicState.locusCount > 1) {
+        if (trackView.track instanceof igv.RulerTrack) {
+
             this.$close = $('<div class="igv-viewport-fa-close">');
             this.$closeButton = $('<i class="fa fa-times-circle">');
             this.$close.append(this.$closeButton);
