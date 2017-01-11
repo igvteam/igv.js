9f502a7a5fe2d64052c59297db6ca35732c214e0
David Ma <david.ma@petermac.org>
2017-01-11 11:21:10 +1100

    Copy css/igv.css and css/img/* to dist folder when project is built
    Former-commit-id: ea815f6cb278754482444c02c74979179cb25de5

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,8 +40,21 @@ module.exports = function (grunt) {
                 src: 'dist/igv.js',
                 dest: 'dist/igv.min.js'
             }
-        }
+        },
 
+        copy: {
+            css: {
+                expand: true,
+                src: 'css/igv.css',
+                dest: 'dist'
+            },
+            img: {
+                expand: true,
+                cwd: 'css/img',
+                src: '**',
+                dest: 'dist/img/'
+            }
+        }
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
@@ -50,11 +63,12 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     //grunt.registerTask('default', ['concat:igvexp', 'uglify:igvexp']);
     //grunt.registerTask('default', ['concat:igv', 'uglify:igv', 'md2html:igv']);
-    grunt.registerTask('default', ['concat:igv', 'uglify:igv']);
+    grunt.registerTask('default', ['concat:igv', 'uglify:igv', 'copy']);
 
     grunt.task.registerTask('unittest', 'Run one unit test.', function (testname) {
 
