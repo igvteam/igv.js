d5647b64204a6693f4d3abe9d5c42e541225d7c1
Jim Robinson <jrobinso@broadinstitute.org>
2017-02-20 08:20:16 -0800

    Add google analytics

--- js/browser.js
+++ js/browser.js
@@ -1459,7 +1459,7 @@ var igv = (function (igv) {
             isRulerTrack = false,
             isMouseDown = false,
             isDragging = false,
-            lastMouseX = undefined,
+            lastMouseX = unde
             mouseDownX = undefined;
 
         $(trackContainerDiv).mousedown(function (e) {
