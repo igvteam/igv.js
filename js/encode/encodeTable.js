495ec3719a17e2dd2684f5d64cbecf73e579d7e1
Douglass Turner <douglass.turner@gmail.com>
2017-08-05 10:33:30 -0400

    unbindAllMouseHandlers() uncommented.

--- js/encode/encodeTable.js
+++ js/encode/encodeTable.js
@@ -91,19 +91,19 @@ var encode = (function (encode) {
 
     };
 
-    // encode.EncodeTable.prototype.unbindAllMouseHandlers = function () {
-    //
-    //     this.$modalTable.find('tbody').unbind();
-    //
-    //     $('#hicEncodeModal').unbind();
-    //
-    //     $('#encodeModalTopCloseButton').unbind();
-    //
-    //     $('#encodeModalBottomCloseButton').unbind();
-    //
-    //     $('#encodeModalGoButton').unbind();
-    //
-    // };
+    encode.EncodeTable.prototype.unbindAllMouseHandlers = function () {
+
+        this.$modalTable.find('tbody').unbind();
+
+        $('#hicEncodeModal').unbind();
+
+        $('#encodeModalTopCloseButton').unbind();
+
+        $('#encodeModalBottomCloseButton').unbind();
+
+        $('#encodeModalGoButton').unbind();
+
+    };
 
     encode.EncodeTable.prototype.createTableWithDataSource = function (dataSource) {
 
