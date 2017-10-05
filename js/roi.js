146b518265bc4a17f78e5e2ccd82b3bebd748ed6
Douglass Turner <douglass.turner@gmail.com>
2017-10-05 17:48:27 -0400

    Highlight Region of Interest (ROI). It begins.

---
+++ js/roi.js
@@ -0,0 +1,137 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2017 The Regents of the University of California
+ * Author: Jim Robinson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+var igv = (function (igv) {
+
+    igv.ROI = function (config) {
+
+        igv.configTrack(this, config);
+
+        this.featureSource = new igv.FeatureSource(config);
+
+        this.render = renderRegion;
+
+    };
+
+    igv.ROI.prototype.getFeatures = function (chr, bpStart, bpEnd, bpPerPixel) {
+
+        var self = this;
+
+        return new Promise(function (fulfill, reject) {
+
+            self.featureSource
+                .getFeatures(chr, bpStart, bpEnd, bpPerPixel)
+                .then(fulfill)
+                .catch(reject);
+
+        });
+    };
+
+    igv.ROI.prototype.draw = function (options) {
+
+        var self = this,
+            regions,
+            bpEnd,
+            coord,
+            color,
+            region;
+
+        igv.graphics.fillRect(options.context, 0, 0, options.pixelWidth, options.pixelHeight, { fillStyle:'rgb(255, 255, 255)' });
+
+        if (options.features) {
+
+            regions = options.features;
+            bpEnd = options.bpStart + (options.pixelWidth * options.bpPerPixel + 1);
+            for (var i = 0, len = regions.length; i < len; i++) {
+
+                region = regions[ i ];
+                if (region.end < options.bpStart) {
+                    continue;
+                }
+
+                if (region.start > bpEnd) {
+                    break;
+                }
+
+                color = this.color;
+
+                if (feature.color) {
+                    color = region.color;
+                }
+
+                options.context.fillStyle = color;
+                options.context.strokeStyle = color;
+
+                coord = coordinates(region, options.bpStart, options.bpPerPixel);
+                options.context.fillRect(coord.x, 0, coord.width, this.featureHeight);
+
+            }
+
+        }
+
+    };
+
+    function renderRegion(region, startBP, bpp, pixelHeight, ctx, options) {
+
+        var coord,
+            color;
+
+        coord = coordinates(region, startBP, bpp);
+        color = this.color;
+
+        if (region.color) {
+            color = region.color;
+        }
+
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color;
+
+        ctx.fillRect(coord.x, 0, coord.width, this.featureHeight);
+
+    }
+
+    function coordinates(region, startBP, bpp) {
+
+        var ss,
+            ee,
+            width;
+
+        ss = Math.round((region.start - startBP) / bpp);
+        ee = Math.round((region.end - startBP) / bpp);
+        width = ee - ss;
+
+        if (width < 3) {
+            width = 3;
+            ss -= 1;
+        }
+
+        return { x: ss, width: width };
+    }
+
+    return igv;
+
+})
+(igv || {});
