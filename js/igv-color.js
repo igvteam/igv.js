1d75f96556099550483c0654196ebee84d7b50d1
Douglass Turner <douglass.turner@gmail.com>
2017-10-26 17:01:04 -0400

    IGV Colorpicker. Based on JuiceboxJS colorpicker (#452)
    Former-commit-id: a562f6d11424af0acb6d20482774040557bbc14d

--- js/igv-color.js
+++ js/igv-color.js
@@ -28,6 +28,167 @@
  */
 var igv = (function (igv) {
 
+    igv.createColorSwatchSelector = function ($parent, colorHandler, closeHandler) {
+
+        var $div,
+            $fa,
+            $close_container,
+            rgbStrings,
+            s;
+
+        s = 1;
+        rgbStrings = [];
+        for(var v = 1; v >= 0.5; v -= .1) {
+            for (var rgb, h = 0; h < 1; h += 1/28) {
+                rgb = "rgb(" + hsvToRgb(h, s, v).join(",") + ")";
+                rgbStrings.push(rgb);
+            }
+        }
+
+        // close button container
+        $close_container = $('<div>');
+        $parent.append($close_container);
+
+        // close button
+        $div = $('<div>', { class: 'igv-colorpicker-menu-close-button' });
+        $close_container.append($div);
+
+        $fa = $("<i>", {class: 'fa fa-times'});
+        $div.append($fa);
+
+        $fa.on('click', function (e) {
+            closeHandler();
+        });
+
+        rgbStrings.forEach(function (rgbString) {
+            var $swatch;
+
+            $swatch = igv.colorSwatch(rgbString);
+            $parent.append($swatch);
+
+            $swatch.click(function () {
+                colorHandler(rgbString);
+            });
+
+        });
+
+    };
+
+    igv.colorSwatch = function (rgbString) {
+        var $swatch,
+            $fa;
+
+        $swatch = $('<div>', { class: 'igv-color-swatch' });
+
+        $fa = $('<i>', { class: 'fa fa-square fa-lg', 'aria-hidden': 'true' });
+        $swatch.append($fa);
+
+        $fa.css({color: rgbString});
+
+        return $swatch;
+    };
+
+    function rgbToHex(rgb) {
+        rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+        return (rgb && rgb.length === 4) ? "#" +
+            ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+            ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+            ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
+    }
+
+    function hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return "rgb(" +
+            parseInt(result[1], 16) + ", " +
+            parseInt(result[2], 16) + ", " +
+            parseInt(result[3], 16) + ")";
+    }
+
+    /**
+     * Converts an HSV color value to RGB. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+     * Assumes h, s, and v are contained in the set [0, 1] and
+     * returns r, g, and b in the set [0, 255].
+     *
+     * Credit: https://gist.githubusercontent.com/mjackson/5311256
+     *
+     * @param   Number  h       The hue
+     * @param   Number  s       The saturation
+     * @param   Number  v       The value
+     * @return  Array           The RGB representation
+     */
+    function hsvToRgb(h, s, v) {
+        var r, g, b;
+
+        var i = Math.floor(h * 6);
+        var f = h * 6 - i;
+        var p = v * (1 - s);
+        var q = v * (1 - f * s);
+        var t = v * (1 - (1 - f) * s);
+
+        switch (i % 6) {
+            case 0:
+                r = v, g = t, b = p;
+                break;
+            case 1:
+                r = q, g = v, b = p;
+                break;
+            case 2:
+                r = p, g = v, b = t;
+                break;
+            case 3:
+                r = p, g = q, b = v;
+                break;
+            case 4:
+                r = t, g = p, b = v;
+                break;
+            case 5:
+                r = v, g = p, b = q;
+                break;
+        }
+
+        return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
+    }
+
+    /**
+     * Converts an HSL color value to RGB. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes h, s, and l are contained in the set [0, 1] and
+     * returns r, g, and b in the set [0, 255].
+     *
+     * Credit: https://gist.githubusercontent.com/mjackson/5311256
+     *
+     * @param   Number  h       The hue
+     * @param   Number  s       The saturation
+     * @param   Number  l       The lightness
+     * @return  Array           The RGB representation
+     */
+    function hslToRgb(h, s, l) {
+        var r, g, b;
+
+        if (s === 0) {
+            r = g = b = l; // achromatic
+        } else {
+            function hue2rgb(p, q, t) {
+                if (t < 0) t += 1;
+                if (t > 1) t -= 1;
+                if (t < 1 / 6) return p + (q - p) * 6 * t;
+                if (t < 1 / 2) return q;
+                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+                return p;
+            }
+
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+
+            r = hue2rgb(p, q, h + 1 / 3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1 / 3);
+        }
+
+        return [r * 255, g * 255, b * 255];
+    }
+
     igv.hex2Color = function (hex) {
 
         var cooked = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
