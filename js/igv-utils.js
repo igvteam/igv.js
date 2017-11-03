7326545baa4cc2820c803cbb270d57562c60c9e7
Douglass Turner <douglass.turner@gmail.com>
2017-11-03 17:12:49 -0400

    Discard obsolete colorpicker. CSS Refactoring. (#465)
    * Discard colorpicker.js. Replaced by juiceboxjs color picker.
    
    * discard unused css
    
    * add _igv-genereic-container.scss
    
    * refactor css

--- js/igv-utils.js
+++ js/igv-utils.js
@@ -27,6 +27,45 @@ var igv = (function (igv) {
 
     var self = this;
 
+    igv.genericContainer = function ($parent, config, closeHandler) {
+
+        var $generic_container,
+            $header,
+            $fa;
+
+        $generic_container = $('<div>', { class:'igv-generic-container' });
+        $parent.append($generic_container);
+
+        // width
+        if (config && config.width) {
+            $generic_container.width(config.width);
+        }
+
+        // height
+        if (config && config.height) {
+            $generic_container.height(config.height);
+        }
+
+        // height
+        if (config && config.classes) {
+            $generic_container.addClass( config.classes.join(' ') );
+        }
+
+        // header
+        $header = $('<div>');
+        $generic_container.append($header);
+
+        // close button
+        $fa = $("<i>", { class:'fa fa-times' });
+        $header.append($fa);
+
+        $fa.on('click', function (e) {
+            closeHandler();
+        });
+
+        return $generic_container;
+    };
+
     igv.makeDraggable = function ($target, $handle) {
         $handle.on('mousedown', function (event) {
 
