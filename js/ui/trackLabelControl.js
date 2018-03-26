3b719d8be1571da4f92633b3f7bea6c5a9de2897
Douglass Turner <douglass.turner@gmail.com>
2018-03-26 17:33:52 -0400

    Browser. Add track label visibility api and trackLabelControl object.
    Former-commit-id: 9fcc9cfc5cfc6b80740992c651078872ae6f69db

---
+++ js/ui/trackLabelControl.js
@@ -0,0 +1,57 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 University of California San Diego
+ * Author: Jim Robinson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * Created by dat on 3/26/18.
+ */
+var igv = (function (igv) {
+
+    igv.TrackLabelControl = function ($parent, config) {
+        var self = this;
+
+        this.$button = $('<div class="igv-nav-bar-button">');
+        $parent.append(this.$button);
+        this.$button.text('track labels');
+
+        this.$button.on('click', function () {
+            if (true === igv.browser.trackLabelsVisible) {
+                igv.browser.hideTracklabels();
+            } else {
+                igv.browser.showTracklabels();
+            }
+        });
+
+        if (true === config.showTrackLabels) {
+            this.$button.show();
+        } else {
+            this.$button.hide();
+        }
+
+    };
+
+    return igv;
+
+}) (igv || {});
