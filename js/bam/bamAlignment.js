e67eaa45805bcdd893f133977a7592fdfbe13bc2
Jim Robinson <jrobinso@broadinstitute.org>
2018-05-10 00:33:14 -0700

    Add readgroup filter.   Fixes #145
    Former-commit-id: 11fcc5b04142997a7531a25d6e9f22785507cda1

--- js/bam/bamAlignment.js
+++ js/bam/bamAlignment.js
@@ -302,12 +302,16 @@ var igv = (function (igv) {
 
 
     igv.BamFilter = function (options) {
+
         if (!options) options = {};
         this.vendorFailed = options.vendorFailed === undefined ? true : options.vendorFailed;
         this.duplicates = options.duplicates === undefined ? true : options.duplicates;
         this.secondary = options.secondary || false;
         this.supplementary = options.supplementary || false;
         this.mqThreshold = options.mqThreshold === undefined ? 0 : options.mqThreshold;
+        if(options.readgroups) {
+            this.readgroups = new Set(options.readgroups);
+        }
     }
 
     igv.BamFilter.prototype.pass = function (alignment) {
@@ -317,6 +321,11 @@ var igv = (function (igv) {
         if (this.secondary && alignment.isSecondary()) return false;
         if (this.supplementary && alignment.isSupplementary()) return false;
         if (alignment.mq < this.mqThreshold) return false;
+
+        if(this.readgroups) {
+            var rg = alignment.tags()['RG'];
+            return this.readgroups.has(rg);
+        }
 
         return true;
 
