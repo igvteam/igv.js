64943dafaec5cbaba537fb07002e4f5b08aaebda
Jim Robinson <jrobinso@broadinstitute.org>
2018-07-05 23:26:23 -0700

    Fix gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,7 +90,7 @@ module.exports = function (grunt) {
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     //grunt.registerTask('default', ['concat:igvexp', 'uglify:igvexp']);
     //grunt.registerTask('default', ['concat:igv', 'uglify:igv', 'md2html:igv']);
-    grunt.registerTask('default', [ 'concat:css', 'embed-css', 'concat:igv', 'uglify:igv','copy']);
+    grunt.registerTask('default', [ 'concat:css', 'embed-css', 'concat:igv', 'uglify:igv']);
 
     grunt.task.registerTask('unittest', 'Run one unit test.', function (testname) {
 
