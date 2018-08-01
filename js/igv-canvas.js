dde0273dbfcbf8d4a6fb40fa76696ac19b82d584
Jim Robinson <jrobinso@broadinstitute.org>
2018-08-01 07:42:17 -0700

    restore missing graphics functions
    Former-commit-id: 3639f021c5f3e3a1a71aa4a96d271ed7db510eeb

--- js/igv-canvas.js
+++ js/igv-canvas.js
@@ -243,6 +243,59 @@ var igv = (function (igv) {
             ctx.restore();
         },
 
+        roundRect: function (ctx, x, y, width, height, radius, fill, stroke) {
+
+            ctx.save();
+            if (typeof stroke == "undefined") {
+                stroke = true;
+            }
+            if (typeof radius === "undefined") {
+                radius = 5;
+            }
+            ctx.beginPath();
+            ctx.moveTo(x + radius, y);
+            ctx.lineTo(x + width - radius, y);
+            ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+            ctx.lineTo(x + width, y + height - radius);
+            ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+            ctx.lineTo(x + radius, y + height);
+            ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+            ctx.lineTo(x, y + radius);
+            ctx.quadraticCurveTo(x, y, x + radius, y);
+            ctx.closePath();
+            if (stroke) {
+                ctx.stroke();
+            }
+            if (fill) {
+                ctx.fill();
+            }
+            ctx.restore();
+        },
+        polygon: function (ctx, x, y, fill, stroke) {
+
+            ctx.save();
+            if (typeof stroke == "undefined") {
+                stroke = true;
+            }
+
+            ctx.beginPath();
+            var len = x.length;
+            ctx.moveTo(x[0], y[0]);
+            for (var i = 1; i < len; i++) {
+                ctx.lineTo(x[i], y[i]);
+                // this.moveTo(x[i], y[i]);
+            }
+
+            ctx.closePath();
+            if (stroke) {
+                ctx.stroke();
+            }
+            if (fill) {
+                ctx.fill();
+            }
+            ctx.restore();
+        }
+
 
     }
 
