8c798be8b27d2756118e09ec456a4ccaf8e28ac6
Jim Robinson <jrobinso@broadinstitute.org>
2018-07-31 21:21:36 -0700

    es6 build -- almost working.  See issue #674
    Former-commit-id: 3615fc9566070e83cc87baf450c66591a047d9b1

---
+++ examples/js/main-es6.js
@@ -0,0 +1,39 @@
+import igv from './igv.es6.js'
+
+(function () {
+
+    var igvDiv,
+        options;
+
+    igvDiv = document.getElementById("igv-div");
+    options =
+    {
+        showNavigation: true,
+        showRuler: true,
+        genome: "hg19",
+        locus: 'chr7',
+        tracks: [
+            {
+                url: 'https://data.broadinstitute.org/igvdata/test/igv-web/segmented_data_080520.seg.gz',
+                indexed: false,
+                isLog: true,
+                name: 'Segmented CN'
+            },
+            {
+                name: "Genes",
+                type: "annotation",
+                format: "bed",
+                sourceType: "file",
+                url: "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz",
+                indexURL: "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz.tbi",
+                order: Number.MAX_VALUE,
+                visibilityWindow: 300000000,
+                displayMode: "EXPANDED"
+            }
+        ]
+    };
+
+
+    igv.createBrowser(igvDiv, options);
+
+})()
