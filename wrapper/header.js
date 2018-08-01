a5e0101cb53012b96e9d84a9cbf9c01598289fb5
Jim Robinson <jrobinso@broadinstitute.org>
2018-08-01 14:32:11 -0700

    restore amd and commjs wrapper
    Former-commit-id: 343c27c633de084bea894f39880ab34113bd5bc7

--- wrapper/header.js
+++ wrapper/header.js
@@ -1,10 +1,27 @@
-let igv;
+/* Module header based on https://github.com/umdjs/umd/blob/master/templates/returnExports.js
+ */
 
 (function (root, factory) {
-
-
-    igv = factory();
-
-}(this, function () {
+   
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.igv = factory();
+    }
+ }(this, function () {
+//
+//     // Just return a value to define the module export.
+//     // This example returns an object, but the module
+//     // can return a function as the exported value.
+//     return igv;
+//
+// }));
 
 
