80e4ec25aed91b7ae1389b6438d2ab1fba240221
Jim Robinson <jrobinso@broadinstitute.org>
2018-08-03 11:20:13 -0700

    update example and dev html
    Former-commit-id: ef874f46dbe985e789eaf8b0c3dc426229593186

--- examples/js/main-es6.js
+++ examples/js/main-es6.js
@@ -16,17 +16,6 @@ import igv from '../../dist/igv.es6.min.js'
                 indexed: false,
                 isLog: true,
                 name: 'Segmented CN'
-            },
-            {
-                name: "Genes",
-                type: "annotation",
-                format: "bed",
-                sourceType: "file",
-                url: "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz",
-                indexURL: "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz.tbi",
-                order: Number.MAX_VALUE,
-                visibilityWindow: 300000000,
-                displayMode: "EXPANDED"
             }
         ]
     };
