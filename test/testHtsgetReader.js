011f742ad48072fe904a04ef8b3a2a933f21339d
Jim Robinson <jrobinso@broadinstitute.org>
2018-08-08 14:00:57 -0700

    Unit test maintenance

--- test/testHtsgetReader.js
+++ test/testHtsgetReader.js
@@ -20,17 +20,24 @@ function runHtsgetTests() {
 
         var url = 'http://htsnexus.rnd.dnanex.us/v1',
             id = 'BroadHiSeqX_b37/NA12878',
-            chr = 'chr1',
+            chr = '1',
             s = 10000,
             end = 10100;
 
         var reader = new igv.HtsgetReader({endpoint: url, id: id});
-        reader.readAlignments(chr, s, end).then(function (alignmentContainer) {
 
-            ok(alignmentContainer);
-            ok(alignmentContainer.alignments.length > 0);
-            start();
-        });
+        reader.readAlignments(chr, s, end)
+
+            .then(function (alignmentContainer) {
+                ok(alignmentContainer);
+                ok(alignmentContainer.alignments.length > 0);
+                start();
+            })
+            .catch(function (error) {
+                console.log(error);
+                ok(false);
+                start();
+            });
     });
 
     asyncTest('Load Urls - EBI', function () {
