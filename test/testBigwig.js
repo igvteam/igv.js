d3efb626445a0516ecd05b2e12c4895018de15ff
Jim Robinson <jrobinso@broadinstitute.org>
2018-08-08 13:11:00 -0700

    Support uncompressed bigwig files
    Former-commit-id: d064f1259bd7def38c9e8ecda941bf7e860989d7

--- test/testBigwig.js
+++ test/testBigwig.js
@@ -52,7 +52,6 @@ function runBigwigTests() {
     });
 
 
-
     asyncTest("Bigwig meta data", function () {
 
         var url = dataURL + "bigwig/bigWigExample.bw",
@@ -147,6 +146,32 @@ function runBigwigTests() {
             ok(features);
 
             equal(features.length, 337);   // Verified in iPad app
+
+            start();
+        }).catch(function (error) {
+            console.log(error);
+            ok(false);
+        });
+    });
+
+    asyncTest("Uncompressed bigwig", function () {
+
+        //chr21:19,146,376-19,193,466
+        var url =  dataURL + "bigwig/uncompressed.bw",
+            chr = "chr21",
+            bpStart = 0,
+            bpEnd = Number.MAX_SAFE_INTEGER,
+            bpPerPixel = 6191354.824;    // To match iOS unit test
+
+        createMockObjects(bpPerPixel);
+
+        var bwReader = new igv.BWReader({url: url});
+
+        bwReader.readFeatures(chr, bpStart, chr, bpEnd, bpPerPixel).then(function (features) {
+
+            ok(features);
+
+            equal(features.length, 8);   // Verified in iPad app
 
             start();
         }).catch(function (error) {
