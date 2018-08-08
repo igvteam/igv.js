4c84bc243703e8561f360013e02b7140ba570c6e
Jim Robinson <jrobinso@broadinstitute.org>
2018-08-08 15:41:26 -0700

    add test for chromosome aliasing for htsget reader
    Former-commit-id: 66f108326e41e02e87a19e2fdde6100b3ea94c26

--- test/testHtsgetReader.js
+++ test/testHtsgetReader.js
@@ -1,30 +1,23 @@
 function runHtsgetTests() {
     // mock object
-    if (igv === undefined) {
-        igv = {};
+
+    const genome = {
+        getChromosomeName: function (chr) {
+            return "chr" + chr;
+        }
     }
 
-    igv.browser = {
-        getFormat: function () {
-        },
-
-        genome: {
-
-            getChromosomeName: function (chr) {
-                return "chr" + chr;
-            }
-        }
-    };
 
     asyncTest('Load Urls - DNANexus', function () {
 
-        var url = 'http://htsnexus.rnd.dnanex.us/v1',
+        const url = 'http://htsnexus.rnd.dnanex.us/v1',
             id = 'BroadHiSeqX_b37/NA12878',
             chr = 'chr1',
             s = 10000,
             end = 10100;
 
-        var reader = new igv.HtsgetReader({endpoint: url, id: id});
+        const reader = new igv.HtsgetReader({endpoint: url, id: id, genome: genome});
+
 
         reader.readAlignments(chr, s, end)
 
