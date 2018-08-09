95b118374c87fe33f737e360e129b06ead35b414
Jim Robinson <jrobinso@broadinstitute.org>
2018-08-08 19:42:37 -0700

    Allow 200 response code from servers when entire (small) file is returned in response to a range request.

--- test/testIgvSession.js
+++ test/testIgvSession.js
@@ -1,24 +1,5 @@
 function runIgvSessionTests() {
 
-
-    // mock object
-    if (igv === undefined) {
-        igv = {};
-    }
-
-    igv.browser = {
-        getFormat: function () {
-        },
-
-        genome: {
-            getChromosome: function (chr) {
-            },
-            getChromosomeName: function (chr) {
-                return chr
-            }
-        }
-    };
-
     asyncTest("session file", function () {
 
         var url = "http://homer.ucsd.edu/cbenner/test/report-170817/homo_sapiens.igv.xml";
