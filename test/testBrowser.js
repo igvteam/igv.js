a5ee7cad2bf258c58cf2a7de36c6b20eaa1fa91d
Jim Robinson <jrobinso@broadinstitute.org>
2018-08-20 20:52:01 -0700

    update "Browser" test
    Former-commit-id: bdce35297e8f54609303480f0dea07ab14c35c9a

--- test/testBrowser.js
+++ test/testBrowser.js
@@ -4,11 +4,31 @@ function runBrowserTests() {
 
     const div = document.createElement("div");
 
-    asyncTest("Test options", function () {
+    // asyncTest("Test navigation option", function () {
+    //
+    //     const options = {
+    //         genome: "hg19",
+    //         showNavigation: false
+    //     };
+    //
+    //     igv.createBrowser(div, options)
+    //         .then(function (browser) {
+    //             ok(browser);
+    //             start();
+    //         })
+    //
+    //         .catch(function (error) {
+    //             ok(false);
+    //             console.log(error);
+    //             start();
+    //         })
+    // });
+
+    asyncTest("Test ruler option", function () {
 
         const options = {
             genome: "hg19",
-            showNavigation: false
+            showRuler: false
         };
 
         igv.createBrowser(div, options)
@@ -22,8 +42,6 @@ function runBrowserTests() {
                 console.log(error);
                 start();
             })
-
-
     })
 
 
