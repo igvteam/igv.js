077086e231f4c33469c61afbc0f89499daeb2199
Jim Robinson <jrobinso@broadinstitute.org>
2018-08-22 10:34:15 -0700

    Some memory cleanup, still some resource leaks.  See #726
    Former-commit-id: 0c6d6e5763795739c80bdbc8494284f85a778be5

--- js/igv-utils.js
+++ js/igv-utils.js
@@ -343,32 +343,6 @@ var igv = (function (igv) {
         return {x: eFixed.pageX, y: eFixed.pageY}
     }
 
-
-    igv.throttle = function (fn, threshhold, scope) {
-        threshhold || (threshhold = 200);
-        var last, deferTimer;
-
-        return function () {
-            var context = scope || this;
-
-            var now = +new Date,
-                args = arguments;
-            if (last && now < last + threshhold) {
-                // hold on to it
-                clearTimeout(deferTimer);
-                deferTimer = setTimeout(function () {
-                    last = now;
-                    fn.apply(context, args);
-                }, threshhold);
-            } else {
-                last = now;
-                fn.apply(context, args);
-            }
-        }
-    };
-
-    var foo = typeof igv.throttle;
-
     igv.splitStringRespectingQuotes = function (string, delim) {
 
         var tokens = [],
