5ddd8087a0d4119b3d0bbc0ed67f247db831a631
Jim Robinson <jrobinso@broadinstitute.org>
2018-08-22 18:51:17 -0700

    Add test file for removing browser
    Former-commit-id: 5551376bc388d6bf8ec609e4ee55535dcd23c686

--- test/testBrowser.js
+++ test/testBrowser.js
@@ -24,33 +24,33 @@ function runBrowserTests() {
             })
     });
 
-    // asyncTest("Test ruler option", function () {
-    //
-    //     const options = {
-    //         genome: "hg19",
-    //         showRuler: false
-    //     };
-    //
-    //     igv.createBrowser(div, options)
-    //         .then(function (browser) {
-    //             ok(browser);
-    //             start();
-    //         })
-    //
-    //         .catch(function (error) {
-    //             ok(false);
-    //             console.log(error);
-    //             start();
-    //         })
-    // });
+    asyncTest("Test ruler option", function () {
+
+        const options = {
+            genome: "hg19",
+            showRuler: false
+        };
+
+        igv.createBrowser(div, options)
+            .then(function (browser) {
+                ok(browser);
+                start();
+            })
+
+            .catch(function (error) {
+                ok(false);
+                console.log(error);
+                start();
+            })
+    });
 
 
     asyncTest("Test remove browser", function () {
 
         // Create and remove "n" browsers.
 
-        const n = 10;
-        let i =0;
+        const n = 5;
+        let i = 0;
 
         createRemove();
 
@@ -58,16 +58,18 @@ function runBrowserTests() {
 
             igv.createBrowser(div, {
                 genome: "hg19",
-                showRuler: false
+                locus: "myc"
             })
                 .then(function (browser) {
+
                     igv.removeBrowser(browser);
-                    if(i++ < 10) {
+                    if (i++ < 10) {
                         createRemove();
                     } else {
                         ok(true);
                         start();
                     }
+
 
                 })
                 .catch(function (error) {
@@ -76,8 +78,6 @@ function runBrowserTests() {
                     start();
                 })
         }
-
-
 
     })
 
