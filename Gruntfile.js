a73426d5e74c49e2919fe1c5e6bdf444effd2ba2
Eric Weitz <eweitz@broadinstitute.org>
2018-09-04 16:19:31 -0400

    Remove extraneous 'noSandbox' option

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,6 @@ module.exports = function (grunt) {
             test: {
               options: {
                 headless: true,
-                noSandbox: true,
                 traceSettings: {
                   outputConsole: true,
                   outputAllAssertions: true
