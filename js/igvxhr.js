fa9b37ccf4f5ff7c027f9ae5a8e4a8ea91d71a60
Jim Robinson <jrobinso@broadinstitute.org>
2018-10-19 20:36:55 -0700

    Implement TextDecoder for IE
    Former-commit-id: 75e3599a9307045ce6995d70cb172718d4272094

--- js/igvxhr.js
+++ js/igvxhr.js
@@ -490,7 +490,12 @@ var igv = (function (igv) {
             plain = new Uint8Array(arraybuffer);
         }
 
-        return new TextDecoder().decode(plain);
+        if(TextDecoder) {
+            return new TextDecoder().decode(plain);
+        }
+        else {
+            return decodeUtf8(plain);
+        }
 
     };
 
@@ -546,6 +551,7 @@ var igv = (function (igv) {
     //Increments an anonymous usage count.  Count is anonymous, needed for our continued funding.  Please don't delete
 
     let startupCalls = 0;
+
     function startup() {
 
         const href = window.document.location.href;
@@ -569,6 +575,53 @@ var igv = (function (igv) {
         const regex = new RegExp(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/);
         return regex.test(address);
     }
+
+
+    /**
+     * Use when TextDecoder is not available (primarily IE).
+     *
+     * From: https://gist.github.com/Yaffle/5458286
+     *
+     * @param octets
+     * @returns {string}
+     */
+    decodeUtf8 = function (octets) {
+        var string = "";
+        var i = 0;
+        while (i < octets.length) {
+            var octet = octets[i];
+            var bytesNeeded = 0;
+            var codePoint = 0;
+            if (octet <= 0x7F) {
+                bytesNeeded = 0;
+                codePoint = octet & 0xFF;
+            } else if (octet <= 0xDF) {
+                bytesNeeded = 1;
+                codePoint = octet & 0x1F;
+            } else if (octet <= 0xEF) {
+                bytesNeeded = 2;
+                codePoint = octet & 0x0F;
+            } else if (octet <= 0xF4) {
+                bytesNeeded = 3;
+                codePoint = octet & 0x07;
+            }
+            if (octets.length - i - bytesNeeded > 0) {
+                var k = 0;
+                while (k < bytesNeeded) {
+                    octet = octets[i + k + 1];
+                    codePoint = (codePoint << 6) | (octet & 0x3F);
+                    k += 1;
+                }
+            } else {
+                codePoint = 0xFFFD;
+                bytesNeeded = octets.length - i;
+            }
+            string += String.fromCodePoint(codePoint);
+            i += bytesNeeded + 1;
+        }
+        return string
+    }
+
 
     return igv;
 })
