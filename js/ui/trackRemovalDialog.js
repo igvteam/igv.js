745e38a9f890fce04b7cbd534c9113c520a730e7
Douglass Turner <douglass.turner@gmail.com>
2018-11-01 15:20:40 -0400

    Track Color Picker Rethink. (#772)
    * track color paletter
    
    * drag bug fix
    
    * positioning
    
    * track color picker
    
    * color swatch tweaks
    
    * add default track color to color picker
    
    * track color palette ideation
    
    * apple color palette
    
    * bug fix. interactionTrack
    
    * dev
    
    * reorg
    
    * rebuild .css
    
    
    Former-commit-id: 2208bba96fb53013a7614dfd84bb28e9d4454091

--- js/ui/trackRemovalDialog.js
+++ js/ui/trackRemovalDialog.js
@@ -76,8 +76,8 @@ var igv = (function (igv) {
             self.$container.hide();
         });
 
-       // this.$container.draggable({ handle:$header.get(0) });
-igv.makeDraggable(this.$container.get(0), $header.get(0));
+        // this.$container.draggable({ handle:$header.get(0) });
+        igv.makeDraggable(this.$container.get(0), $header.get(0));
         this.$container.hide();
     };
 
