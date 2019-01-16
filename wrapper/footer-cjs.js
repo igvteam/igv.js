0b89b991624448db3206a7c87adb0282ad03a2b3
Jim Robinson <jrobinso@broadinstitute.org>
2019-01-15 22:20:20 -0800

    Packaging overhaul * webpack now used to bundle * async/await supported

---
+++ wrapper/footer-cjs.js
@@ -0,0 +1,4 @@
+// Just return a value to define the module export.
+//
+    return igv;
+})()
---
+++ wrapper/header-cjs.js
@@ -0,0 +1,6 @@
+/* Module header based on https://github.com/umdjs/umd/blob/master/templates/returnExports.js
+ */
+
+module.exports = (function () {
+
+    const regeneratorRuntime = require("regenerator-runtime");
