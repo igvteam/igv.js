e4808a0fb0d507d73aeaa708636a24f71bbba87b
Douglass Turner <douglass.turner@gmail.com>
2019-01-17 13:17:59 -0500

    embedCss.js edits

--- wrapper/embedCss.js
+++ wrapper/embedCss.js
@@ -6,19 +6,6 @@
     style.setAttribute('type', 'text/css');
     style.innerHTML = css;
 
-    var headChildren = document.head.childNodes;
-    var beforeChild = null;
-
-    for (var i = headChildren.length - 1; i > -1; i--) {
-        var child = headChildren[i];
-        var tagName = (child.tagName || '').toUpperCase();
-        if (['STYLE', 'LINK'].indexOf(tagName) > -1) {
-            beforeChild = child;
-        }
-    }
-    // document.head.insertBefore(style, beforeChild);
-
-    beforeChild = headChildren[ headChildren.length - 1 ];
-    document.head.insertBefore(style, beforeChild);
+    document.head.insertBefore(style, document.head.childNodes[ document.head.childNodes.length - 1 ]);
 
 })();
