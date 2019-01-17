32670b5953a4f70fd76a532a3c3f2a46ce5fc6ff
Douglass Turner <douglass.turner@gmail.com>
2019-01-17 15:15:16 -0500

    embedCss.js edits (#838)
    * rethink placement of igv.css in html
    
    * embedCss.js edits

--- wrapper/embedCss.js
+++ wrapper/embedCss.js
@@ -1,22 +1,11 @@
 (function () {
 
     var css =  '_CSS_';
-    
+
     var style = document.createElement('style');
     style.setAttribute('type', 'text/css');
     style.innerHTML = css;
 
-    var headChildren = document.head.childNodes;
-    var beforeChild = null;
-
-    for (var i = headChildren.length - 1; i > -1; i--) {
-        var child = headChildren[i];
-        var tagName = (child.tagName || '').toUpperCase();
-        if (['STYLE', 'LINK'].indexOf(tagName) > -1) {
-            beforeChild = child;
-        }
-    }
-
-    document.head.insertBefore(style, beforeChild);
+    document.head.insertBefore(style, document.head.childNodes[ document.head.childNodes.length - 1 ]);
 
 })();
