d95a44ebdf58be42b75c6e0f85372a3c4504763d
Jim Robinson <jrobinso@broadinstitute.org>
2019-01-17 15:51:53 -0800

    use igvxhr for cram reading -- fetch fails with dropbox and google
    Former-commit-id: a265de27dcf7af30a2c7465f93f1b5f5e6b3ac87

--- js/cram/cramReader.js
+++ js/cram/cramReader.js
@@ -50,15 +50,16 @@ var igv = (function (igv) {
         this.genome = genome;
 
         this.cramFile = new gmodCRAM.CramFile({
-            url: config.url,
+            filehandle: new RemoteFile(config.url),
             seqFetch: config.seqFetch || seqFetch.bind(this),
             checkSequenceMD5: config.checkSequenceMD5 !== undefined ? config.checkSequenceMD5 : true
         })
 
+        const indexFileHandle = new RemoteFile(config.indexURL)
         this.indexedCramFile = new gmodCRAM.IndexedCramFile({
             cram: this.cramFile,
             index: new gmodCRAM.CraiIndex({
-                url: config.indexURL
+                filehandle: indexFileHandle
             }),
             fetchSizeLimit: 30000000
         });
@@ -395,6 +396,264 @@ var igv = (function (igv) {
       readName
 
    */
+    class RemoteFile {
+        constructor(source) {
+            this.position = 0
+            this.url = source
+            this.cache = new BufferCache({
+                fetch: (start, length) => this._fetch(start, length),
+            })
+        }
+
+        async _fetch(position, length) {
+
+            const loadRange = {start: position, size: length};
+            this._stat = {size: undefined}
+            return  igv.xhr.loadArrayBuffer(this.url, igv.buildOptions({}, {range: loadRange}))
+                .then(function (arrayBuffer) {
+                    const nodeBuffer = Buffer.from(arrayBuffer)
+                    return nodeBuffer
+                })
+
+            // const headers = {}
+            // if (length < Infinity) {
+            //     headers.range = `bytes=${position}-${position + length}`
+            // } else if (length === Infinity && position !== 0) {
+            //     headers.range = `bytes=${position}-`
+            // }
+            // const response = await fetch(this.url, {
+            //     method: 'GET',
+            //     headers,
+            //     redirect: 'follow',
+            //     mode: 'cors',
+            // })
+            // if (
+            //     (response.status === 200 && position === 0) ||
+            //     response.status === 206
+            // ) {
+            //     const nodeBuffer = Buffer.from(await response.arrayBuffer())
+            //
+            //     // try to parse out the size of the remote file
+            //     const sizeMatch = /\/(\d+)$/.exec(response.headers.get('content-range'))
+            //     if (sizeMatch && sizeMatch[1]) {
+            //         this._stat = { size: parseInt(sizeMatch[1], 10)}
+            //     } else {
+            //         this._stat = { size: undefined}
+            //     }
+            //
+            //     return nodeBuffer
+            // }
+            // throw new Error(`HTTP ${response.status} fetching ${this.url}`)
+        }
+
+        read(buffer, offset = 0, length = Infinity, position = 0) {
+            let readPosition = position
+            if (readPosition === null) {
+                readPosition = this.position
+                this.position += length
+            }
+            return this.cache.get(buffer, offset, length, position)
+        }
+
+        async readFile() {
+            const arrayBuffer = await igv.xhr.loadArrayBuffer(this.url, igv.buildOptions({}))
+            return Buffer.from(arrayBuffer)
+        }
+
+        async stat() {
+            if (!this._stat) {
+                const buf = Buffer.allocUnsafe(10)
+                await this.read(buf, 0, 10, 0)
+                if (!this._stat)
+                    throw new Error(`unable to determine size of file at ${this.url}`)
+            }
+            return this._stat
+        }
+    }
+
+    class BufferCache {
+        constructor({ fetch, size = 10000000, chunkSize = 32768 }) {
+            if (!fetch) throw new Error('fetch function required')
+            this.fetch = fetch
+            this.chunkSize = chunkSize
+            this.lruCache = new QuickLRU({ maxSize: Math.floor(size / chunkSize) })
+        }
+        async get(outputBuffer, offset, length, position) {
+            if (outputBuffer.length < offset + length)
+                throw new Error('output buffer not big enough for request')
+
+            // calculate the list of chunks involved in this fetch
+            const firstChunk = Math.floor(position / this.chunkSize)
+            const lastChunk = Math.floor((position + length) / this.chunkSize)
+
+            // fetch them all as necessary
+            const fetches = new Array(lastChunk - firstChunk + 1)
+            for (let chunk = firstChunk; chunk <= lastChunk; chunk += 1) {
+                fetches[chunk - firstChunk] = this._getChunk(chunk).then(data => ({
+                    data,
+                    chunkNumber: chunk,
+                }))
+            }
+
+            // stitch together the response buffer using them
+            const chunks = await Promise.all(fetches)
+            const chunksOffset = position - chunks[0].chunkNumber * this.chunkSize
+            chunks.forEach(({ data, chunkNumber }) => {
+                const chunkPositionStart = chunkNumber * this.chunkSize
+                let copyStart = 0
+                let copyEnd = this.chunkSize
+                let copyOffset =
+                    offset + (chunkNumber - firstChunk) * this.chunkSize - chunksOffset
+
+                if (chunkNumber === firstChunk) {
+                    copyOffset = offset
+                    copyStart = chunksOffset
+                }
+                if (chunkNumber === lastChunk) {
+                    copyEnd = position + length - chunkPositionStart
+                }
+
+                data.copy(outputBuffer, copyOffset, copyStart, copyEnd)
+            })
+        }
+
+        _getChunk(chunkNumber) {
+            const cachedPromise = this.lruCache.get(chunkNumber)
+            if (cachedPromise) return cachedPromise
+
+            const freshPromise = this.fetch(
+                chunkNumber * this.chunkSize,
+                this.chunkSize,
+            )
+            this.lruCache.set(chunkNumber, freshPromise)
+            return freshPromise
+        }
+    }
+
+
+// From https://github.com/sindresorhus/quick-lru
+// MIT License
+//
+// Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (sindresorhus.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//     The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    'use strict';
+
+    class QuickLRU {
+        constructor(options = {}) {
+            if (!(options.maxSize && options.maxSize > 0)) {
+                throw new TypeError('`maxSize` must be a number greater than 0');
+            }
+
+            this.maxSize = options.maxSize;
+            this.cache = new Map();
+            this.oldCache = new Map();
+            this._size = 0;
+        }
+
+        _set(key, value) {
+            this.cache.set(key, value);
+            this._size++;
+
+            if (this._size >= this.maxSize) {
+                this._size = 0;
+                this.oldCache = this.cache;
+                this.cache = new Map();
+            }
+        }
+
+        get(key) {
+            if (this.cache.has(key)) {
+                return this.cache.get(key);
+            }
+
+            if (this.oldCache.has(key)) {
+                const value = this.oldCache.get(key);
+                this._set(key, value);
+                return value;
+            }
+        }
+
+        set(key, value) {
+            if (this.cache.has(key)) {
+                this.cache.set(key, value);
+            } else {
+                this._set(key, value);
+            }
+
+            return this;
+        }
+
+        has(key) {
+            return this.cache.has(key) || this.oldCache.has(key);
+        }
+
+        peek(key) {
+            if (this.cache.has(key)) {
+                return this.cache.get(key);
+            }
+
+            if (this.oldCache.has(key)) {
+                return this.oldCache.get(key);
+            }
+        }
+
+        delete(key) {
+            const deleted = this.cache.delete(key);
+            if (deleted) {
+                this._size--;
+            }
+
+            return this.oldCache.delete(key) || deleted;
+        }
+
+        clear() {
+            this.cache.clear();
+            this.oldCache.clear();
+            this._size = 0;
+        }
+
+        * keys() {
+            for (const [key] of this) {
+                yield key;
+            }
+        }
+
+        * values() {
+            for (const [, value] of this) {
+                yield value;
+            }
+        }
+
+        * [Symbol.iterator]() {
+            for (const item of this.cache) {
+                yield item;
+            }
+
+            for (const item of this.oldCache) {
+                const [key] = item;
+                if (!this.cache.has(key)) {
+                    yield item;
+                }
+            }
+        }
+
+        get size() {
+            let oldCacheSize = 0;
+            for (const key of this.oldCache.keys()) {
+                if (!this.cache.has(key)) {
+                    oldCacheSize++;
+                }
+            }
+
+            return this._size + oldCacheSize;
+        }
+    }
 
     return igv;
 })
