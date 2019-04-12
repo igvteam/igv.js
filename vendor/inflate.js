ba4c2daaf769dd43b82a97f461f7b5d82fa98183
Jim Robinson <jrobinso@broadinstitute.org>
2019-04-11 23:32:59 -0700

    fixes #895

--- vendor/inflate.js
+++ vendor/inflate.js
@@ -2119,6 +2119,9 @@ function jszlib_inflate_buffer(buffer, start, length, afterUncOffset) {
         z.next_out_index = 0;
         z.avail_out = obuf.length;
         var status = z.inflate(Z_NO_FLUSH);
+        if (status == Z_BUF_ERROR) {
+            break;
+        }
         if (status != Z_OK && status != Z_STREAM_END) {
             throw z.msg;
         }
