b3e68ab4eb9444f5879bcb6e2e3983db9a07a8da
Douglass Turner <douglass.turner@gmail.com>
2019-08-27 15:49:33 -0400

    Edits to Support Webapp Use of Modularized igvjs (#959)
    * edits to support webapp
    
    * missing .js on import
    
    
    Former-commit-id: 656fa68e053fa9ff76a603799d151bf3f108e1ec

--- js/api.js
+++ js/api.js
@@ -1,13 +1,38 @@
 // Defines the top-level API for the igv module
 
 import {createBrowser, removeBrowser, visibilityChange, getBrowser} from './igv-create.js';
+import getDataWrapper from  "./feature/dataWrapper.js";
 import oauth from './oauth.js';
-
+import igvxhr from "./igvxhr.js";
+import GtexUtils from "./gtex/gtexUtils.js";
+import { createIcon } from "./igv-icons.js";
+import { guid } from "./util/domUtils.js";
+import { attachDialogCloseHandlerWithParent } from "./ui/ui-utils.js";
+import { getFilename, getExtension, isFilePath } from "./util/fileUtils";
+import { inferTrackTypes, inferFileFormat, knownFileExtensions } from './util/trackUtils.js';
 
 function setGoogleOauthToken(accessToken) {
     return oauth.setToken(accessToken);
 }
 
+const xhr = igvxhr;
 
-
-export {createBrowser, removeBrowser, visibilityChange, getBrowser, setGoogleOauthToken, oauth}
+export {
+    getFilename,
+    getExtension,
+    isFilePath,
+    inferFileFormat,
+    inferTrackTypes,
+    knownFileExtensions,
+    attachDialogCloseHandlerWithParent,
+    guid,
+    createIcon,
+    GtexUtils,
+    xhr,
+    getDataWrapper,
+    createBrowser,
+    removeBrowser,
+    visibilityChange,
+    getBrowser,
+    setGoogleOauthToken,
+    oauth };
