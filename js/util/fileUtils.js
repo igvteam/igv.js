656fa68e053fa9ff76a603799d151bf3f108e1ec
Douglass Turner <douglass.turner@gmail.com>
2019-08-27 15:49:33 -0400

    Edits to Support Webapp Use of Modularized igvjs (#959)
    * edits to support webapp
    
    * missing .js on import

--- js/util/fileUtils.js
+++ js/util/fileUtils.js
@@ -1,8 +1,38 @@
+
+
+function getExtension(config) {
+
+    if (undefined === config.url) {
+        return undefined;
+    }
+
+    let path = isFilePath(config.url) ? config.url.name : config.url;
+    let filename = path.toLowerCase();
+
+    //Strip parameters -- handle local files later
+    let index = filename.indexOf("?");
+    if (index > 0) {
+        filename = filename.substr(0, index);
+    }
+
+    //Strip aux extensions .gz, .tab, and .txt
+    if (filename.endsWith(".gz")) {
+        filename = filename.substr(0, filename.length - 3);
+    } else if (filename.endsWith(".txt") || filename.endsWith(".tab")) {
+        filename = filename.substr(0, filename.length - 4);
+    }
+
+    index = filename.lastIndexOf(".");
+
+    return index < 0 ? filename : filename.substr(1 + index);
+}
+
 /**
  * Return the filename from the path.   Example
  *   https://foo.com/bar.bed?param=2   => bar.bed
  * @param path
  */
+
 function getFilename (path) {
 
     var index, filename;
@@ -27,4 +57,4 @@ function isFilePath (path) {
     return (path instanceof File);
 }
 
-export {getFilename, isFilePath}
+export {getExtension, getFilename, isFilePath}
