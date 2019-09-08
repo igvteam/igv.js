65ad2b6cf09c2987f1b6fbbc68329047c6f250a2
Jim Robinson <jrobinso@broadinstitute.org>
2019-09-08 09:29:14 -0700

    update api with functions needed by juicebox
    Former-commit-id: 430b099e26cab2b9bae1817900bf0f642453f17e

--- js/api.js
+++ js/api.js
@@ -1,16 +1,28 @@
 // Defines the top-level API for the igv module
 
-import {createBrowser, removeBrowser, visibilityChange, getBrowser} from './igv-create.js';
-import getDataWrapper from  "./feature/dataWrapper.js";
+import {createBrowser, createTrack, getBrowser, removeBrowser, visibilityChange} from './igv-create.js';
+import getDataWrapper from "./feature/dataWrapper.js";
 import oauth from './oauth.js';
 import igvxhr from "./igvxhr.js";
 import GtexUtils from "./gtex/gtexUtils.js";
-import { createIcon } from "./igv-icons.js";
-import { guid } from "./util/domUtils.js";
-import { attachDialogCloseHandlerWithParent } from "./ui/ui-utils.js";
-import { getFilename, getExtension, isFilePath } from "./util/fileUtils.js";
-import { inferTrackTypes, inferFileFormat, knownFileExtensions } from './util/trackUtils.js';
+import {createIcon} from "./igv-icons.js";
+import {guid} from "./util/domUtils.js";
+import {attachDialogCloseHandlerWithParent, createColorSwatchSelector} from "./ui/ui-utils.js";
+import {getExtension, getFilename, isFilePath} from "./util/fileUtils.js";
+import {inferFileFormat, inferTrackTypes, knownFileExtensions} from './util/trackUtils.js';
+import {isString, numberFormatter, splitLines} from "./util/stringUtils.js";
+import {doAutoscale} from './util/igvUtils.js';
+import IGVColor from "./igv-color.js";
+import IGVGraphics from "./igv-canvas.js";
 import google from "./google/googleUtils.js";
+import TrackView from "./trackView.js";
+import GenericContainer from "./ui/genericContainer.js";
+import makeDraggable from "./ui/draggable.js";
+import Popover from "./ui/popover.js";
+import AlertDialog from "./ui/alertDialog.js";
+import InputDialog from "./ui/inputDialog.js";
+import TrackRemovalDialog from "./ui/trackRemovalDialog.js";
+import DataRangeDialog from "./ui/dataRangeDialog.js";
 
 function setGoogleOauthToken(accessToken) {
     return oauth.setToken(accessToken);
@@ -20,27 +32,51 @@ function setApiKey(apiKey) {
     return google.setApiKey(apiKey);
 }
 
+// for juicebox
+function appendRightHandGutter($parent) {
+    TrackView.prototype.appendRightHandGutter.call(this, $parent);
+}
+
 const xhr = igvxhr;
+const Color = IGVColor;
+const graphics = IGVGraphics;
 
 export {
     createBrowser,
     removeBrowser,
     visibilityChange,
-    getBrowser,
     setGoogleOauthToken,
     oauth,
+
     //exports below are not supported and will be removed over time
+    getBrowser,
+    doAutoscale,
+    graphics,
+    createTrack,
     getFilename,
     getExtension,
     isFilePath,
     inferFileFormat,
     inferTrackTypes,
     knownFileExtensions,
+    appendRightHandGutter,
     attachDialogCloseHandlerWithParent,
     guid,
     createIcon,
     GtexUtils,
     xhr,
+    Color,
+    splitLines,
+    isString,
+    numberFormatter,
     getDataWrapper,
-    setApiKey
+    setApiKey,
+    createColorSwatchSelector,
+    makeDraggable,
+    GenericContainer,
+    Popover,
+    AlertDialog,
+    InputDialog,
+    TrackRemovalDialog,
+    DataRangeDialog
 };
