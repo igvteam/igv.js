7e42ba78b71a11d5b044c703df81e4286baaa064
Jim Robinson <jrobinso@broadinstitute.org>
2019-09-11 12:42:25 -0700

    build changes * embed css prior to webpack * replace babel polyfill with regeneratorRuntime

--- wrapper/generateEmbedCss.js
+++ wrapper/generateEmbedCss.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 
-let ping = fs.readFileSync('../css/igv.css', 'utf-8');
+const cssPath = require.resolve('../css/igv.css')
+let ping = fs.readFileSync(cssPath, 'utf-8');
 ping = ping.replace(/\r\n/g, '\\n');
 ping = ping.replace(/\n/g, '\\n');
 ping = ping.replace(/"/g, '\\"');
-let foo = fs.readFileSync('./embedCssTemplate.js',  'utf-8');
+
+const templatePath = require.resolve('./embedCssTemplate.js')
+let foo = fs.readFileSync(templatePath,  'utf-8');
 foo = foo.replace('_CSS_', ping);
-fs.writeFileSync('../js/embedCss.js', foo,  'utf-8');
+
+const outputPath = require.resolve('../js/embedCss.js')
+fs.writeFileSync(outputPath, foo,  'utf-8');
