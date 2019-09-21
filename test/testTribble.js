918e6ded183d57b0cd506e2b8d391b420c96066f
Jim Robinson <jrobinso@broadinstitute.org>
2019-09-21 13:42:51 -0700

    eslint config, add unit test

---
+++ test/testTribble.js
@@ -0,0 +1,36 @@
+// Tests in this file require a server which supports range-byte requests, e.g. Apache.
+
+import loadTribbleIndex from "../js/feature/tribble.js";
+
+function runTribbleTests() {
+
+
+//mock object
+    const genome = {
+        getChromosome: function (chr) {
+        },
+        getChromosomeName: function (chr) {
+            return chr
+        }
+    }
+
+
+    QUnit.test("tribble", async function (assert) {
+
+        const done = assert.async();
+
+        const index = await loadTribbleIndex('https://data.broadinstitute.org/igvdata/t2d/recomb_decode.bedgraph', {}, genome);
+        assert.ok(index);
+
+        //var blocks = index.blocksForRange("chr1", 0, Number.MAX_VALUE);
+        //
+        //ok(blocks);
+
+        done();
+    });
+}
+
+export default runTribbleTests
+
+
+
