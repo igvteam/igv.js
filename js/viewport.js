8916074a5e0dde394ef067c8c9b7c455dd4c4a74
Douglass Turner <douglass.turner@gmail.com>
2019-09-25 18:48:17 -0400

    Bug fix. Save track to SVG.

--- js/viewport.js
+++ js/viewport.js
@@ -416,63 +416,6 @@ ViewPort.prototype.repaint = async function (tile) {
 
 };
 
-/**
- *
- * @param tile - the tile is created whenever features are loaded.  It contains the genomic state
- * representing the features,as well as the features.  The object evolved, at one time it was an image tile.
- * Should be renamed.
- */
-ViewPort.prototype.toSVG = async function (tile) {
-
-    const genomicState = this.genomicState;
-    const referenceFrame = genomicState.referenceFrame;
-    const bpPerPixel = tile.bpPerPixel;
-    const features = tile.features;
-    const pixelWidth = this.$viewport.width();
-    const pixelHeight = this.$viewport.height();
-    const bpStart = referenceFrame.start;
-    const bpEnd = referenceFrame.start + pixelWidth * referenceFrame.bpPerPixel;
-
-    const ctx = new C2S(
-        {
-            // svg
-            width: pixelWidth,
-            height: pixelHeight,
-            viewbox:
-                {
-                    x: 0,
-                    y: -$(this.contentDiv).position().top,
-                    width: pixelWidth,
-                    height: pixelHeight
-                }
-
-        });
-
-    const drawConfiguration =
-        {
-            viewport: this,
-            context: ctx,
-            top: -$(this.contentDiv).position().top,
-            pixelTop: 0,   // for compatibility with canvas draw
-            pixelWidth: pixelWidth,
-            pixelHeight: pixelHeight,
-            bpStart: bpStart,
-            bpEnd: bpEnd,
-            bpPerPixel: bpPerPixel,
-            referenceFrame: referenceFrame,
-            genomicState: this.genomicState,
-            selection: this.selection,
-            viewportWidth: pixelWidth,
-            viewportContainerX: 0,
-            viewportContainerWidth: this.browser.viewportContainerWidth()
-        };
-
-    await draw.call(this, drawConfiguration, features);
-
-    return ctx.getSerializedSvg(true);
-
-};
-
 async function draw(drawConfiguration, features) {
 
     if (features) {
@@ -496,7 +439,6 @@ async function draw(drawConfiguration, features) {
         }
     }
 }
-
 
 function viewIsReady() {
     return this.browser && this.browser.genomicStateList && this.genomicState.referenceFrame;
@@ -673,13 +615,81 @@ ViewPort.prototype.renderSVGContext = async function (context, offset) {
 };
 
 ViewPort.prototype.saveSVG = function () {
-    const str = this.$trackLabel ? this.$trackLabel.text() : this.trackView.track.id;
-    const filename = str + ".svg";
-    const svg = this.toSVG(this.tile);
-    const data = URL.createObjectURL(new Blob([svg], {
-        type: "application/octet-stream"
-    }));
-    download(filename, data);
+
+    const width = this.$viewport.width();
+    const height = this.$viewport.height();
+
+    const context = new C2S(
+        {
+            width,
+            height,
+            viewbox:
+                {
+                    x: 0,
+                    y: -$(this.contentDiv).position().top,
+                    width,
+                    height
+                }
+
+        });
+
+    // const bpPerPixel = this.tile.bpPerPixel;
+    const { start, bpPerPixel } = this.genomicState.referenceFrame;
+
+    const drawConfiguration =
+        {
+            viewport: this,
+            context,
+            top: -$(this.contentDiv).position().top,
+            pixelTop: 0,
+            pixelWidth: width,
+            pixelHeight: height,
+            bpStart: start,
+            bpEnd: start + (width * bpPerPixel),
+            bpPerPixel,
+            referenceFrame: this.genomicState.referenceFrame,
+            genomicState: this.genomicState,
+            selection: this.selection,
+            viewportWidth: width,
+            viewportContainerX: 0,
+            viewportContainerWidth: this.browser.viewportContainerWidth()
+        };
+
+    (async (config) => {
+
+        try {
+            await draw.call(this, config, this.tile.features);
+        } catch (e) {
+            console.warn(e.message)
+        }
+
+
+        if (this.$trackLabel && true === this.browser.trackLabelsVisible) {
+
+            const shim = 4;
+            const {x, y, width, height} = relativeDOMBBox(this.$viewport.get(0), this.$trackLabel.get(0));
+
+            context.font = "12px Arial";
+            context.fillStyle = 'rgb(68, 68, 68)';
+            context.fillText(this.$trackLabel.text(), x, y + height - shim);
+
+            const {width: stringWidth} = context.measureText(this.$trackLabel.text());
+
+            context.strokeStyle = 'rgb(68, 68, 68)';
+            context.strokeRect(x - shim, y, stringWidth + (2 * shim), height);
+
+        }
+
+        const svg = config.context.getSerializedSvg(true);
+
+        const data = URL.createObjectURL(new Blob([ svg ], { type: "application/octet-stream" }));
+
+        const str = this.$trackLabel ? this.$trackLabel.text() : this.trackView.track.id;
+        const filename = str + ".svg";
+        download(filename, data);
+
+    })(drawConfiguration);
+
 };
 
 /**
