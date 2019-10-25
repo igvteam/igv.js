e926988b2f61182e52694d6b3989a50718defe09
Jim Robinson <jrobinso@broadinstitute.org>
2019-10-25 10:32:02 -0700

    Deepcoverage (#1007)
    Better support for deep coverage bam fles.  See #1006 
    
    * bgunzip in chunks
    * Remove unneccessary allocation of UInt8Array from bg-unzip
    
    
    Former-commit-id: 6da35d6daa524c6e31b9c19d23fb1b8f211cfe9b

--- js/feature/featureFileReader.js
+++ js/feature/featureFileReader.js
@@ -239,7 +239,7 @@ FeatureFileReader.prototype.loadFeaturesWithIndex = async function (chr, start, 
 
             if (tabix) {
                 const data = await igvxhr.loadArrayBuffer(config.url, options);
-                const inflated = new Uint8Array(unbgzf(data));
+                const inflated = unbgzf(data);
                 parse(inflated);
 
             } else {
