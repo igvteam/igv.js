3d84ea447bba45c55ae52199e11537caa45ff72b
Jim Robinson <jrobinso@broadinstitute.org>
2019-10-28 10:44:51 -0700

    Update version.js

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
-const _version = "2.3.2 (6da35d6daa524c6e31b9c19d23fb1b8f211cfe9b)"
+const _version = "2.3.2 (d336d4caff62df1409a8218c3a36da1128abf685)"
 
 function version() {
     return _version;
