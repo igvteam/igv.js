f69833d5904840ce40e98399e727927693bed7a0
bw2 <weisburd@broadinstitute.org>
2019-11-12 04:40:07 -0500

    version file

--- js/version.js
+++ js/version.js
@@ -1,10 +1,29 @@
-const _version = "2.3.2 (d336d4caff62df1409a8218c3a36da1128abf685)"
+const _version = "2.3.5 (848c16d4c9b25df10e12ab50ac9dd06945c59dea)"
 
 function version() {
     return _version;
 }
 
 export default version
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
