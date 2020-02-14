21654921c1ceaaef88626f0c52a846ca929b53e0
Jim Robinson <jrobinso@broadinstitute.org>
2020-02-14 09:21:59 -0800

    Allow setting of endpoint for htsget alignment api.  See #1061

--- test/testHtsgetReader.js
+++ test/testHtsgetReader.js
@@ -9,44 +9,39 @@ function runHtsgetTests() {
         }
     }
 
-    // eweitz 2018-09-06: Disabling for now due to test failure
-    // TODO: Investigate failure, re-enable.
-    // QUnit.test('Load Urls - DNANexus', function (assert) {
-    //     var done = assert.async();
-
-    //     const url = 'http://htsnexus.rnd.dnanex.us/v1',
-    //         id = 'BroadHiSeqX_b37/NA12878',
-    //         chr = 'chr1',
-    //         s = 10000,
-    //         end = 10100;
-
-    //     const reader = new igv.HtsgetReader({endpoint: url, id: id}, genome);
-
-
-    //     reader.readAlignments(chr, s, end)
-
-    //         .then(function (alignmentContainer) {
-    //             assert.ok(alignmentContainer);
-    //             assert.ok(alignmentContainer.alignments.length > 0);
-    //             done();
-    //         })
-    //         .catch(function (error) {
-    //             console.log(error);
-    //             assert.ok(false);
-    //             done();
-    //         });
-    // });
-
     QUnit.test('Load Urls - EBI', function (assert) {
+
+        var done = assert.async();
+        var id = 'genomics-public-data/platinum-genomes/bam/NA12877_S1.bam',
+            chr = 'chr1',
+            s = 10000,
+            end = 10100;
+
+        const trackConfig = {
+            url: 'http://35.196.212.220',
+            endpoint: '/reads/',
+            id: id
+        }
+
+        const reader = new HtsgetReader(trackConfig);
+        reader.readAlignments(chr, s, end).then(function (alignmentContainer) {
+
+            assert.ok(alignmentContainer);
+            assert.ok(alignmentContainer.alignments.length > 0);
+            done();
+        });
+    });
+
+    QUnit.test('Load Urls - EBI - Legacy convention', function (assert) {
         var done = assert.async();
 
-        var url = 'http://35.196.212.220',
+        var endpoint = 'http://35.196.212.220',
             id = 'genomics-public-data/platinum-genomes/bam/NA12877_S1.bam',
             chr = 'chr1',
             s = 10000,
             end = 10100;
 
-        var reader = new HtsgetReader({endpoint: url, id: id});
+        var reader = new HtsgetReader({endpoint: endpoint, id: id});
         reader.readAlignments(chr, s, end).then(function (alignmentContainer) {
 
             assert.ok(alignmentContainer);
