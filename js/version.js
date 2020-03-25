7033b76e48c111bdd88e0bf4da2337cf01755c46
Jim Robinson <jrobinso@broadinstitute.org>
2020-03-24 20:02:09 -0700

    update version

--- js/version.js
+++ js/version.js
@@ -1,10 +1,11 @@
-const _version = "2.4.0 (87757c0cc9e7b22fdbafafb7aa29764ebbbd663b)"
+const _version = "2.4.1 (b4ec82114394289cda84f0cc0042c127c8b7b108)"
 
 function version() {
     return _version;
 }
 
 export default version
+
 
 
 
