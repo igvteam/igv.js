d7e8e2243054245b411b0de79bd1ab50db861c80
Jim Robinson <jrobinso@broadinstitute.org>
2020-04-04 19:17:18 -0700

    allow url to be a function for custom web services.  Function is of the form ``` functinon({chr, start, end}) {    return <urlToWebService> } ``` see issue #1086
    Former-commit-id: 5c093799cc028a60f224353fa966eabc7b982163

--- scripts/updateVersion.js
+++ scripts/updateVersion.js
@@ -6,19 +6,19 @@ const version = JSON.parse(jsonText).version;
 
 
 // Try to get git commit hash
-let githash = "";
-try {
-    const headPath = require.resolve('../.git/HEAD');
-    const rev = fs.readFileSync(headPath).toString().trim().split(/.*[: ]/).slice(-1)[0];
-    if (rev.indexOf('/') === -1) {
-        githash = rev;
-    } else {
-        const revpath = require.resolve('../.git/' + rev);
-        githash = fs.readFileSync(revpath).toString().trim();
-    }
-} catch (e) {
-    console.error("Error determining git hash")
-}
+// let githash = "";
+// try {
+//     const headPath = require.resolve('../.git/HEAD');
+//     const rev = fs.readFileSync(headPath).toString().trim().split(/.*[: ]/).slice(-1)[0];
+//     if (rev.indexOf('/') === -1) {
+//         githash = rev;
+//     } else {
+//         const revpath = require.resolve('../.git/' + rev);
+//         githash = fs.readFileSync(revpath).toString().trim();
+//     }
+// } catch (e) {
+//     console.error("Error determining git hash")
+// }
 
 
 const versionJS = require.resolve('../js/version.js')
@@ -27,6 +27,7 @@ const lines = ping.split(/\r?\n/);
 let foundVersionLine = false;
 var fd = fs.openSync(versionJS, 'w');
 for (let line of lines) {
+    if(!line) continue;
     if(line.startsWith("const _version")) {
         //fs.writeSync(fd, `const _version = "${version} (${githash})"\n`, null, 'utf-8')
         fs.writeSync(fd, `const _version = "${version}"\n`, null, 'utf-8')
