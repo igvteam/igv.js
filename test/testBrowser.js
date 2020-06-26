ff117c530c43da17efecc3a33f1c277810c5fec6
Eric Weitz <eweitz@broadinstitute.org>
2020-06-26 16:53:02 -0400

    Fix indentation
    Former-commit-id: a6e980dc4246e6799574e1fac480ea398e320113

--- test/testBrowser.js
+++ test/testBrowser.js
@@ -87,45 +87,45 @@ function runBrowserTests() {
     })
 
     QUnit.test("Test custom Ensembl search backend", function (assert) {
-      var done = assert.async();
-      // The Ensembl REST API for symbol lookup (https://rest.ensembl.org/documentation/info/symbol_lookup)
-      // enables searching genes for a wider array of organisms than those supported by default
-      // in https://igv.org/genomes/locus.php.
-      //
-      // This tests handling for the Ensembl lookup API as a custom igv.js search backend.
-      //
-      // The example below uses a human reference, to avoid complexity around also mocking support
-      // for custom reference genomes.  The search itself uses  crab-eating macaque (Macaca
-      // fascicularis) -- the original motivation to support a custom Ensembl search backend.
-      // In real-world examples, the `genome` would use a custom reference for Macaca fascicularis.
+        var done = assert.async();
+        // The Ensembl REST API for symbol lookup (https://rest.ensembl.org/documentation/info/symbol_lookup)
+        // enables searching genes for a wider array of organisms than those supported by default
+        // in https://igv.org/genomes/locus.php.
+        //
+        // This tests handling for the Ensembl lookup API as a custom igv.js search backend.
+        //
+        // The example below uses a human reference, to avoid complexity around also mocking support
+        // for custom reference genomes.  The search itself uses  crab-eating macaque (Macaca
+        // fascicularis) -- the original motivation to support a custom Ensembl search backend.
+        // In real-world examples, the `genome` would use a custom reference for Macaca fascicularis.
 
-      createBrowser(div, {
-          genome: "hg19",
-          locus: "myc",
-          search: {
-              url: 'https://rest.ensembl.org/lookup/symbol/macaca_fascicularis/$FEATURE$?content-type=application/json',
-              chromosomeField: 'seq_region_name',
-              displayName: 'display_name'
-          }
-      })
-      .then(function (browser) {
+        createBrowser(div, {
+            genome: "hg19",
+            locus: "myc",
+            search: {
+                url: 'https://rest.ensembl.org/lookup/symbol/macaca_fascicularis/$FEATURE$?content-type=application/json',
+                chromosomeField: 'seq_region_name',
+                displayName: 'display_name'
+            }
+        })
+        .then(function (browser) {
 
-          browser.search('BRCA2').then(function (genomicState) {
-              // console.log('genomicState')
-              // console.log(JSON.stringify(genomicState))
+            browser.search('BRCA2').then(function (genomicState) {
+                // console.log('genomicState')
+                // console.log(JSON.stringify(genomicState))
 
-              assert.equal(genomicState[0].chromosome.name, 'chr17');
-              assert.equal(genomicState[0].start, 11863385);
-              assert.equal(genomicState[0].end, 11957305);
-              done();
-          })
-      })
-      .catch(function (error) {
-          console.error(error);
-          assert.ok(false);
-          done();
-      })
-      });
+                assert.equal(genomicState[0].chromosome.name, 'chr17');
+                assert.equal(genomicState[0].start, 11863385);
+                assert.equal(genomicState[0].end, 11957305);
+                done();
+            })
+        })
+        .catch(function (error) {
+            console.error(error);
+            assert.ok(false);
+            done();
+        })
+    })
 }
 
 export default runBrowserTests;
