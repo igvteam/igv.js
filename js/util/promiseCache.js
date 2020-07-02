c263a5cae5b78b4f6ccaea6020602bb5dbd8e581
Jim Robinson <jrobinso@broadinstitute.org>
2020-07-01 20:52:55 -0700

    promise cachce object (experimental)
    Former-commit-id: b865cf8112f71c88209e6148483ad3dc4977e0b2

---
+++ js/util/promiseCache.js
@@ -0,0 +1,35 @@
+
+
+class PromiseCache {
+
+    constructor() {
+        this.map = new Map();
+    }
+
+    getPromise(track, chr, bpStart, bpEnd, bpPerPixel, viewport) {
+
+        let trackMap = this.map.get(track);
+        if(!trackMap) {
+            trackMap = new Map();
+            this.map.set(track, trackMap);
+        }
+
+        if(trackMap.size > 0) {
+            console.log();
+        }
+
+        const windowFunction = track.windowFunction;
+        const key = `${chr}_${bpStart}_${bpEnd}_${bpPerPixel}_${windowFunction}`;
+        if(trackMap.has(key)) {
+            return trackMap.get(key);
+        } else {
+            const p = track.getFeatures(chr, bpStart, bpEnd, bpPerPixel, viewport);
+            trackMap.set(key, p);
+            return p;
+        }
+    }
+
+}
+
+
+export default PromiseCache
