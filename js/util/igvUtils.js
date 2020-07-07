c9f58d34d827008e63f1c97d683af0574a497eda
Jim Robinson <jrobinso@broadinstitute.org>
2020-07-06 21:52:07 -0700

    Support proportional (to value) arcs for bedpe files.  See issue #1114 Still needed   * updated menus   * hit testing and popup text
    Former-commit-id: a6fbd46852b4821e88db25d84560888ded6dc9eb

--- js/util/igvUtils.js
+++ js/util/igvUtils.js
@@ -148,5 +148,24 @@ const validateLocusExtent = function (chromosomeLengthBP, extent, minimumBP) {
     extent.end = Math.floor(ee);
 };
 
-export {extend, isSimpleType, buildOptions, download, validateLocusExtent, doAutoscale}
+
+
+/*!
+ * is-number <https://github.com/jonschlinkert/is-number>
+ *
+ * Copyright (c) 2014-present, Jon Schlinkert.
+ * Released under the MIT License.
+ */
+
+const isNumber = function(num) {
+    if (typeof num === 'number') {
+        return num - num === 0;
+    }
+    if (typeof num === 'string' && num.trim() !== '') {
+        return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
+    }
+    return false;
+};
+
+export {extend, isSimpleType, buildOptions, download, validateLocusExtent, doAutoscale, isNumber}
 
