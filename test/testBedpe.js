c67503bb2cc1af7d1b28ea5b3bfa963bb47e843e
Jim Robinson <jrobinso@broadinstitute.org>
2020-07-08 14:55:24 -0700

    "interact" file support * support plain text UCSC interact file * add "interact" as synonym for "interaction" track type for UCSC compatibility * add "useScore" for UCSC compatibility * add "showBlocks" option

--- test/testBedpe.js
+++ test/testBedpe.js
@@ -51,7 +51,6 @@ function runBedpeTests() {
     })
 
     QUnit.test("Multiple header lines -- column 8 score", async function (assert) {
-
         const chr = "chr1";
         const bpStart = 1;
         const bpEnd = Number.MAX_SAFE_INTEGER;
@@ -69,7 +68,45 @@ function runBedpeTests() {
             assert.ok(f.name);
             assert.ok(!isNaN(f.value));
         }
+    })
 
+    QUnit.test("10X SV", async function (assert) {
+        const chr = "chr1";
+        const bpStart = 1;
+        const bpEnd = Number.MAX_SAFE_INTEGER;
+        const featureSource = new FeatureSource({
+                url: './data/bedpe/sv_calls.10X.bedpe',
+                format: 'bedpe'
+            },
+            genome);
+
+        const features = await featureSource.getFeatures(chr, bpStart, bpEnd);
+        assert.ok(features);
+        assert.equal(features.length,6)
+        for(let f of features) {
+            assert.ok(f.name);
+            assert.ok(f.value > 0);
+        }
+    })
+
+    QUnit.test("interact example 1", async function (assert) {
+        const chr = "chr12";
+        const bpStart = 1;
+        const bpEnd = Number.MAX_SAFE_INTEGER;
+        const featureSource = new FeatureSource({
+                url: './data/bedpe/interactExample1.txt',
+                format: 'interact'
+            },
+            genome);
+
+        const features = await featureSource.getFeatures(chr, bpStart, bpEnd);
+        assert.ok(features);
+        assert.equal(features.length,4)
+        for(let f of features) {
+            assert.ok(f.name);
+            assert.equal(f.score, 0);
+            assert.ok(f.value > 0);
+        }
     })
 }
 
