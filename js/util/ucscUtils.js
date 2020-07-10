402f87a0aaeaba998d01dde8c7d3f5d8263ff64a
Jim Robinson <jrobinso@broadinstitute.org>
2020-07-09 22:47:16 -0700

    Add support for autoSql in bigBed files.
    Former-commit-id: 1d8eee830c3845869186a1b3658c3d7af7ddcff0

--- js/util/ucscUtils.js
+++ js/util/ucscUtils.js
@@ -1,7 +1,38 @@
-
-
-function scoreShade(score) {
-      return Math.max(0.1, score / 1000).toString();
+function scoreShade(score, color) {
+    const alpha = Math.min(1, 0.11 + 0.89 * (score / 779));
+    return alpha.toString();
 }
 
-export {scoreShade}
+function parseAutoSQL(str) {
+
+    let table;
+    const fields = [];
+    let startDecoding = false;
+    const lines = str.trim().split(/\s*[\r\n]+\s*/g);
+    for (let line of lines) {
+        if (line.startsWith('table')) {
+            table = line.split(/\s+/)[1].trim();
+        } else if (line.startsWith('(')) {
+            startDecoding = true;
+        } else if (line.startsWith(')')) {
+        } else if (startDecoding) {
+            if (line.length > 0) {
+                const idx = line.indexOf(';');
+                const tokens = line.substr(0, idx).split(/\s+/);
+                const description = line.substr(idx + 1).replace(/"/g, '').trim();
+                fields.push({
+                    type: tokens[0],
+                    name: tokens[1],
+                    description: description
+                })
+            }
+        }
+    }
+    return {
+        table: table,
+        fields: fields
+    }
+}
+
+
+export {scoreShade, parseAutoSQL}
---
+++ test/testBigbed.js
@@ -0,0 +1,74 @@
+import BWSource from "../js/bigwig/bwSource.js";
+import {parseAutoSQL} from "../js/util/ucscUtils"
+
+function runBigbedTests() {
+
+    QUnit.test("bed9+2 features", async function (assert) {
+        var url = "./data/bb/myBigBed2.bb",
+            chr = "chr7",
+            bpStart = 0,
+            bpEnd = Number.MAX_SAFE_INTEGER;
+        var bWSource = new BWSource({url: url});
+        const features = await bWSource.getFeatures(chr, bpStart, bpEnd, 1);
+        assert.ok(features);
+        assert.equal(features.length, 3339);   // Verified in iPad app
+
+        //chr7	773975	792642	uc003sjb.2	0	+	776710	791816	0,255,0	HEATR2	Q86Y56-3
+        const f = features[20];
+        assert.equal(f.start, 773975);
+        assert.equal(f.geneSymbol, 'HEATR2');
+        assert.equal(f.spID, 'Q86Y56-3')
+    });
+
+    QUnit.test("interact features", async function (assert) {
+        var url = "./data/bb/interactExample3.inter.bb",
+            chr = "chr3",
+            bpStart = 63820967,
+            bpEnd = 63880091;
+
+        var bWSource = new BWSource({url: url});
+        const features = await bWSource.getFeatures(chr, bpStart, bpEnd, 1);
+        assert.ok(features);
+        assert.equal(features.length, 17);   // Verified in iPad app
+    });
+
+
+    QUnit.test("Autosql", function (assert) {
+        const autosql = `
+table chromatinInteract
+"Chromatin interaction between two regions"
+    (
+    string chrom;      "Chromosome (or contig, scaffold, etc.). For interchromosomal, use 2 records"
+    uint chromStart;   "Start position of lower region. For interchromosomal, set to chromStart of this region"
+    uint chromEnd;     "End position of upper region. For interchromosomal, set to chromEnd of this region"
+    string name;       "Name of item, for display"
+    uint score;        "Score from 0-1000"
+    double value;      "Strength of interaction or other data value. Typically basis for score"
+    string exp;        "Experiment name (metadata for filtering). Use . if not applicable"
+    string color;      "Item color.  Specified as r,g,b or hexadecimal #RRGGBB or html color name, as in //www.w3.org/TR/css3-color/#html4."
+    string region1Chrom;  "Chromosome of lower region. For non-directional interchromosomal, chrom of this region."
+    uint region1Start;  "Start position of lower/this region"
+    uint region1End;    "End position in chromosome of lower/this region"
+    string region1Name;  "Identifier of lower/this region"
+    string region1Strand; "Orientation of lower/this region: + or -.  Use . if not applicable"
+    string region2Chrom; "Chromosome of upper region. For non-directional interchromosomal, chrom of other region"
+    uint region2Start;  "Start position in chromosome of upper/this region"
+    uint region2End;    "End position in chromosome of upper/this region"
+    string region2Name; "Identifier of upper/this region"
+    string region2Strand; "Orientation of upper/this region: + or -.  Use . if not applicable"
+    )
+`
+
+        const autosqlObject = parseAutoSQL(autosql);
+        assert.ok(autosqlObject);
+        assert.equal('chromatinInteract', autosqlObject.table);
+        assert.equal(autosqlObject.fields.length, 18);
+
+        const r2s = autosqlObject.fields[17];
+        assert.equal(r2s.type, 'string');
+        assert.equal(r2s.name, 'region2Strand');
+        assert.equal(r2s.description, 'Orientation of upper/this region: + or -.  Use . if not applicable');
+    })
+}
+
+export default runBigbedTests;
