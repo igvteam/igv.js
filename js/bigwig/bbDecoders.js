2ea00ab1672b5d5f65028abd88531269097e3c53
Jim Robinson <jrobinso@broadinstitute.org>
2020-07-10 16:08:29 -0700

    Support for "bigInteract" Support for bigBed autosql Updated examples See issue #1134
    Former-commit-id: 34407efa49b544d254aabc0b8fe0ed76cfc501be

--- js/bigwig/bbDecoders.js
+++ js/bigwig/bbDecoders.js
@@ -29,55 +29,87 @@ function getDecoder(definedFieldCount, fieldCount, as) {
     if (as) {
         autoSql = parseAutoSQL(as);
     }
-    const standardFieldCount = definedFieldCount - 3;
 
-    return function(feature, tokens) {
+    if (autoSql && 'chromatinInteract' === autoSql.table) {
+        return decodeInteract;
+    } else {
+        const standardFieldCount = definedFieldCount - 3;
+        return function (feature, tokens) {
 
-        if (standardFieldCount > 0) {
-            feature.name = tokens[0];
-        }
-        if (standardFieldCount > 1) {
-            feature.score = parseFloat(tokens[1]);
-        }
-        if (standardFieldCount > 2) {
-            feature.strand = tokens[2];
-        }
-        if (standardFieldCount > 3) {
-            feature.cdStart = parseInt(tokens[3]);
-        }
-        if (standardFieldCount > 4) {
-            feature.cdEnd = parseInt(tokens[4]);
-        }
-        if (standardFieldCount > 5) {
-            if (tokens[5] !== "." && tokens[5] !== "0" && tokens[5] !== "-1") {
-                const c = IGVColor.createColorString(tokens[5]);
-                feature.color = c.startsWith("rgb") ? c : undefined;
+            if (standardFieldCount > 0) {
+                feature.name = tokens[0];
             }
-        }
-        if (standardFieldCount > 8) {
-            const exonCount = parseInt(tokens[6]);
-            const exonSizes = tokens[7].split(',');
-            const exonStarts = tokens[8].split(',');
-            const exons = [];
-            for (let i = 0; i < exonCount; i++) {
-                const eStart = feature.start + parseInt(exonStarts[i]);
-                const eEnd = eStart + parseInt(exonSizes[i]);
-                exons.push({start: eStart, end: eEnd});
+            if (standardFieldCount > 1) {
+                feature.score = parseFloat(tokens[1]);
             }
-            feature.exons = exons;
-        }
+            if (standardFieldCount > 2) {
+                feature.strand = tokens[2];
+            }
+            if (standardFieldCount > 3) {
+                feature.cdStart = parseInt(tokens[3]);
+            }
+            if (standardFieldCount > 4) {
+                feature.cdEnd = parseInt(tokens[4]);
+            }
+            if (standardFieldCount > 5) {
+                if (tokens[5] !== "." && tokens[5] !== "0" && tokens[5] !== "-1") {
+                    const c = IGVColor.createColorString(tokens[5]);
+                    feature.color = c.startsWith("rgb") ? c : undefined;
+                }
+            }
+            if (standardFieldCount > 8) {
+                const exonCount = parseInt(tokens[6]);
+                const exonSizes = tokens[7].split(',');
+                const exonStarts = tokens[8].split(',');
+                const exons = [];
+                for (let i = 0; i < exonCount; i++) {
+                    const eStart = feature.start + parseInt(exonStarts[i]);
+                    const eEnd = eStart + parseInt(exonSizes[i]);
+                    exons.push({start: eStart, end: eEnd});
+                }
+                feature.exons = exons;
+            }
 
-        if (autoSql) {
-            // TODO -- these should be equal, validate?  fieldCount-definedFieldCount, as.fields.length, tokens.length-3
-            const extraStart = definedFieldCount;
-            for(let i = extraStart; i < fieldCount; i++) {
-                if(i < autoSql.fields.length) {
-                    const name = autoSql.fields[i].name;
-                    const value = tokens[i - 3];
-                    feature[name] = value;
+            if (autoSql) {
+                // TODO -- these should be equal, validate?  fieldCount-definedFieldCount, as.fields.length, tokens.length-3
+                const extraStart = definedFieldCount;
+                for (let i = extraStart; i < fieldCount; i++) {
+                    if (i < autoSql.fields.length) {
+                        const name = autoSql.fields[i].name;
+                        const value = tokens[i - 3];
+                        feature[name] = value;
+                    }
                 }
             }
         }
+    }
+
+    function decodeInteract(feature, tokens) {
+
+        feature.chr1 = tokens[5];
+        feature.start1 = Number.parseInt(tokens[6]);
+        feature.end1 = Number.parseInt(tokens[7]);
+
+        feature.chr2 = tokens[10];
+        feature.start2 = Number.parseInt(tokens[11]);
+        feature.end2 = Number.parseInt(tokens[12]);
+
+        feature.name = tokens[0];
+        feature.score = Number.parseFloat(tokens[1]);
+        feature.value = Number.parseFloat(tokens[2]);
+        feature.color = tokens[4] === '.' ? undefined : tokens[4] === "0" ? "rgb(0,0,0)" : tokens[4];
+
+        feature.interchr = feature.chr1 !== feature.chr2;
+
+        // Midpoints
+        if(feature.chr1 === feature.chr2) {
+            let m1 = (feature.start1 + feature.end1) / 2;
+            let m2 = (feature.start2 + feature.end2) / 2;
+            feature.m1 = (m1 < m2) ? m1 : m2;
+            feature.m2 = (m1 < m2) ? m2 : m1;
+        }
+
+        return feature;
     }
 }
 
