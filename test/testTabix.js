308bf7ba119ca16261fa90cee139a2f0fcecbab0
Jim Robinson <jrobinso@broadinstitute.org>
2020-07-29 17:10:13 -0700

    support "CSI" tabix index -- bam CSI not supported yet see issue #435

--- test/testTabix.js
+++ test/testTabix.js
@@ -1,6 +1,8 @@
+import loadCsiIndex from "../js/bam/csiIndex.js";
 import loadBamIndex from "../js/bam/bamIndex.js";
 import {unbgzf} from "../js/bam/bgzf.js";
 import igvxhr from "../js/igvxhr.js";
+import FeatureFileReader from "../js/feature/featureFileReader.js";
 
 function runTabixTests() {
 
@@ -37,6 +39,48 @@ function runTabixTests() {
         assert.ok(result)
         done()
     })
+
+    QUnit.test("CSI", async function (assert) {
+
+        var refID = 0,
+            beg = 1226000,
+            end = 1227000,
+            indexPath = "data/tabix/csi-test.vcf.gz.csi",
+            config = {};
+
+        const csiIndex = await loadCsiIndex(indexPath, config, true);
+        assert.ok(csiIndex);
+
+        const blocks = csiIndex.blocksForRange(refID, beg, end);
+        assert.ok(blocks.length > 0)
+
+    });
+
+    QUnit.test("CSI query", async function (assert) {
+
+        var chr = "chr1",
+            beg = 1226000,
+            end = 1227000;
+
+        const reader = new FeatureFileReader({
+            format: "vcf",
+            url: "data/tabix/csi-test.vcf.gz",
+            indexURL:"data/tabix/csi-test.vcf.gz.csi"
+        });
+        await reader.readHeader();
+        const features = await reader.readFeatures(chr, beg, end);
+        assert.ok(features);
+
+        // Verify features are in query range.  The first feature before and after query range are returned by design
+        const len = features.length;
+        for(let i=1; i<len-1; i++) {
+            const f = features[i];
+            assert.ok(f.chr === chr && f.end >= beg && f.start <= end);
+        }
+
+    });
+
+
 
 }
 
