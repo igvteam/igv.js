9cc5f8b67d8818571975923dfb295b4f0300092c
Jim Robinson <jrobinso@broadinstitute.org>
2020-09-13 23:38:28 -0700

    oAuth changes
    Former-commit-id: 1c91ad3249a0d348e1972ed54976cee7f27e9c70

--- js/oauth.js
+++ js/oauth.js
@@ -1,27 +1,8 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2014-2015 Broad Institute
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+// Support for oauth token based authorization
+// This class supports explicit setting of an oauth token either globally or for specific hosts.
+//
+// The variable oauth.google.access_token, which becomes igv.oauth.google.access_token on ES5 conversion is
+// supported for backward compatibility
 
 
 const oauth = {
@@ -29,19 +10,15 @@ const oauth = {
     oauthTokens: {},
 
     setToken: function (token, host) {
-
         if (!host) {
             this.google.access_token = token;
         } else {
             this.oauthTokens[host] = token;
         }
-
     },
 
     getToken: function (host) {
-
         let token;
-
         if (!host) {
             token = this.google.access_token;
         } else {
@@ -53,18 +30,14 @@ const oauth = {
                 }
             }
         }
-
         return token;
-
     },
 
     removeToken: function (host) {
-
         if (!host) {
             delete oauth.google["access_token"];
         } else {
             delete this.oauthTokens[host];
-
         }
     },
 
