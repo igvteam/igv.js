999cb2f581d591a48b1a93397dc263f4069ea55e
Jim Robinson <jrobinso@broadinstitute.org>
2020-09-22 09:53:22 -0700

    Test for VCF file with no variants.   See issue #1148
    Former-commit-id: d5a5c09d51a4ad5fef379e0566fab714bf81849e

--- test/testVariant.js
+++ test/testVariant.js
@@ -1,4 +1,5 @@
 import VcfParser from "../js/variant/vcfParser.js"
+import FeatureFileReader from "../js/feature/featureFileReader.js"
 import igvxhr from "../js/igvxhr.js"
 import {assert} from 'chai';
 import {createMockObjects} from "@igvteam/test-utils/src"
@@ -15,7 +16,7 @@ suite("testVariant", function () {
         const header = await parser.parseHeader(data);
         const variants = await parser.parseFeatures(data);
         assert.equal(variants.length, 4);
-        for(let v of variants) {
+        for (let v of variants) {
             assert.equal(v.type, "NONVARIANT");
         }
     })
@@ -27,9 +28,21 @@ suite("testVariant", function () {
         const header = await parser.parseHeader(data);
         const variants = await parser.parseFeatures(data);
         assert.equal(variants.length, 5);
-        for(let v of variants) {
+        for (let v of variants) {
             assert.equal(v.type, "MIXED");
         }
     })
+
+    test("No variants", async function () {
+        const reader = new FeatureFileReader({
+            format: "vcf",
+            url: require.resolve("./data/vcf/NoVariantsVCF/novariants.vcf.gz"),
+            indexURL: require.resolve("./data/vcf/NoVariantsVCF/novariants.vcf.gz.tbi")
+        })
+
+        const features = await reader.readFeatures("chr1", 1, 1000);
+        assert.equal(features.length, 0);
+    })
+
 })
 
