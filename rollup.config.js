0d35e795f167454e3c2eeebc2d64943878832ea9
Jim Robinson <jrobinso@broadinstitute.org>
2020-12-07 16:23:29 -0800

    further simplification
    Former-commit-id: 06c0d31053c011a37d9ea97ff8afe1a0ac098533

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,8 @@
-import strip from 'rollup-plugin-strip';
-
 export default [
-
     {
         input: 'js/index.js',
         output: [
             {file: 'dist/igv.esm.js', format: 'es'},
-        ],
-        plugins: [
-            strip({
-                debugger: true,
-                functions: ['console.log', 'assert.*', 'debug']
-            })
         ]
     }
 ];
