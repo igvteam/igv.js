2f88e82a2c20d62def7b601b4b05587f8c420a7f
Douglass Turner <douglass.turner@gmail.com>
2021-03-01 10:41:18 -0500

    Hover div placement

--- js/sampleNameViewport.js
+++ js/sampleNameViewport.js
@@ -1,8 +1,9 @@
+import {StringUtils} from '../node_modules/igv-utils/src/index.js'
+import {IGVMath} from '../node_modules/igv-utils/src/index.js'
 import $ from './vendor/jquery-3.3.1.slim.js'
 import ViewportBase from './viewportBase.js'
 import IGVGraphics from './igv-canvas.js'
 import { appleCrayonRGB, appleCrayonRGBA, randomRGB, randomRGBConstantAlpha } from './util/colorPalletes.js'
-import {IGVMath} from "../node_modules/igv-utils/src/index.js";
 
 const sampleNameViewportWidth = 128
 const sampleNameXShim = 4
@@ -25,17 +26,9 @@ class SampleNameViewport extends ViewportBase {
     }
 
     initializationHelper() {
-
         this.$viewport.data('viewport-type', 'sample-name')
-
-        const $hover = $('<div>', { class:'igv-sample-name-viewport-hover' })
-        this.trackView.$viewportContainer.append($hover)
-
-        this.featureMap = undefined
-        this.canvasTop = undefined
-
-        this.configureSampleNameHover($hover)
-
+        this.$hover = $('<div>', { class:'igv-sample-name-viewport-hover' })
+        this.trackView.$viewportContainer.append(this.$hover)
     }
 
     setTop(contentTop) {
@@ -96,6 +89,7 @@ class SampleNameViewport extends ViewportBase {
         this.canvasVerticalRange = {top: pixelTop, bottom: pixelTop + pixelHeight}
 
         if (this.$canvas) {
+            this.$canvas.off()
             this.$canvas.remove()
             this.canvas = this.ctx = null
         }
@@ -104,6 +98,39 @@ class SampleNameViewport extends ViewportBase {
         this.$content.append(this.$canvas)
         this.canvas = canvas
         this.ctx = context
+
+        this.canvas.addEventListener('mousemove', e => {
+
+            const { currentTarget, clientY } = e
+
+            const { y:target_bbox_min_y } = currentTarget.getBoundingClientRect()
+
+            const y = (clientY - target_bbox_min_y) + pixelTop
+            // console.log(`y ${ StringUtils.numberFormatter(y) }`)
+
+            let yMin = pixelTop
+            for (let name of samples.names) {
+
+                const yMax = getYFont(context, name.toUpperCase(), yMin, samples.height)
+                if (y < yMin || y > yMax) {
+                    // do nothing
+                } else {
+
+                    const cssConfig =
+                        {
+                            right: 0,
+                            top: y + this.$content.position().top
+                        }
+
+                    this.$hover.css(cssConfig)
+
+                    this.$hover.text(name.toUpperCase())
+                }
+
+                yMin += samples.height
+            }
+
+        })
     }
 
     draw({ context, pixelWidth, pixelTop, samples }) {
@@ -135,58 +162,17 @@ class SampleNameViewport extends ViewportBase {
             context.fillStyle = appleCrayonRGB('lead')
 
             const text = name.toUpperCase()
-            const dy = getSampleNameYShim(context, text, samples.height)
-            context.fillText(text, sampleNameXShim, y + samples.height - dy)
+
+            // const yFont = y + samples.height - getSampleNameYShim(context, text, samples.height)
+            const yFont = getYFont(context, text, y, samples.height)
+
+            // const diagnostic = `min ${ StringUtils.numberFormatter(y) } max ${ StringUtils.numberFormatter(y + samples.height) }`
+            // context.fillText(diagnostic, sampleNameXShim, yFont)
+
+            context.fillText(text, sampleNameXShim, yFont)
 
             y += samples.height
         }
-
-    }
-
-    configureSampleNameHover($hover) {
-
-        this.ctx.canvas.addEventListener('mousemove', (e) => {
-
-            const { currentTarget, clientX, clientY, screenX, screenY } = e
-
-            if (this.featureMap && undefined !== this.canvasTop) {
-
-                const { x:x_current_target, y:y_current_target, width: width_current_target } = currentTarget.getBoundingClientRect()
-
-                // (clientY - y_current_target) is equivalent to event.offsetY which does not work correctly
-                const dy = (clientY - y_current_target) + this.canvasTop
-                const result = getBBox(this.featureMap, dy)
-
-                if (result) {
-
-                    const { width: width_viewport_container } = this.trackView.$viewportContainer.get(0).getBoundingClientRect()
-
-                    const fudge = 3
-                    const font_size = Math.min(this.trackView.track.expandedRowHeight, maxFontSize)
-                    const cssConfig =
-                        {
-                            'font-size': `${ font_size }px`,
-                            // left: width_viewport_container - width_current_target + sampleNameXShim,
-                            right: 0,
-                            top: result.y + this.$content.position().top - fudge
-                        }
-
-                    $hover.css(cssConfig)
-
-                    $hover.text(result.name)
-                }
-            }
-        })
-
-        this.ctx.canvas.addEventListener('mouseenter', () => {
-            $hover.show()
-        })
-
-        this.ctx.canvas.addEventListener('mouseleave', () => {
-            $hover.hide()
-        })
-
-        $hover.hide()
 
     }
 
@@ -234,27 +220,14 @@ class SampleNameViewport extends ViewportBase {
     }
 }
 
+function getYFont(context, text, y, height) {
+    return y + height - getSampleNameYShim(context, text, height)
+}
+
 function getSampleNameYShim(context, text, h) {
 
     const { fontBoundingBoxAscent, fontBoundingBoxDescent } = context.measureText(text)
     return (h - (fontBoundingBoxAscent + fontBoundingBoxDescent))/2
-}
-function getBBox(featureMap, y) {
-
-    for (let [ key, value ] of featureMap) {
-
-        if ('sampleHeight' === key) {
-            continue
-        }
-
-        if (y < value.y || y > (value.h + value.y)) {
-
-        } else {
-            return value
-        }
-    }
-
-    return undefined
 }
 
 function configureFont(ctx, { textAlign, textBaseline, strokeStyle, fillStyle }, sampleHeight) {
