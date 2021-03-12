b6526c70bb5f42c0e9e26f710222636d4db17fa0
Douglass Turner <douglass.turner@gmail.com>
2021-03-12 14:23:13 -0500

    naming consistency
    Former-commit-id: b663a62bc4cea70240a31242ca9f539450a83e3f

--- js/browser.js
+++ js/browser.js
@@ -103,7 +103,7 @@ class Browser {
         this.trackLabelsVisible = true;
         this.isCenterGuideVisible = false;
 
-        this.sampleNamesVisible = false;
+        this.showSampleNames = false;
 
         this.cursorGuideVisible = false;
         this.constants = {
@@ -182,7 +182,7 @@ class Browser {
     }
 
     getSampleNameViewportWidth() {
-        return false === this.sampleNamesVisible ? 0 : this.sampleNameViewportWidth
+        return false === this.showSampleNames ? 0 : this.sampleNameViewportWidth
     }
 
     startSpinner() {
@@ -349,9 +349,9 @@ class Browser {
 
         this.removeAllTracks();
 
-        if (session.sampleNamesVisible) {
-            this.sampleNamesVisible = 'true'=== session.sampleNamesVisible
-            this.sampleNameControl.setState(this.sampleNamesVisible)
+        if (session.showSampleNames) {
+            this.showSampleNames = 'true'=== session.showSampleNames
+            this.sampleNameControl.setState(this.showSampleNames)
         }
 
         if (session.sampleNameViewportWidth) {
@@ -1453,7 +1453,7 @@ class Browser {
             "version": version()
         }
 
-        json['sampleNamesVisible'] = true === this.sampleNamesVisible ? 'true' : 'false'
+        json['showSampleNames'] = true === this.showSampleNames ? 'true' : 'false'
         json['sampleNameViewportWidth'] = this.sampleNameViewportWidth
 
         json["reference"] = this.genome.toJSON();
