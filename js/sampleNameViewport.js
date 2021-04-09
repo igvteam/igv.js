31c9c1b98eb6b999522ac892286dcac224c22eaf
jrobinso <jrobinso@broadinstitute.org>
2021-04-08 23:08:02 -0700

    refactor sample name viewport

--- js/sampleNameViewport.js
+++ js/sampleNameViewport.js
@@ -1,7 +1,6 @@
 import $ from './vendor/jquery-3.3.1.slim.js'
-import ViewportBase from './viewportBase.js'
-import IGVGraphics from './igv-canvas.js'
-import { appleCrayonRGB, appleCrayonRGBA, randomRGB } from './util/colorPalletes.js'
+import {appleCrayonRGB} from './util/colorPalletes.js'
+import {DOMUtils} from '../node_modules/igv-utils/src/index.js';
 
 const sampleNameXShim = 4
 
@@ -13,183 +12,151 @@ const fontConfigureTemplate =
         textAlign: 'start',
         textBaseline: 'bottom',
         strokeStyle: 'black',
-        fillStyle:'black'
+        fillStyle: 'black'
     }
 
-class SampleNameViewport extends ViewportBase {
+class SampleNameViewport { //extends ViewportBase {
 
     constructor(trackView, $viewportContainer, referenceFrame, width) {
-        super(trackView, $viewportContainer, referenceFrame, width)
+
+        this.guid = DOMUtils.guid();
+        this.trackView = trackView;
+        this.referenceFrame = referenceFrame;
+
+        this.browser = trackView.browser;
+
+        this.$viewport = $('<div class="igv-viewport">');
+        $viewportContainer.append(this.$viewport);
+
+        this.$canvas = $('<canvas class ="igv-canvas">');
+        this.$viewport.append(this.$canvas);
+
+        this.canvas = this.$canvas.get(0);
+        this.ctx = this.canvas.getContext("2d");
+
+        this.contentTop = 0;
+
+        this.setWidth(width);
+
     }
 
-    initializationHelper() {
-        this.$viewport.data('viewport-type', 'sample-name')
-        // this.hover = document.createElement('div')
-        // this.hover.classList.add('igv-sample-name-viewport-hover')
-        // this.trackView.$viewportContainer.append($(this.hover))
+    checkCanvas() {
+
+        const dpi = window.devicePixelRatio;
+        const requiredHeight = this.$viewport.height();
+        const requiredWidth = this.browser.sampleNameViewportWidth;
+
+        if (this.canvas.width !== requiredWidth*dpi || this.canvas.height !== requiredHeight*dpi) {
+            const canvas = this.canvas;
+            canvas.width = requiredWidth * dpi;
+            canvas.height = requiredHeight * dpi;
+            canvas.style.width = `${requiredWidth}px`
+            canvas.style.height = `${requiredHeight}px`
+            this.ctx = this.canvas.getContext("2d");
+            this.ctx.scale(dpi, dpi);
+        }
+
     }
+
 
     setTop(contentTop) {
-
-        const viewportHeight = this.$viewport.height()
-        const viewTop = -contentTop
-        const viewBottom = viewTop + viewportHeight
-
-        this.$content.css('top', `${ contentTop }px`)
-
-        if (undefined === this.canvasVerticalRange || this.canvasVerticalRange.bottom < viewBottom || this.canvasVerticalRange.top > viewTop) {
-            if(typeof this.trackView.track.getSamples === 'function' && typeof this.trackView.track.computePixelHeight === 'function') {
-
-                const features = this.trackView.viewports[ 0 ].cachedFeatures
-                const contentHeight = this.trackView.track.computePixelHeight(features)
-
-                const samples = this.trackView.track.getSamples()
-                this.repaint({ contentHeight, samples })
-            }
-        }
-
+        this.contentTop = contentTop;
+        const samples = this.trackView.track.getSamples();
+        this.repaint(samples)
     }
 
-    async repaint({ contentHeight, samples }) {
 
-        let devicePixelRatio
-        if ("FILL" === this.trackView.track.displayMode) {
-            devicePixelRatio = window.devicePixelRatio
-        } else {
-            devicePixelRatio = (this.trackView.track.supportHiDPI === false) ? 1 : window.devicePixelRatio
-        }
-
-        const canvas = document.createElement('canvas')
-        canvas.classList.add('igv-canvas')
-
-        const context = canvas.getContext("2d")
-
-        canvas.style.width = `${ this.browser.sampleNameViewportWidth }px`
-        canvas.width = devicePixelRatio * this.browser.sampleNameViewportWidth
-
-        const viewportHeight = this.$viewport.height()
-        const pixelHeight = Math.min(contentHeight, 3 * viewportHeight)
-
-        canvas.style.height = `${ pixelHeight }px`
-        canvas.height = devicePixelRatio * pixelHeight
-
-        context.scale(devicePixelRatio, devicePixelRatio)
-
-        const pixelTop = Math.max(0, -(this.$content.position().top) - viewportHeight)
-
-        canvas.style.top = `${ pixelTop }px`
-
-        context.translate(0, -pixelTop)
-
-        this.draw({ context, pixelWidth: this.browser.sampleNameViewportWidth, samples })
-
-        this.canvasVerticalRange = {top: pixelTop, bottom: pixelTop + pixelHeight}
-
-        if (this.$canvas) {
-            this.$canvas.off()
-            this.$canvas.remove()
-            this.canvas = this.ctx = null
-        }
-
-        this.$canvas = $(canvas)
-        this.$content.append(this.$canvas)
-        this.canvas = canvas
-        this.ctx = context
-
-        // this.addMouseHandler(context, pixelTop, samples)
-
+    setWidth(width) {
+        this.$viewport.width(width);
+        this.checkCanvas();
     }
 
-    draw({ context, pixelWidth, samples }) {
+    async repaint(samples) {
+        this.checkCanvas();
+        this.draw({context: this.ctx, samples})
+    }
+
+    draw({context, samples}) {
 
         if (!samples || samples.names.length === 0) {
             return
         }
 
-        configureFont(context, fontConfigureTemplate, samples.height)
+        context.save();
 
-        const sampleNameXShim = 4
+        configureFont(context, fontConfigureTemplate, samples.height);
+        const sampleNameXShim = 4;
 
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
-        let y = samples.yOffset || 0;
+        context.fillStyle = appleCrayonRGB('lead');
+
+        let y = (samples.yOffset || 0) + this.contentTop;    // contentTop will always be a negative number (top relative to viewport)
+
         for (let name of samples.names) {
 
-            // context.save()
-
-            // context.fillStyle = appleCrayonRGB('snow')
-            // context.fillRect(0, y, pixelWidth, h)
-
-            // context.fillStyle = randomRGBConstantAlpha(180, 240, 1)
-            context.fillStyle = appleCrayonRGB('snow')
-            context.fillRect(0, y, pixelWidth, samples.height)
-
-            // context.restore()
-
-            context.fillStyle = appleCrayonRGB('lead')
-
-            const text = name.toUpperCase()
-
-            // const yFont = y + samples.height - getSampleNameYShim(context, text, samples.height)
-            const yFont = getYFont(context, text, y, samples.height)
-
-            // const diagnostic = `min ${ StringUtils.numberFormatter(y) } max ${ StringUtils.numberFormatter(y + samples.height) }`
-            // context.fillText(diagnostic, sampleNameXShim, yFont)
-
-            context.fillText(text, sampleNameXShim, yFont)
-
-            y += samples.height
+            const text = name.toUpperCase();
+            const yFont = getYFont(context, text, y, samples.height);
+            context.fillText(text, sampleNameXShim, yFont);
+            y += samples.height;
         }
 
-    }
-
-    renderSVGContext(context, { deltaX, deltaY }) {
-
-        const yScrollDelta = this.$content.position().top
-
-        const { width, height } = this.$viewport.get(0).getBoundingClientRect()
-
-        const id = (this.trackView.track.name || this.trackView.track.id).replace(/\W/g, '')
-        context.addTrackGroupWithTranslationAndClipRect(id, deltaX, deltaY + yScrollDelta, width, height, -yScrollDelta)
-
-        this.drawSVGWithContext(context, width)
+        context.restore();
 
     }
 
-    drawSVGWithContext(context, width) {
+    renderSVGContext(context, {deltaX, deltaY}) {
 
-        if(typeof this.trackView.track.getSamples === 'function' && typeof this.trackView.track.computePixelHeight === 'function') {
+        if (typeof this.trackView.track.getSamples === 'function') {
 
             const samples = this.trackView.track.getSamples();
-            if(!samples || samples.names.length == 0) return;
 
-            context.save()
+            const yScrollDelta = this.contentTop;
+            const {width, height} = this.$viewport.get(0).getBoundingClientRect()
 
-            configureFont(context, fontConfigureTemplate, samples.height)
+            const id = (this.trackView.track.name || this.trackView.track.id).replace(/\W/g, '')
+            context.addTrackGroupWithTranslationAndClipRect(id, deltaX, deltaY + yScrollDelta, width, height, -yScrollDelta)
 
-            const sampleNameXShim = 4
-
-            let y = samples.yOffest || 0;
-            for (let name of samples.names) {
-
-                context.fillStyle = appleCrayonRGB('snow')
-                context.fillRect(0, y, width, samples.height)
-
-                context.fillStyle = appleCrayonRGB('lead')
-
-                const text = name.toUpperCase()
-
-                const yFont = getYFont(context, text, y, samples.height)
-
-                context.fillText(text, sampleNameXShim, yFont)
-
-                y += samples.height
-            }
-
-            context.restore()
+            this.draw({context, samples});
         }
-
     }
+
+    //
+    // drawSVGWithContext(context, width) {
+    //
+    //     if (typeof this.trackView.track.getSamples === 'function' && typeof this.trackView.track.computePixelHeight === 'function') {
+    //
+    //         const samples = this.trackView.track.getSamples();
+    //         if (!samples || samples.names.length == 0) return;
+    //
+    //         context.save()
+    //
+    //         configureFont(context, fontConfigureTemplate, samples.height)
+    //
+    //         const sampleNameXShim = 4
+    //
+    //         let y = samples.yOffest || 0;
+    //         for (let name of samples.names) {
+    //
+    //             context.fillStyle = appleCrayonRGB('snow')
+    //             context.fillRect(0, y, width, samples.height)
+    //
+    //             context.fillStyle = appleCrayonRGB('lead')
+    //
+    //             const text = name.toUpperCase()
+    //
+    //             const yFont = getYFont(context, text, y, samples.height)
+    //
+    //             context.fillText(text, sampleNameXShim, yFont)
+    //
+    //             y += samples.height
+    //         }
+    //
+    //         context.restore()
+    //
+    //     }
+    //
+    // }
 
     addMouseHandler(context, pixelTop, samples) {
 
@@ -200,9 +167,9 @@ class SampleNameViewport extends ViewportBase {
                 this.hover.textContent = ''
             } else {
 
-                const { currentTarget, clientY } = e
+                const {currentTarget, clientY} = e
 
-                const { y:target_bbox_min_y } = currentTarget.getBoundingClientRect()
+                const {y: target_bbox_min_y} = currentTarget.getBoundingClientRect()
 
                 const y = (clientY - target_bbox_min_y) + pixelTop
 
@@ -214,7 +181,7 @@ class SampleNameViewport extends ViewportBase {
                         // do nothing
                     } else {
 
-                        this.hover.style.top = `${ yMin + this.$content.position().top }px`
+                        this.hover.style.top = `${yMin + this.contentTop}px`
                         this.hover.style.right = '0px'
 
                         this.hover.textContent = name.toUpperCase()
@@ -243,18 +210,18 @@ function getYFont(context, text, y, height) {
 
 function getSampleNameYShim(context, text, h) {
 
-    const { fontBoundingBoxAscent, fontBoundingBoxDescent } = context.measureText(text)
-    return (h - (fontBoundingBoxAscent + fontBoundingBoxDescent))/2
+    const {fontBoundingBoxAscent, fontBoundingBoxDescent} = context.measureText(text)
+    return (h - (fontBoundingBoxAscent + fontBoundingBoxDescent)) / 2
 }
 
-function configureFont(ctx, { textAlign, textBaseline, strokeStyle, fillStyle }, sampleHeight) {
+function configureFont(ctx, {textAlign, textBaseline, strokeStyle, fillStyle}, sampleHeight) {
     const pixels = Math.min(sampleHeight, maxFontSize)
-    ctx.font = `${ pixels }px sans-serif`
+    ctx.font = `${pixels}px sans-serif`
     ctx.textAlign = textAlign
     ctx.textBaseline = textBaseline
     ctx.fillStyle = fillStyle
 }
 
-export { sampleNameXShim }
+export {sampleNameXShim}
 
 export default SampleNameViewport
