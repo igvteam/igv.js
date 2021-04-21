5389c4f73afb33cf94433cb1a0b50bd69a641a52
jrobinso <jrobinso@broadinstitute.org>
2021-04-21 10:31:22 -0700

    update dev html
    Former-commit-id: ff13a80f669c9ecf26d68f72e70b69cda931ba01

--- dev/cbio/cbio.js
+++ dev/cbio/cbio.js
@@ -37,8 +37,7 @@ const cBioUtils = {
     fetchSamplesByStudy: async function (study, baseURL) {
 
         baseURL = baseURL || "https://www.cbioportal.org/api"
-
-        let url = baseURL + "/studies/" + study.studyId + "/samples";
+        let url = baseURL + "/studies/" + study + "/samples";
         const samples = await igvxhr.loadJson(url);
         let sampleStudyList = [];
         samples.forEach(function (sampleJson) {
@@ -52,13 +51,45 @@ const cBioUtils = {
         return sampleStudyList;
     },
 
+    // Copy number
+    // {
+    //     "uniqueSampleKey": "VENHQS1PUi1BNUoyLTAxOmFjY190Y2dh",
+    //     "uniquePatientKey": "VENHQS1PUi1BNUoyOmFjY190Y2dh",
+    //     "patientId": "TCGA-OR-A5J2",
+    //     "start": 3218610,
+    //     "end": 4749076,
+    //     "segmentMean": -0.2239,
+    //     "studyId": "acc_tcga",
+    //     "sampleId": "TCGA-OR-A5J2-01",
+    //     "chromosome": "1",
+    //     "numberOfProbes": 958
+    // }
+
     fetchCopyNumberByStudy: async function (study, baseURL) {
         baseURL = baseURL || "https://www.cbioportal.org/api";
         const sampleStudyList = await this.fetchSamplesByStudy(study);
-        let url = baseURL + "/copy-number-segments/fetch?projection=SUMMARY";
-        let body = JSON.stringify(sampleStudyList);
-        return igvxhr.loadJson(url, {method: "POST", sendData: body})
+        const url = baseURL + "/copy-number-segments/fetch?projection=SUMMARY";
+        const body = JSON.stringify(sampleStudyList);
+        const json = await igvxhr.loadJson(url, {method: "POST", sendData: body});
+        for (let j of json) {
+            j.chr = j["chromosome"];
+            j.value = j["segmentMean"];
+            j.sampleKey = j["uniqueSampleKey"];
+            j.sample = j["sampleId"];
+        }
+        return json;
     },
+
+    fetchMutationsByStudy: async function (study, baseURL) {
+        baseURL = baseURL || "https://www.cbioportal.org/api";
+        const sampleStudyList = await this.fetchSamplesByStudy(study);
+        const url = baseURL + "/molecular-profiles/ov_tcga_pub_mutations/mutations/fetch?projection=DETAILED";
+        const sampleList = sampleStudyList.map(ss => ss.sampleId);
+        const body =  JSON.stringify({"sampleIds":["TCGA-13-1489-01"]}) ;// JSON.stringify({sampleIds: sampleList});
+        const json = await igvxhr.loadJson(url, {method: "POST", sendData: body});
+        return json;
+    },
+
 
 }
 
