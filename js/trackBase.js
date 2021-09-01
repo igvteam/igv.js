c7f1e30b3216006228d6b2db7d999e92964944db
jrobinso <jrobinso@broadinstitute.org>
2021-09-01 09:46:29 -0700

    use transient property for "_derivedName"
    Former-commit-id: 91b25df13576b61aaee060f78238e53783a9d24e

--- js/trackBase.js
+++ js/trackBase.js
@@ -119,13 +119,10 @@ class TrackBase {
      */
     getState() {
 
-        // Transient properties that do not follow the _ convention
-        const transientProperties = new Set(["derivedName"]);
-
         // Create copy of config, minus transient properties (convention is name starts with '_')
         const state = {};
         for (let key of Object.keys(this.config)) {
-            if (!(key.startsWith("_") || transientProperties.has(key))) {
+            if (!key.startsWith("_")) {
                 state[key] = this.config[key];
             }
         }
@@ -268,7 +265,7 @@ class TrackBase {
         // is an exception if it was derived and not explicitly entered (that is derived from the web app from filename).
         for (let key of Object.keys(tracklineConfg)) {
 
-            if (!this.config.hasOwnProperty(key) || (key === "name" && this.config.derivedName)) {
+            if (!this.config.hasOwnProperty(key) || (key === "name" && this.config._derivedName)) {
                 this[key] = tracklineConfg[key];
                 if (key === "height" && this.trackView) {
                     try {
