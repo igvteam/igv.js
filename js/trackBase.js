de0de8db2f35a57640e9bef5bacadced26a3ee46
jrobinso <jrobinso@broadinstitute.org>
2021-09-02 16:20:40 -0700

    Add an "Info" line for a feature info link, rather than make "name" the link.  Its important to keep name as plain text as its passed to the event call back for a "trackclick" event, and very likely used by application developers. Update event examples
    Former-commit-id: e2db7ebfd45530bbee25255cbdc411b2990a9da5

--- js/trackBase.js
+++ js/trackBase.js
@@ -320,12 +320,6 @@ class TrackBase {
                 isSimpleType(feature[property])) {
 
                 let value = feature[property];
-                // if("name" === property && (this.infoURL || this.config.infoURL)) {
-                //     const url = this.infoURL || this.config.infoURL;
-                //     const href = url.replace("$$", feature.name);
-                //     value = `<a target="_blank" href=${href}>${value}</a>`;
-                // }
-
                 data.push({name: StringUtils.capitalize(property), value: value});
 
                 if (property === "alleles") {
