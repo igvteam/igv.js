8a1064fbf208b54c7480a314cb82c0f5a4d282d2
jrobinso <jrobinso@broadinstitute.org>
2022-04-06 00:44:01 -0700

    bb decoder fix

--- js/bigwig/bbDecoders.js
+++ js/bigwig/bbDecoders.js
@@ -1,4 +1,63 @@
-//table chromatinInteract
+import {IGVColor} from "../../node_modules/igv-utils/src/index.js"
+
+function getDecoder(definedFieldCount, fieldCount, autoSql, format) {
+
+    if ("biginteract" === format || (autoSql && ('chromatinInteract' === autoSql.table) || 'interact' === autoSql.table)) {
+        return decodeInteract
+    } else {
+        const standardFieldCount = definedFieldCount - 3
+        return function (feature, tokens) {
+
+            if (standardFieldCount > 0) {
+                feature.name = tokens[0]
+            }
+            if (standardFieldCount > 1) {
+                feature.score = Number(tokens[1])
+            }
+            if (standardFieldCount > 2) {
+                feature.strand = tokens[2]
+            }
+            if (standardFieldCount > 3) {
+                feature.cdStart = parseInt(tokens[3])
+            }
+            if (standardFieldCount > 4) {
+                feature.cdEnd = parseInt(tokens[4])
+            }
+            if (standardFieldCount > 5) {
+                if (tokens[5] !== "." && tokens[5] !== "0" && tokens[5] !== "-1") {
+                    const c = IGVColor.createColorString(tokens[5])
+                    feature.color = c.startsWith("rgb") ? c : undefined
+                }
+            }
+            if (standardFieldCount > 8) {
+                const exonCount = parseInt(tokens[6])
+                const exonSizes = tokens[7].split(',')
+                const exonStarts = tokens[8].split(',')
+                const exons = []
+                for (let i = 0; i < exonCount; i++) {
+                    const eStart = feature.start + parseInt(exonStarts[i])
+                    const eEnd = eStart + parseInt(exonSizes[i])
+                    exons.push({start: eStart, end: eEnd})
+                }
+                findUTRs(exons, feature.cdStart, feature.cdEnd)
+                feature.exons = exons
+            }
+
+            if (autoSql) {
+                // TODO -- these should be equal, validate?  fieldCount-definedFieldCount, as.fields.length, tokens.length-3
+                const extraStart = definedFieldCount
+                for (let i = extraStart; i < fieldCount; i++) {
+                    if (i < autoSql.fields.length) {
+                        const name = autoSql.fields[i].name
+                        const value = tokens[i - 3]
+                        feature[name] = value
+                    }
+                }
+            }
+        }
+    }
+
+    //table chromatinInteract
 // "Chromatin interaction between two regions"
 //     (
 //     string chrom;      "Chromosome (or contig, scaffold, etc.). For interchromosomal, use 2 records"
@@ -20,126 +79,25 @@
 //     string region2Name; "Identifier of upper/this region"
 //     string region2Strand; "Orientation of upper/this region: + or -.  Use . if not applicable"
 //     )
-import {IGVColor} from "../../node_modules/igv-utils/src/index.js"
+    function decodeInteract(feature, tokens) {
 
-function getDecoder(definedFieldCount, fieldCount, autoSql, format) {
+        feature.chr1 = tokens[5]
+        feature.start1 = Number.parseInt(tokens[6])
+        feature.end1 = Number.parseInt(tokens[7])
 
-    if ("biginteract" === format || (autoSql && ('chromatinInteract' === autoSql.table) || 'interact' === autoSql.table)) {
-        return decodeInteract
-    } else if ("biggenepred" === format|| (autoSql && 'bigGenePred' === autoSql.table)) {
-        return decodeBigGenePred
-    } else if ("bignarrowpeak" === format|| (autoSql && 'bigNarrowPeak' === autoSql.table)) {
-        return decodeBigGenePred
-    } else {
-        const standardFieldCount = definedFieldCount - 3
-        return function (feature, tokens) {
+        feature.chr2 = tokens[10]
+        feature.start2 = Number.parseInt(tokens[11])
+        feature.end2 = Number.parseInt(tokens[12])
 
-            decodeStandardFields(feature, tokens, standardFieldCount)
-
-            if (autoSql) {
-                const extraStart = definedFieldCount
-                for (let i = extraStart; i < fieldCount; i++) {
-                    if (i < autoSql.fields.length) {
-                        const name = autoSql.fields[i].name
-                        const value = tokens[i - 3]
-                        feature[name] = value
-                    }
-                }
-            }
-        }
-    }
-}
-
-function decodeStandardFields(feature, tokens, standardFieldCount) {
-
-    if (standardFieldCount > 0) {
         feature.name = tokens[0]
-    }
-    if (standardFieldCount > 1) {
         feature.score = Number(tokens[1])
-    }
-    if (standardFieldCount > 2) {
-        feature.strand = tokens[2]
-    }
-    if (standardFieldCount > 3) {
-        feature.cdStart = parseInt(tokens[3])
-    }
-    if (standardFieldCount > 4) {
-        feature.cdEnd = parseInt(tokens[4])
-    }
-    if (standardFieldCount > 5) {
-        if (tokens[5] !== "." && tokens[5] !== "0" && tokens[5] !== "-1") {
-            const c = IGVColor.createColorString(tokens[5])
-            feature.color = c.startsWith("rgb") ? c : undefined
-        }
-    }
-    if (tokens.length > 8) {
-        const exonCount = parseInt(tokens[6])
-        const exonSizes = tokens[7].split(',')
-        const exonStarts = tokens[8].split(',')
-        const exons = []
-        for (let i = 0; i < exonCount; i++) {
-            const eStart = feature.start + parseInt(exonStarts[i])
-            const eEnd = eStart + parseInt(exonSizes[i])
-            exons.push({start: eStart, end: eEnd})
-        }
-        findUTRs(exons, feature.cdStart, feature.cdEnd)
-        feature.exons = exons
+        feature.value = Number(tokens[2])
+        feature.color = tokens[4] === '.' ? undefined : tokens[4] === "0" ? "rgb(0,0,0)" : tokens[4]
+
+        return feature
     }
 }
 
-function decodeInteract(feature, tokens) {
-
-    feature.chr1 = tokens[5]
-    feature.start1 = Number.parseInt(tokens[6])
-    feature.end1 = Number.parseInt(tokens[7])
-
-    feature.chr2 = tokens[10]
-    feature.start2 = Number.parseInt(tokens[11])
-    feature.end2 = Number.parseInt(tokens[12])
-
-    feature.name = tokens[0]
-    feature.score = Number(tokens[1])
-    feature.value = Number(tokens[2])
-    feature.color = tokens[4] === '.' ? undefined : tokens[4] === "0" ? "rgb(0,0,0)" : tokens[4]
-
-    return feature
-}
-
-
-/**
- string chrom;        "Reference sequence chromosome or scaffold"
- uint   chromStart;    "Start position in chromosome"
- uint   chromEnd;        "End position in chromosome"
- ------------
- 0 string name;            "Name or ID of item, ideally both human-readable and unique"
- 1 uint score;            "Score (0-1000)"
- 2 char[1] strand;        "+ or - for strand"
- 3 uint thickStart;        "Start of where display should be thick (start codon)"
- 4 uint thickEnd;        "End of where display should be thick (stop codon)"
- 5 uint reserved;        "RGB value (use R,G,B string in input file)"
- 6 int blockCount;        "Number of blocks"
- 7 int[blockCount] blockSizes; "Comma separated list of block sizes"
- 8 int[blockCount] chromStarts;"Start positions relative to chromStart"
- 9 string name2;        "Alternative/human readable name"
- 10 string cdsStartStat;    "Status of CDS start annotation (none, unknown, incomplete, or complete)"
- 11 string cdsEndStat;    "Status of CDS end annotation (none, unknown, incomplete, or complete)"
- 12 int[blockCount] exonFrames; "Exon frame {0,1,2}, or -1 if no frame for exon"
- 13 string type;            "Transcript type"
- 14 string geneName;        "Primary identifier for gene"
- 15 string geneName2;    "Alternative/human-readable gene name"
- 16 string geneType;        "Gene type"
- * @param feature
- * @param tokens
- * @returns {*}
- */
-
-function decodeBigGenePred(feature, tokens) {
-    decodeStandardFields(feature, tokens, 9)
-    // TODO frame shifts, extra gene names
-}
-
-// TODO -- copied from "ucsc.js", move to common file
 function findUTRs(exons, cdStart, cdEnd) {
 
     for (let exon of exons) {
