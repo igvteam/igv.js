5e8ce7a6e15b786260221677f11d50eeb81dc9a0
Douglass Turner <douglass.turner@gmail.com>
2022-04-11 19:03:21 -0400

    session save/restore edits

--- js/roi/ROIManager.js
+++ js/roi/ROIManager.js
@@ -162,7 +162,7 @@ class ROIManager {
     }
 
     toJSON() {
-        return [ ...this.roiSets, this.interativeROISet ].filter(roiSet => !roiSet.isImmutable).map(roiSet => roiSet.toJSON())
+        return [ ...this.roiSets, this.interativeROISet ].map(roiSet => roiSet.toJSON())
     }
 }
 
