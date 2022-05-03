624fa99d1d487b5d07bc73c8a7b174b5be044785
Douglass Turner <douglass.turner@gmail.com>
2022-05-03 15:33:46 -0400

    ROIMenu - Add Go To menu item

--- js/roi/ROIMenu.js
+++ js/roi/ROIMenu.js
@@ -1,35 +1,13 @@
-import { DOMUtils, Icon, makeDraggable } from '../../node_modules/igv-utils/src/index.js'
-import {appleCrayonRGB, appleCrayonRGBA} from '../util/colorPalletes.js'
+import { DOMUtils } from '../../node_modules/igv-utils/src/index.js'
+import {deleteRegionWithKey, parseRegionKey} from './ROIManager.js'
 
 class ROIMenu {
     constructor(browser, parent) {
 
         this.browser = browser
 
-        // container
         this.container = DOMUtils.div({ class: 'igv-roi-menu' })
         parent.appendChild(this.container)
-
-        // header
-        const header = DOMUtils.div({ class: 'igv-roi-header' })
-        this.container.appendChild(header)
-
-        // dismiss button
-        // const dismiss = DOMUtils.div()
-        // header.appendChild(dismiss)
-        // dismiss.appendChild(Icon.createIcon('times'))
-        //
-        // dismiss.addEventListener('click', event => {
-        //     event.stopPropagation()
-        //     event.preventDefault()
-        //     this.container.style.display = 'none'
-        // })
-
-        // body container
-        this.bodyContainer = DOMUtils.div({ class: 'igv-roi-body' })
-        this.container.appendChild(this.bodyContainer)
-
-        makeDraggable(this.container, header)
 
         this.container.style.display = 'none'
 
@@ -37,31 +15,40 @@ class ROIMenu {
 
     present(x, y, roiManager, columnContainer, regionElement) {
 
-        removeAllChildNodes(this.bodyContainer)
+        removeAllChildNodes(this.container)
 
-        const row = DOMUtils.div({ class: 'igv-roi-body-row' })
-        this.bodyContainer.appendChild(row)
+        let row
 
+        // Go To
+        row = DOMUtils.div({ class: 'igv-roi-body-row' })
+        row.innerText = 'Go To'
+        this.container.appendChild(row)
+
+        row.addEventListener('click', event => {
+            event.stopPropagation()
+            this.container.style.display = 'none'
+            const { locus } = parseRegionKey(regionElement.dataset.region)
+            this.browser.search(locus)
+        })
+
+        // Delete
+        row = DOMUtils.div({ class: 'igv-roi-body-row' })
         row.innerText = 'Delete'
+        this.container.appendChild(row)
 
-        row.addEventListener('click', () => roiManager.roiTable.removeRegionElement(regionElement))
+        row.addEventListener('click', event => {
+            event.stopPropagation()
+            this.container.style.display = 'none'
+            deleteRegionWithKey(this.browser.roiManager.roiSets, regionElement.dataset.region, this.browser.columnContainer)
+        })
 
         this.container.style.left = `${ x }px`
         this.container.style.top  = `${ y }px`
         this.container.style.display = 'flex'
 
         columnContainer.addEventListener('click', event => {
-
-            event.preventDefault()
             event.stopPropagation()
-
-            if (this.container.querySelector('.igv-roi-header') === event.target) {
-                console.log(`${ Date.now() } clicked header`)
-            } else {
-                this.container.style.display = 'none'
-            }
-
-
+            this.container.style.display = 'none'
         })
 
     }
