bcc8af30c536ab394ed2442048122f42044a1197
jrobinso <933148+jrobinso@users.noreply.github.com>
2022-07-11 08:00:17 -0700

    Limit genome ID display on command bar to 10 characters.

--- js/browser.js
+++ js/browser.js
@@ -655,7 +655,11 @@ class Browser {
     }
 
     updateNavbarDOMWithGenome(genome) {
-        this.$current_genome.text(genome.id || '')
+
+        // If the genome is defined directly from a fasta file or data url the "id" will be the full url.  Don't display
+        // this.
+        let genomeLabel = (genome.id && genome.id.length < 10 ? genome.id : '')
+        this.$current_genome.text(genomeLabel)
         this.$current_genome.attr('title', genome.id || '')
         this.chromosomeSelectWidget.update(genome)
     }
