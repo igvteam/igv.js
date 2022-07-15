3620e7864e0a34469a3a845eb6970a7154a86a0f
Douglass Turner <douglass.turner@gmail.com>
2022-07-15 11:13:18 -0400

    ROI Issues (#1529)
    * ROI Menu/Popup rethink
    
    * ROI Menu. Description edits.
    
    * ROI deletion bug edits
    
    Co-authored-by: Jim Robinson <933148+jrobinso@users.noreply.github.com>
    Former-commit-id: 47d28411162c804bc964228b5eda3d7b96cf43dc

--- js/roi/ROIMenu.js
+++ js/roi/ROIMenu.js
@@ -1,18 +1,108 @@
-import { DOMUtils } from '../../node_modules/igv-utils/src/index.js'
+import { DOMUtils, UIUtils } from '../../node_modules/igv-utils/src/index.js'
 
 class ROIMenu {
     constructor(browser, parent) {
 
         this.browser = browser
 
-        this.container = DOMUtils.div({ class: 'igv-roi-menu' })
+        // container
+        this.container = DOMUtils.div({ class: 'igv-roi-menu-next-gen' })
         parent.appendChild(this.container)
+
+        // header
+        const header = DOMUtils.div()
+        this.container.appendChild(header)
+
+        UIUtils.attachDialogCloseHandlerWithParent(header, () => this.container.style.display = 'none')
+
+        // body
+        this.body = DOMUtils.div()
+        this.container.appendChild(this.body)
 
         this.container.style.display = 'none'
 
     }
 
     async present(x, y, roiManager, columnContainer, regionElement) {
+
+        removeAllChildNodes(this.body)
+
+        const feature = await this.browser.roiManager.findUserDefinedRegionWithKey(regionElement.dataset.region)
+
+        // Description Copy
+        const _description_copy_ = DOMUtils.div()
+        this.body.appendChild(_description_copy_)
+
+        const placeholder = 'Description'
+        const str = (feature.name || placeholder)
+
+        _description_copy_.innerText = str
+        _description_copy_.setAttribute('title', str)
+        placeholder === str ? _description_copy_.classList.add('igv-roi-placeholder') : _description_copy_.classList.remove('igv-roi-placeholder')
+
+
+        // Set Description
+        const description = DOMUtils.div()
+        this.body.appendChild(description)
+        description.innerText = 'Set Description'
+
+        description.addEventListener('click', event => {
+
+            event.stopPropagation()
+
+            this.container.style.display = 'none'
+
+            const callback = () => {
+
+                const value = this.browser.inputDialog.input.value || ''
+                feature.name = value.trim()
+
+                this.container.style.display = 'none'
+
+                this.browser.roiManager.repaintTable()
+            }
+
+            const config =
+                {
+                    label: 'Description',
+                    value: (feature.name || ''),
+                    callback
+                }
+
+            this.browser.inputDialog.present(config, event)
+
+        })
+
+
+        // Delete Region
+        const _delete_ = DOMUtils.div()
+        this.body.appendChild(_delete_)
+        _delete_.innerText = 'Delete Region'
+
+        _delete_.addEventListener('click', event => {
+            event.stopPropagation()
+            this.container.style.display = 'none'
+            this.browser.roiManager.deleteUserDefinedRegionWithKey(regionElement.dataset.region, this.browser.columnContainer)
+        })
+
+
+
+
+
+
+
+        // columnContainer.addEventListener('click', event => {
+        //     event.stopPropagation()
+        //     this.container.style.display = 'none'
+        // })
+
+        this.container.style.left = `${ x }px`
+        this.container.style.top  = `${ y }px`
+        this.container.style.display = 'flex'
+
+    }
+
+    async __present(x, y, roiManager, columnContainer, regionElement) {
 
         removeAllChildNodes(this.container)
 
