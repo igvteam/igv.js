147dd32f936e2bcd72d7bdbab91621bf2cb90ddb
jrobinso <933148+jrobinso@users.noreply.github.com>
2022-07-19 08:48:01 -0700

    Revert "roi - rename "isUserDefined" => "editable""
    This reverts commit 31f9269d32beb05e229b1a921ebcb5840e699b95.

--- js/roi/ROIManager.js
+++ js/roi/ROIManager.js
@@ -70,7 +70,7 @@ class ROIManager {
         const records = []
 
         for (let roiSet of this.roiSets) {
-            const setName = roiSet.editable ? '' : (roiSet.name || '')
+            const setName = roiSet.isUserDefined ? '' : (roiSet.name || '')
             const allFeatures = await roiSet.getAllFeatures()
             for (let chr of Object.keys(allFeatures)) {
                 for (let feature of allFeatures[chr]) {
@@ -185,7 +185,7 @@ class ROIManager {
 
         regionElement.dataset.region = regionKey
 
-        if (true === roiSet.editable) {
+        if (true === roiSet.isUserDefined) {
 
             const header = DOMUtils.div()
             regionElement.appendChild(header)
@@ -206,7 +206,7 @@ class ROIManager {
 
     async getUserDefinedROISet() {
 
-        const userDefinedROISet = this.roiSets.find(roiSet => true === roiSet.editable)
+        const userDefinedROISet = this.roiSets.find(roiSet => true === roiSet.isUserDefined)
 
         if (userDefinedROISet) {
             const features = await userDefinedROISet.getAllFeatures()
@@ -221,7 +221,7 @@ class ROIManager {
 
         const config =
             {
-                editable: true,
+                isUserDefined: true,
                 features: []
             }
         const userDefinedROISet = new ROISet(config, this.browser.genome)
