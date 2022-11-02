igvteam/igv.js

commit 4ff3cd8264d2bdf16e592205091083d11b12bf68

Fix issue #1506,  problem with autoscaling merged tracks

js/feature/mergedTrack.js
@@ -118,27 +118,27 @@ class MergedTrack extends TrackBase {
         return items
     }
 
+    /**
+     * Returns a MergedFeatureCollection containing an array of features for the specified range, 1 for each track.
+     */
     async getFeatures(chr, bpStart, bpEnd, bpPerPixel) {
 
         const promises = this.tracks.map((t) => t.getFeatures(chr, bpStart, bpEnd, bpPerPixel))
-        return Promise.all(promises)
+        const featureArrays = await Promise.all(promises)
+        return new MergedFeatureCollection(featureArrays)
     }
 
     draw(options) {
 
-        const mergedFeatures = options.features    // Array of feature arrays, 1 for each track
-
-        if (this.autoscale) {
-            this.dataRange = autoscale(options.referenceFrame.chr, mergedFeatures)
-        }
+        const mergedFeatures = options.features    // A MergedFeatureCollection
 
         for (let i = 0, len = this.tracks.length; i < len; i++) {
             const trackOptions = Object.assign({}, options)
-            trackOptions.features = mergedFeatures[i]
+            trackOptions.features = mergedFeatures.featureArrays[i]
             this.tracks[i].dataRange = this.dataRange
             this.tracks[i].flipAxis = this.flipAxis
             this.tracks[i].logScale = this.logScale
-            if(this.graphType){
+            if (this.graphType) {
                 this.tracks[i].graphType = this.graphType
             }
             this.tracks[i].draw(trackOptions)
@@ -169,19 +169,29 @@ class MergedTrack extends TrackBase {
     }
 }
 
-function autoscale(chr, featureArrays) {
 
-    let min = 0
-    let max = -Number.MAX_VALUE
-    for(let features of featureArrays) {
-        for(let f of features) {
-            if (typeof f.value !== 'undefined' && !Number.isNaN(f.value)) {
-                min = Math.min(min, f.value)
+class MergedFeatureCollection {
+
+    constructor(featureArrays) {
+        this.featureArrays = featureArrays
+    }
+
+    getMax(start, end) {
+        let max = -Number.MAX_VALUE
+        for (let a of this.featureArrays) {
+            for (let f of a) {
+                if (f.end < start) {
+                    continue
+                }
+                if (f.start > end) {
+                    break
+                }
                 max = Math.max(max, f.value)
             }
         }
+        return max
     }
-    return {min: min, max: max}
+
 }
 
 export default MergedTrack

dev/wig/merged.html
@@ -25,20 +25,20 @@
             genome: "hg19",
             locus: "chr4:40,174,668-40,221,204",
             tracks: [
-                {
-                    "type": "wig",
-                    "format": "bigwig",
-                    "url": "https://www.encodeproject.org/files/ENCFF000ASJ/@@download/ENCFF000ASJ.bigWig",
-                    "color": "red",
-                    "name": "ENCFF000ASJ"
-                },
-                {
-                    "type": "wig",
-                    "format": "bigwig",
-                    "url": "https://www.encodeproject.org/files/ENCFF351WPV/@@download/ENCFF351WPV.bigWig",
-                    "color": "green",
-                    "name": "ENCFF351WPV"
-                },
+                // {
+                //     "type": "wig",
+                //     "format": "bigwig",
+                //     "url": "https://www.encodeproject.org/files/ENCFF000ASJ/@@download/ENCFF000ASJ.bigWig",
+                //     "color": "red",
+                //     "name": "ENCFF000ASJ"
+                // },
+                // {
+                //     "type": "wig",
+                //     "format": "bigwig",
+                //     "url": "https://www.encodeproject.org/files/ENCFF351WPV/@@download/ENCFF351WPV.bigWig",
+                //     "color": "green",
+                //     "name": "ENCFF351WPV"
+                // },
                 {
                     name: "Merged - autoscaled",
                     type: "merged",
@@ -58,26 +58,26 @@
                         }
                     ]
                 },
-                {
-                    name: "Merged - fixed scale",
-                    type: "merged",
-                    min: 0,
-                    max: 50,
-                    tracks: [
-                        {
-                            "type": "wig",
-                            "format": "bigwig",
-                            "url": "https://www.encodeproject.org/files/ENCFF000ASJ/@@download/ENCFF000ASJ.bigWig",
-                            "color": "red"
-                        },
-                        {
-                            "type": "wig",
-                            "format": "bigwig",
-                            "url": "https://www.encodeproject.org/files/ENCFF351WPV/@@download/ENCFF351WPV.bigWig",
-                            "color": "green"
-                        }
-                    ]
-                }
+                // {
+                //     name: "Merged - fixed scale",
+                //     type: "merged",
+                //     min: 0,
+                //     max: 50,
+                //     tracks: [
+                //         {
+                //             "type": "wig",
+                //             "format": "bigwig",
+                //             "url": "https://www.encodeproject.org/files/ENCFF000ASJ/@@download/ENCFF000ASJ.bigWig",
+                //             "color": "red"
+                //         },
+                //         {
+                //             "type": "wig",
+                //             "format": "bigwig",
+                //             "url": "https://www.encodeproject.org/files/ENCFF351WPV/@@download/ENCFF351WPV.bigWig",
+                //             "color": "green"
+                //         }
+                //     ]
+                // }
             ]
         }
 

js/trackView.js
@@ -412,17 +412,18 @@ class TrackView {
         }
 
         if (this.track.autoscale) {
-            let allFeatures = []
+            let allFeatures
             for (let visibleViewport of visibleViewports) {
                 const referenceFrame = visibleViewport.referenceFrame
                 const start = referenceFrame.start
                 const end = start + referenceFrame.toBP($(visibleViewport.contentDiv).width())
                 if (visibleViewport.featureCache && visibleViewport.featureCache.features) {
+                    // If the "features" object has a getMax function use it.  Currently only alignmentContainer implements this, for coverage.
                     if (typeof visibleViewport.featureCache.features.getMax === 'function') {
                         const max = visibleViewport.featureCache.features.getMax(start, end)
-                        allFeatures.push({value: max})
+                        allFeatures = [{value: max}]
                     } else {
-                        allFeatures = allFeatures.concat(FeatureUtils.findOverlapping(visibleViewport.featureCache.features, start, end))
+                        allFeatures = FeatureUtils.findOverlapping(visibleViewport.featureCache.features, start, end)
                     }
                 }
             }

commit 259229d730c2f8e27765aeae7a6b1f340b54db00

Fix autscale problem introduced with 4ff3cd82 for merged tracks of hetergenous track types.
Former-commit-id: c53872a0c8f0850410721caa2b91f05d4fd8ae9f

js/feature/mergedTrack.js
@@ -180,13 +180,15 @@ class MergedFeatureCollection {
         let max = -Number.MAX_VALUE
         for (let a of this.featureArrays) {
             for (let f of a) {
-                if (f.end < start) {
-                    continue
+                if (typeof f.value !== 'undefined' && !Number.isNaN(f.value)) {
+                    if (f.end < start) {
+                        continue
+                    }
+                    if (f.start > end) {
+                        break
+                    }
+                    max = Math.max(max, f.value)
                 }
-                if (f.start > end) {
-                    break
-                }
-                max = Math.max(max, f.value)
             }
         }
         return max