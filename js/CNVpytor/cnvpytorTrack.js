1b103e9733823b73912e1d3c75944d691aa83b4e
arpanda <arijpanda@gmail.com>
2023-02-03 10:14:46 -0600

    bug fix for build

--- js/CNVpytor/cnvpytorTrack.js
+++ js/CNVpytor/cnvpytorTrack.js
@@ -2,7 +2,7 @@
 * The MIT License (MIT)
 *
 * Copyright (c) 2016-2017 The Regents of the University of California
-* Author: Jim Robinson
+* Author: Arijit Panda
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
