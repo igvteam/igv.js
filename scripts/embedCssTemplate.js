87c41d40379f9966e3390a1051f173dd3f1138e2
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-02-10 18:50:04 -0800

    Use "append" to insert style into dom

--- scripts/embedCssTemplate.js
+++ scripts/embedCssTemplate.js
@@ -1,12 +1,12 @@
 function embedCSS() {
 
-    var css =  '_CSS_';
+    var css = '_CSS_'
 
-    var style = document.createElement('style');
-    style.setAttribute('type', 'text/css');
-    style.innerHTML = css;
+    var style = document.createElement('style')
+    style.setAttribute('type', 'text/css')
+    style.innerHTML = css
 
-    document.head.insertBefore(style, document.head.childNodes[ document.head.childNodes.length - 1 ]);
+    document.head.append(style)
 
 }
 
