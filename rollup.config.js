f66472e71de248fa9662a7276d84dee9cbe4aca7
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-02-24 23:31:44 -0500

    simplify build -- no Babel, require es2015+ (es6)
    Former-commit-id: e026df591ae4f83e6d6640a057f77d9e4ead78d8

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
-import nodeResolve from "@rollup/plugin-node-resolve";
-import babel from "@rollup/plugin-babel";
 import strip from '@rollup/plugin-strip';
-import commonjs from '@rollup/plugin-commonjs';
 import {terser} from "rollup-plugin-terser"
 
 export default [
@@ -31,9 +28,6 @@ export default [
                 debugger: true,
                 functions: [/*'console.log', */'assert.*', 'debug']
             }),
-            commonjs(),
-            nodeResolve(),
-            babel()
         ]
     }
 ];
