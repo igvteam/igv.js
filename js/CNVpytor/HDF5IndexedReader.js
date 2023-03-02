f3f5eb48ffab8b10fe97fd3f3e8d537a73269795
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-03-01 21:19:58 -0500

    load hdf5-indexed-reader as a dependency

--- js/CNVpytor/HDF5IndexedReader.js
+++ js/CNVpytor/HDF5IndexedReader.js
@@ -1,4 +1,4 @@
-import {openH5File} from "../vendor/hdf5-indexed-reader.esm.js"
+import {openH5File} from "../../node_modules/hdf5-indexed-reader/dist/hdf5-indexed-reader.esm.js"
 
 class HDF5Reader {
     
