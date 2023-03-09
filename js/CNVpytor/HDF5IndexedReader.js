5fc1ad82ed8930dc98ef642b49c76138e7361232
Jim Robinson <933148+jrobinso@users.noreply.github.com>
2023-03-09 00:32:50 -0500

    Cnypytor indexed (#1599)
    * pytor example update
    
    * cnvpytor auto-recognition
    
    * use indexed hdf5 reader

--- js/CNVpytor/HDF5IndexedReader.js
+++ js/CNVpytor/HDF5IndexedReader.js
@@ -1,64 +1,59 @@
-import h5wasm from "https://cdn.jsdelivr.net/npm/h5wasm@0.4.9/dist/esm/hdf5_hl.js";
+//import h5wasm from "https://cdn.jsdelivr.net/npm/h5wasm@0.4.9/dist/esm/hdf5_hl.js";
+//import h5wasm from  '../../node_modules/h5wasm/dist/esm/hdf5_hl.js'
+import {openH5File} from "../vendor/hdf5-indexed-reader.esm.js"
 
-
-class HDF5Reader {
-    /**
-     * Reads a cnvpytor file
-     * 
-     * @param {Path} h5_file - path for the pytor file
-     * @param {Number} bin_size - Bin size
-     */
+class Read_HDF5_Indexed{
+    
     constructor(h5_file, bin_size=100000){
+
         this.h5_file = h5_file;
         this.bin_size = bin_size;
-        this.random_name = "pytor_" + make_random_id(10) + ".h5"
-        
+        this.h5_obj = undefined
     }
     
-    /**
-     * 
-     * @returns h5 object
-     */
     async fetch(){
 
-        // the WASM loads asychronously, and you can get the module like this:
-        const Module = await h5wasm.ready;
-
-        // then you can get the FileSystem object from the Module:
-        const { FS } = Module;
-        let response = await fetch(this.h5_file)
-        let ab = await response.arrayBuffer();
-        FS.writeFile(this.random_name, new Uint8Array(ab));
-        let f = new h5wasm.File(this.random_name, "r");
-        return f
-        
+        if(!this.h5_obj) {
+            this.h5_obj = await openH5File({
+                url: this.h5_file,
+                fetchSize: 1000000,
+                maxSize: 200000000
+            })
+        }
+        return this.h5_obj
     }
-    /**
-     * 
-     * @returns list of keys of the pytor file
-     */
+
     async get_keys(){
+        /* returns a list of keys of the pytor file*/
         let h5_obj = await this.fetch();
-        return h5_obj.keys()
+
+        return h5_obj.keys
     }
 
     async get_rd_signal(bin_size=this.bin_size){
-        let h5_obj = await this.fetch();
-        let h5_obj_keys = h5_obj.keys();
 
+        let h5_obj = await this.fetch();
+        let h5_obj_keys = h5_obj.keys;
+
+        // console.log(h5_obj_keys)
         let signal_bin = new ParseSignals(h5_obj_keys);
         this.rd_bins = signal_bin.get_rd_bins()
 
-        
+        // let bin_size = this.bin_size
         if(! this.rd_bins.includes(bin_size)){
             bin_size = this.rd_bins[rd_bins.length-1];    
         }
-        
-        let rd_chromosomes = h5_obj.get("rd_chromosomes").value
+
+        const chr_ds = await h5_obj.get("rd_chromosomes")
+        const type = await chr_ds.dtype
+        const t0 = Date.now()
+        let rd_chromosomes = await chr_ds.value
+        //console.log(`get_rd_signal rd_chromosomes  ${Date.now() - t0}`)
+        //rd_chromosomes = fixString(rd_chromosomes)
         let rd_flag = ""
 
         // get rd stat
-        let rd_stat = this.rd_stat(h5_obj, h5_obj_keys, bin_size)
+        let rd_stat = await this.rd_stat(h5_obj, h5_obj_keys, bin_size)
         
         var wigFeatures = []
         var wigFeatures_gc = []
@@ -70,26 +65,26 @@ class HDF5Reader {
         for (let chrom of rd_chromosomes) {
             // for normal rd signal
             var signal_rd = `his_rd_p_${chrom}_${bin_size}${rd_flag}`
-            let chr_wig = this.get_chr_signal(h5_obj, h5_obj_keys, chrom, bin_size, signal_rd, rd_stat)
+            let chr_wig = await this.get_chr_signal(h5_obj, h5_obj_keys, chrom, bin_size, signal_rd, rd_stat)
             wigFeatures = wigFeatures.concat(chr_wig)
             
             // rd gc corrected
             var signal_rd_gc = `his_rd_p_${chrom}_${bin_size}_GC`
-            let chr_wig_gc = this.get_chr_signal(h5_obj, h5_obj_keys, chrom, bin_size, signal_rd_gc, rd_stat)
+            let chr_wig_gc = await this.get_chr_signal(h5_obj, h5_obj_keys, chrom, bin_size, signal_rd_gc, rd_stat)
             wigFeatures_gc = wigFeatures_gc.concat(chr_wig_gc)
 
             // rd call MeanShift
             
             let signal_rd_call = `his_rd_p_${chrom}_${bin_size}_partition_GC_merge`
-            let chr_wig_rd_call_meanshift = this.get_chr_signal(h5_obj, h5_obj_keys, chrom, bin_size, signal_rd_call, rd_stat)
+            let chr_wig_rd_call_meanshift = await this.get_chr_signal(h5_obj, h5_obj_keys, chrom, bin_size, signal_rd_call, rd_stat)
             wigFeatures_rd_call_meanshift = wigFeatures_rd_call_meanshift.concat(chr_wig_rd_call_meanshift)
             
-            let chr_wig_rd_call = this.rd_call_combined(h5_obj, h5_obj_keys, chrom, bin_size, rd_stat)
+            let chr_wig_rd_call = await this.rd_call_combined(h5_obj, h5_obj_keys, chrom, bin_size, rd_stat)
             wigFeatures_rd_call_combined = wigFeatures_rd_call_combined.concat(chr_wig_rd_call)
 
             // baf likelihood
             let signal_baf_1 = `snp_likelihood_${chrom}_${bin_size}_mask`
-            let chr_wig_bafs = this.get_baf_signals(h5_obj, h5_obj_keys, chrom, bin_size, signal_baf_1)
+            let chr_wig_bafs = await this.get_baf_signals(h5_obj, h5_obj_keys, chrom, bin_size, signal_baf_1)
 
             wigFeatures_baf1 = wigFeatures_baf1.concat(chr_wig_bafs[0])
             wigFeatures_baf2 = wigFeatures_baf2.concat(chr_wig_bafs[1])
@@ -97,18 +92,18 @@ class HDF5Reader {
         }
         this.callers = []
         if (wigFeatures_rd_call_combined.length != 0){
-            this.callers.push('ReadDepth')
+            this.callers.push('MeanShift')
         }
         if (wigFeatures_rd_call_combined.length != 0){
-            this.callers.push('2D')
+            this.callers.push('Combined')
         }
 
         var obj = {}
         var signal_obj = {
             "RD_Raw": wigFeatures,
             "RD_Raw_gc_coor" : wigFeatures_gc,
-            "ReadDepth": wigFeatures_rd_call_meanshift,
-            "2D": wigFeatures_rd_call_combined,
+            "MeanShift": wigFeatures_rd_call_meanshift,
+            "Combined": wigFeatures_rd_call_combined,
             "BAF1": wigFeatures_baf1,
             "BAF2": wigFeatures_baf2
         }
@@ -130,20 +125,25 @@ class HDF5Reader {
         }
         return segments
     }
-    rd_call_combined(h5_obj, h5_obj_keys, chrom, bin_size, rd_stat){
+
+   async  rd_call_combined(h5_obj, h5_obj_keys, chrom, bin_size, rd_stat){
         let chr_wig = [];
         
         let segments
         let mosaic_call_segments = `his_rd_p_${chrom}_${bin_size}_partition_GC_mosaic_segments_2d`
         if (h5_obj_keys.includes(mosaic_call_segments)){
-            let chrom_data = h5_obj.get(mosaic_call_segments).value
+            const chrom_dataset = await h5_obj.get(mosaic_call_segments)
+            const t0 = Date.now()
+            let chrom_data = await chrom_dataset.value
+            //console.log(`rd_call_combined ${mosaic_call_segments}  ${Date.now() - t0}`)
             segments = this.decode_segments(chrom_data)
             
         }
 
         let mosaic_calls = `his_rd_p_${chrom}_${bin_size}_partition_GC_mosaic_call_2d`
         if (h5_obj_keys.includes(mosaic_calls)){
-            let segments_call = h5_obj.get(mosaic_calls).to_array()
+            const segments_call_dataset = await h5_obj.get(mosaic_calls)
+            let segments_call = await segments_call_dataset.to_array() //create_nested_array(value, shape)
             segments.forEach((ind_segment, segment_idx) => {
                 ind_segment.forEach((bin_value, bin_idx) =>{
                     chr_wig.push({chr:chrom, start: bin_value*bin_size, end: (bin_value+1) * bin_size, value: (segments_call[0][segment_idx]/rd_stat[4]) *2})
@@ -154,42 +154,53 @@ class HDF5Reader {
         return chr_wig
         
     }
-    /**
-     * 
-     * @param {*} h5_obj - cnvpytor read object
-     * @param {*} h5_obj_keys -  a list of available signal names
-     * @param {*} bin_size - bin size
-     * @returns - a list for rd statistics information 
-     */
-    rd_stat(h5_obj, h5_obj_keys, bin_size){
+
+   async rd_stat(h5_obj, h5_obj_keys, bin_size){
+        /* 
+        returns a list for rd statistics information 
+        paramter
+        ---------
+        h5_obj: cnvpytor read object
+        h5_obj_keys: a list of available signal names
+        bin_size: bin size
+        
+        */
         
         let rd_stat_signal =  `rd_stat_${bin_size}_auto`
         let rd_stat;
         if (h5_obj_keys.includes(rd_stat_signal)){
-            rd_stat = h5_obj.get(rd_stat_signal).value
+            const rd_stat_dataset = await h5_obj.get(rd_stat_signal)
+            const t0 = Date.now()
+            rd_stat = await rd_stat_dataset.value
+            //console.log(`rd_stat_signal ${rd_stat_signal}  ${Date.now() - t0}`)
         }
         return rd_stat
     }
 
     
-    get_chr_signal(h5_obj, h5_obj_keys, chrom, bin_size, signal_name, rd_stat){
+    async get_chr_signal(h5_obj, h5_obj_keys, chrom, bin_size, signal_name, rd_stat){
         /* return a list of dictionary for a chromosome */
         let chr_wig = [];
         
         if (h5_obj_keys.includes(signal_name)){
-            let chrom_data = h5_obj.get(signal_name).value
+            const chrom_dataset = await h5_obj.get(signal_name)
+            const t0 = Date.now()
+            let chrom_data = await chrom_dataset.value
+            //console.log(`chr_signal ${signal_name}  ${Date.now() - t0}`)
             chrom_data.forEach((bin_value, bin_idx) => {
                 chr_wig.push({chr:chrom, start: bin_idx*bin_size, end: (bin_idx+1) * bin_size, value: (bin_value/rd_stat[4]) *2})
             });
         }
         return chr_wig
     }
-    get_baf_signals(h5_obj, h5_obj_keys, chrom, bin_size, signal_name){
+
+    async get_baf_signals(h5_obj, h5_obj_keys, chrom, bin_size, signal_name){
         /* return two list of dictionary*/
         let chr_wig_1 = [];
         let chr_wig_2 = [];
         if (h5_obj_keys.includes(signal_name)){
-            let chrom_data = h5_obj.get(signal_name).to_array();
+            let chrom_dataset = await h5_obj.get(signal_name)
+            let chrom_data = await chrom_dataset.to_array() //create_nested_array(value, shape)
             chrom_data.forEach((bin_value, bin_idx) => {
                 let max_value =  Math.max(...bin_value);
                 const res = bin_value.indexOf(max_value);
@@ -205,6 +216,7 @@ class HDF5Reader {
 }
 
 class ParseSignals{
+
     constructor(signals){
         /*
         Parse a signal names
@@ -215,6 +227,7 @@ class ParseSignals{
         */
         this.signals = signals
     }
+
     get_rd_bins(){
         /* return list of rd bins */
         let rd_keys = [];
@@ -240,16 +253,47 @@ class ParseSignals{
     }
 }
 
-function make_random_id(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+function fixString(strings) {
+
+    return strings.map(s => s.substr(0,s.indexOf('\0')))
+
+}
+
+// function to_array(value, shape) {
+//     const { json_value, metadata } = this;
+//     const { shape } = metadata;
+//     if (!isIterable(json_value) || typeof json_value === "string") {
+//         return json_value;
+//     }
+//     let nested = create_nested_array(json_value, shape);
+//     return nested;
+// }
+
+function create_nested_array(value, shape) {
+    // check that shapes match:
+    const total_length = value.length;
+    const dims_product = shape.reduce((previous, current) => (previous * current), 1);
+    if (total_length !== dims_product) {
+        console.warn(`shape product: ${dims_product} does not match length of flattened array: ${total_length}`);
     }
-    return result;
+    // Get reshaped output:
+    let output = value;
+    const subdims = shape.slice(1).reverse();
+    for (let dim of subdims) {
+        // in each pass, replace input with array of slices of input
+        const new_output = [];
+        const { length } = output;
+        let cursor = 0;
+        while (cursor < length) {
+            new_output.push(output.slice(cursor, cursor += dim));
+        }
+        output = new_output;
+    }
+    return output;
 }
 
 
 
-export default HDF5Reader;
+
+
+export default Read_HDF5_Indexed
