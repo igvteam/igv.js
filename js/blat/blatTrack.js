1f6191dbf9cddd1164888391f94b85af3b876ec6
Douglass Turner <douglass.turner@gmail.com>
2023-03-09 10:50:43 -0500

    BlatTrack - BlatTable integration.

--- js/blat/blatTrack.js
+++ js/blat/blatTrack.js
@@ -1,8 +1,5 @@
-import {IGVTable} from '../../node_modules/igv-ui/dist/igv-ui.js'
 import FeatureTrack from "../feature/featureTrack.js"
-import {renderSnp} from "../feature/render/renderSnp.js"
-import $ from "../vendor/jquery-3.3.1.slim.js"
-import {createCheckbox} from "../igv-icons.js"
+import BlatTable from "./blatTable.js"
 
 
 class BlatTrack extends FeatureTrack {
@@ -13,47 +10,57 @@ class BlatTrack extends FeatureTrack {
         if (this.name) {
             this.name = 'Blat Results'
         }
+
+        this.table = undefined
     }
 
     openTableView() {
-        if (!this.table) {
-            const tableData = {
-                title: "<b>BLAT Results</b>:  click on row to go to alignment",
-                headers: ["chr", "start", "end", "strand", "score", "match", "mis-match", "rep. match", "N's", "Q gap count", "Q gap bases", "T gap count", "T gap bases"],
-                rows: this.config.features.map(f => [
-                    f.chr,
-                    (f.start + 1),
-                    f.end,
-                    f.strand,
-                    f.score,
-                    f.matches,
-                    f.misMatches,
-                    f.repMatches,
-                    f.nCount,
-                    f.qNumInsert,
-                    f.qBaseInsert,
-                    f.tNumInsert,
-                    f.tBaseInsert
-                ]),
-                rowClickHandler: (rowData) => {
-                    console.log(`${rowData[0]}:${parseInt(rowData[1]) - 1}-${rowData[2]}`)
+
+        if (undefined === this.table) {
+
+            // const dev_null = this.config.features.map(feature => {
+            //     const kv = Object.entries(feature)
+            //     return kv
+            // })
+
+
+            const rows = this.config.features.map(f => [
+                f.chr,
+                (f.start + 1),
+                f.end,
+                f.strand,
+                f.score,
+                f.matches,
+                f.misMatches,
+                f.repMatches,
+                f.nCount,
+                f.qNumInsert,
+                f.qBaseInsert,
+                f.tNumInsert,
+                f.tBaseInsert
+            ])
+
+            const config =
+                {
+                    browser: this.browser,
+                    parent: this.browser.parent,
+                    headerTitle: this.name,
+                    description: '<h2>This is the description</h2>',
+                    dismissHandler: () => {
+                        this.table.dismiss()
+                        this.table.dispose()
+                        this.table = undefined
+                    },
+                    columnFormat: BlatTable.getColumnFormatConfiguration(),
+                    gotoButtonHandler: BlatTable.gotoButtonHandler
                 }
-            }
-            this.table = new IGVTable(this.browser.root, tableData)
+
+            this.table = new BlatTable(config)
+            this.table.renderTable(rows)
         }
-        this.table.show()
 
-        const config =
-            {
-                browser,
-                parent: document.getElementById('igv-container'),
-                headerTitle: 'BLAT Results',
-                dismissHandler: () => blatTable.dismiss(),
-                columnFormat: BlatTable.getColumnFormatConfiguration(),
-                gotoButtonHandler: BlatTable.gotoButtonHandler
-            }
+        this.table.present()
 
-        const blatTable = new BlatTable(config)
     }
 
     menuItemList() {
