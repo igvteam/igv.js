d63c1039b66933c9f68f26ca36fbb7d7ca1a14cd
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-03-08 23:26:41 -0800

    * fix bug in igv-ui import from regionTableBase. * add table view option to blat track menu
    Former-commit-id: 54f000a6e400f3c97e68d6eeda7204a7336327f0

--- js/blat/blatTrack.js
+++ js/blat/blatTrack.js
@@ -1,5 +1,8 @@
 import {IGVTable} from '../../node_modules/igv-ui/dist/igv-ui.js'
 import FeatureTrack from "../feature/featureTrack.js"
+import {renderSnp} from "../feature/render/renderSnp.js"
+import $ from "../vendor/jquery-3.3.1.slim.js"
+import {createCheckbox} from "../igv-icons.js"
 
 
 class BlatTrack extends FeatureTrack {
@@ -7,10 +10,13 @@ class BlatTrack extends FeatureTrack {
 
     constructor(config, browser) {
         super(config, browser)
+        if (this.name) {
+            this.name = 'Blat Results'
+        }
     }
 
     openTableView() {
-        if(!this.table) {
+        if (!this.table) {
             const tableData = {
                 title: "<b>BLAT Results</b>:  click on row to go to alignment",
                 headers: ["chr", "start", "end", "strand", "score", "match", "mis-match", "rep. match", "N's", "Q gap count", "Q gap bases", "T gap count", "T gap bases"],
@@ -36,19 +42,43 @@ class BlatTrack extends FeatureTrack {
             this.table = new IGVTable(this.browser.root, tableData)
         }
         this.table.show()
+
+        const config =
+            {
+                browser,
+                parent: document.getElementById('igv-container'),
+                headerTitle: 'BLAT Results',
+                dismissHandler: () => blatTable.dismiss(),
+                columnFormat: BlatTable.getColumnFormatConfiguration(),
+                gotoButtonHandler: BlatTable.gotoButtonHandler
+            }
+
+        const blatTable = new BlatTable(config)
     }
+
+    menuItemList() {
+
+        const menuItems = super.menuItemList()
+
+        menuItems.push('<hr/>')
+        menuItems.push({
+            label: 'Open table view',
+            click: () => this.openTableView()
+        })
+        return menuItems
+    }
+
 
     /**
      * Track has been permanently removed.  Release resources and other cleanup
      */
     dispose() {
         super.dispose()
-        // Release DOM element for table -- todo move this to IGVTable class "dispose" method
-        if(this.table) {
+        // Release DOM element for table
+        if (this.table) {
             this.table.popover.parentElement.removeChild(this.table.popover)
 
         }
-
 
 
     }
