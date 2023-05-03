7db369d1bef437c7bd635b4fc6de67f66c92f4f5
Douglass Turner <douglass.turner@gmail.com>
2023-05-03 07:48:59 -0400

    refactor
    Former-commit-id: bddf7ef6a5c50ce58ed01f2bf1ae2f48cfa32c6f

--- js/sample/sampleInfo.js
+++ js/sample/sampleInfo.js
@@ -23,21 +23,14 @@ const sampleInfo =
 
         processSampleInfoFileAsString: async (browser, string) => {
 
-            const raw = string.split('#').filter(line => line.length > 0)
+            const parts = string.split('#').filter(line => line.length > 0)
 
-            // const cooked = []
-            // for (let part of raw) {
-            //     const tmp = part.replace(/\t+/g, "\t")
-            //     cooked.push(tmp)
-            //
-            // }
-
-            createSampleDictionary(raw[0], raw.length > 0)
+            updateSampleDictionary(parts[0], parts.length > 0)
 
             let copyNumber
-            if (raw.length > 0) {
+            if (parts.length > 0) {
 
-                [ copyNumber ] = raw.filter(string => string.startsWith('copynumber'))
+                [ copyNumber ] = parts.filter(string => string.startsWith('copynumber'))
                 copyNumber = copyNumber.split('\r')
                 copyNumber.shift()
 
@@ -48,55 +41,10 @@ const sampleInfo =
 
             }
 
-            attributeRangeLUT = sampleInfo.createAttributeRangeLUT()
+            attributeRangeLUT = createAttributeRangeLUT(sampleDictionary)
 
             await SampleInfoViewport.update(browser)
 
-        },
-
-        createAttributeRangeLUT: () => {
-
-            const lut= {}
-            for (const value of Object.values(sampleDictionary)) {
-
-                for (const attribute of attributes) {
-
-                    let item = value[ attribute ]
-
-                    if (undefined === lut[ attribute ]) {
-                        lut[ attribute ] = []
-                    }
-
-                    lut[ attribute ].push(item)
-
-                } // for (attributes)
-
-            } // for (Object.values(sampleDictionary))
-
-            // clean up oddball cases.
-            const isNumber = element => typeof element === 'number'
-            const isString = element => typeof element === 'string'
-
-            // remove duplicates
-            for (const key of Object.keys(lut)) {
-                const multiples = lut[ key ]
-                const set = new Set( multiples )
-                const list = Array.from( set )
-
-                if (true === list.some(isString) && true === list.some(isNumber)) {
-                    lut[ key ] = list.filter(item => !isString(item))
-                } else {
-                    lut[ key ] = list
-                }
-
-                if (!lut[ key ].some(isString)) {
-                    const clone = lut[ key ].slice()
-                    lut[ key ] = [ Math.min(...clone), Math.max(...clone) ]
-                }
-
-            }
-
-            return lut
         },
 
         getAttributeColor: (attribute, value) => {
@@ -165,92 +113,57 @@ const sampleInfo =
 
             return -1 === sortDirection ? [ ...numbers, ...strings ] : [ ...strings, ...numbers ]
 
-        },
-
-        ____processSampleInfoFileAsString: async (browser, string) => {
-
-            const lines = string.split('\r').filter(line => line.length > 0)
-
-            const sampleTable = []
-            const copyNumber = []
-
-            let isSampleTable = false
-            let isCopyNumber = false
-            for (const line of lines) {
-
-                if (line.startsWith('#sampleTable')) {
-                    isSampleTable = true
-                    isCopyNumber = false
-                }
-
-                if (line.startsWith('#sampleMapping') || line.startsWith('#mutations') || line.startsWith('#colors')) {
-                    isSampleTable = false
-                    isCopyNumber = false
-                }
-
-                if (line.startsWith('#copynumber')) {
-                    isSampleTable = false
-                    isCopyNumber = true
-                }
-
-                if (false === isCopyNumber && true === isSampleTable) {
-                    sampleTable.push(line)
-                }
-
-                if (true === isCopyNumber && false === isSampleTable) {
-                    copyNumber.push(line)
-                }
-            }
-
-            sampleTable.shift()
-            copyNumber.shift()
-
-            await sampleInfo.buildDictionaries(browser, sampleTable, copyNumber)
-        },
-
-        ____buildDictionaries: async (browser, sampleTable, copyNumber) => {
-
-            const lines = sampleTable.slice()
-
-            const scratch = lines.shift().split('\t')
-            scratch.shift()
-
-            attributes = scratch.map(label => label.split(' ').join('_'))
-
-            // sampleDictionary = {}
-
-            for (const line of lines) {
-
-                const record = line.split('\t')
-                const _key_ = record.shift()
-
-                sampleDictionary[ _key_ ] = {}
-                for (let i = 0; i < record.length; i++) {
-                    const obj = {}
-                    obj[ attributes[ i ] ] = record[ i ]
-                    Object.assign(sampleDictionary[ _key_ ], obj)
-                }
-
-            } // for (lines)
-
-            for (const [ key, record ] of Object.entries(sampleDictionary)) {
-                sampleDictionary[ key ] = toNumericalRepresentation(record)
-            }
-
-            for (const line of copyNumber) {
-                const [ a, b ] = line.split('\t')
-                copyNumberDictionary[ a ] = Object.assign({}, sampleDictionary[ b ])
-            }
-
-            sampleInfo.createAttributeRangeLUT()
-
-            await SampleInfoViewport.update(browser)
-        },
+        }
     };
 
-function createSampleDictionary(string, doSampleMapping) {
+function createAttributeRangeLUT(dictionary) {
 
-    const lines = string.split('\r')
+    const lut= {}
+    for (const value of Object.values(dictionary)) {
+
+        for (const attribute of attributes) {
+
+            let item = value[ attribute ]
+
+            if (undefined === lut[ attribute ]) {
+                lut[ attribute ] = []
+            }
+
+            lut[ attribute ].push(item)
+
+        } // for (attributes)
+
+    } // for (Object.values(sampleDictionary))
+
+    // clean up oddball cases.
+    const isNumber = element => typeof element === 'number'
+    const isString = element => typeof element === 'string'
+
+    // remove duplicates
+    for (const key of Object.keys(lut)) {
+        const multiples = lut[ key ]
+        const set = new Set( multiples )
+        const list = Array.from( set )
+
+        if (true === list.some(isString) && true === list.some(isNumber)) {
+            lut[ key ] = list.filter(item => !isString(item))
+        } else {
+            lut[ key ] = list
+        }
+
+        if (!lut[ key ].some(isString)) {
+            const clone = lut[ key ].slice()
+            lut[ key ] = [ Math.min(...clone), Math.max(...clone) ]
+        }
+
+    }
+
+    return lut
+}
+
+function updateSampleDictionary(sampleTableAsString, doSampleMapping) {
+
+    const lines = sampleTableAsString.split('\r')
 
     // discard "sampleTable"
     if (true === doSampleMapping) {
@@ -264,7 +177,6 @@ function createSampleDictionary(string, doSampleMapping) {
     if (true === doSampleMapping) {
         scratch.shift()
     }
-
 
     attributes = scratch.map(label => label.split(' ').join('_'))
 
