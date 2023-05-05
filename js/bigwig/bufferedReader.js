48c31ddc818b520f435f7ec4f3b8e3d0291e29cf
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-05-05 18:14:28 -0400

    Retry bigwig range queries on 416 failure
    # Conflicts:
    #	.gitignore

--- js/bigwig/bufferedReader.js
+++ js/bigwig/bufferedReader.js
@@ -41,30 +41,45 @@ class BufferedReader {
      * @param fulfill - function to receive result
      * @param asUint8 - optional flag to return result as an UInt8Array
      */
-    async dataViewForRange(requestedRange, asUint8) {
+    async dataViewForRange(requestedRange, asUint8, retries = 0) {
+        try {
+            console.log(`buffered reader ${requestedRange}`)
+            const hasData = (this.data && (this.range.start <= requestedRange.start) &&
+                ((this.range.start + this.range.size) >= (requestedRange.start + requestedRange.size)))
 
-        const hasData = (this.data && (this.range.start <= requestedRange.start) &&
-            ((this.range.start + this.range.size) >= (requestedRange.start + requestedRange.size)))
-
-        if (!hasData) {
-            let bufferSize
-            // If requested range size is specified, potentially expand buffer size
-            if (requestedRange.size) {
-                bufferSize = Math.max(this.bufferSize, requestedRange.size)
-            } else {
-                bufferSize = this.bufferSize
+            if (!hasData) {
+                let bufferSize
+                // If requested range size is specified, potentially expand buffer size
+                if (requestedRange.size) {
+                    bufferSize = Math.max(this.bufferSize, requestedRange.size)
+                } else {
+                    bufferSize = this.bufferSize
+                }
+                if (this.contentLength) {
+                    bufferSize = Math.min(bufferSize, this.contentLength - requestedRange.start)
+                }
+                const loadRange = {start: requestedRange.start, size: bufferSize}
+                const arrayBuffer = await igvxhr.loadArrayBuffer(this.path, buildOptions(this.config, {range: loadRange}))
+                this.data = arrayBuffer
+                this.range = loadRange
             }
-            const loadRange = {start: requestedRange.start, size: bufferSize}
-            const arrayBuffer = await igvxhr.loadArrayBuffer(this.path, buildOptions(this.config, {range: loadRange}))
-            this.data = arrayBuffer
-            this.range = loadRange
-        }
 
-        const len = this.data.byteLength
-        const bufferStart = requestedRange.start - this.range.start
-        return asUint8 ?
-            new Uint8Array(this.data, bufferStart, len - bufferStart) :
-            new DataView(this.data, bufferStart, len - bufferStart)
+            const len = this.data.byteLength
+            const bufferStart = requestedRange.start - this.range.start
+            return asUint8 ?
+                new Uint8Array(this.data, bufferStart, len - bufferStart) :
+                new DataView(this.data, bufferStart, len - bufferStart)
+        } catch (e) {
+            if (retries === 0 && e.message && e.message.startsWith("416")) {
+                try {
+                    this.contentLength = await igvxhr.getContentLength(this.path, buildOptions(this.config))
+                    return this.dataViewForRange(requestedRange, asUint8, ++retries)
+                } catch (e1) {
+                    console.error(e1)
+                }
+                throw e
+            }
+        }
     }
 }
 
