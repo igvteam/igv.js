693c64b89872ac396d816ee9789bda434d24f1e7
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-05-31 12:52:50 -0700

    Fix shoebox example
    Former-commit-id: f0bc92b0add3b3809358f5ef2df2ba2535f9bc6b

--- dev/shoebox/bedToSampleInfo.cjs
+++ dev/shoebox/bedToSampleInfo.cjs
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
+const resolution = 10
+const samples = new Set()
 const colors = new Map()
-
 const data = fs.readFileSync('./celltypeAnnotation.bed', {encoding: 'utf-8'})
 const lines = data.split('\n')
 let output = '#sampleTable\n'
-
 output += 'sample\tcelltype\n'
+
 for(let l of lines) {
     if(l.startsWith('track')) continue
 
@@ -18,16 +19,20 @@ for(let l of lines) {
     const color = tokens[8]
     colors.set(cellType, color)
 
-    const start = parseInt(tokens[1])
+    const start = parseInt(tokens[1]) - 1
     const end = parseInt(tokens[2])
-    for(let c = start; c<= end; c++) {
-        output += `${c}\t${cellType}\n`
+    for(let p = start; p<= end; p++) {
+        const sample = Math.floor(p / 10)
+        if(!samples.has(sample)) {
+            samples.add(sample)
+            output += `${sample}\t${cellType}\n`
+        }
     }
 }
 
 output += '\n#colors\n'
 for(let k of colors.keys()) {
-    output += `celltype\t${k}\t${colors.get(k)}\n`
+    output += `celltypeYea \t${k}\t${colors.get(k)}\n`
 }
 
 fs.writeFileSync('./sampleInfo.txt', output, {encoding: 'utf-8'})
