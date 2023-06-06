b78143f4219ccaa80d6c23a4ce1b989c39718856
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-06-06 11:13:02 -0700

    Add basemod classes

---
+++ js/bam/mods/baseModificationColors.js
@@ -0,0 +1,130 @@
+import {byteToUnsignedInt} from "./baseModificationUtils.js"
+import {IGVColor} from "../../../node_modules/igv-utils/src/igv-color.js"
+
+/**
+ * C Modifications
+ * C m 5mC 5-Methylcytosine 27551
+ * C h 5hmC 5-Hydroxymethylcytosine 76792
+ * C f 5fC 5-Formylcytosine 76794
+ * C c 5caC 5-Carboxylcytosine 76793
+ * C C Ambiguity code; any C mod
+ */
+
+const mColor = "rgb(255,0,0)"
+const hColor = "rgb(11, 132, 165)"
+const oColor = "rgb(111, 78, 129)"
+const fColor = "rgb(246, 200, 95)"
+const cColor = "rgb(157, 216, 102)"
+const gColor = "rgb(255, 160, 86)"
+const eColor = "rgb(141, 221, 208)"
+const bColor = "rgb(202, 71, 47)"
+const noModColor5MC = "rgb(0,0,255)"
+const genericColor = "rgb(132, 178, 158)"
+
+const colors = new Map()
+colors.set("mColor", mColor)
+colors.set("hColor", hColor)
+colors.set("oColor", oColor)
+colors.set("fColor", fColor)
+colors.set("cColor", cColor)
+colors.set("gColor", gColor)
+colors.set("eColor", eColor)
+colors.set("bColor", bColor)
+colors.set("genericColor", genericColor)
+colors.set("noModColor5MC", noModColor5MC)
+
+
+const colors5MC = new Map()
+colors5MC.set("m", mColor)
+colors5MC.set("h", hColor)
+colors5MC.set("o", oColor)
+colors5MC.set("f", fColor)
+colors5MC.set("c", cColor)
+colors5MC.set("g", gColor)
+colors5MC.set("e", eColor)
+colors5MC.set("b", bColor)
+colors5MC.set("h", "rgb(255, 0, 255)")
+
+/**
+ * Cache for modified colors
+ */
+const modColorMap = new Map()
+const modColorMap5MC = new Map()
+
+//String modification, byte likelihood, AlignmentTrack.ColorOption colorOption
+function getModColor(modification, likelihood, colorOption) {
+
+    let baseColor = getBaseColor(modification, colorOption)
+
+    let l = byteToUnsignedInt(likelihood)
+    if (l > 255) {
+        return baseColor
+    }
+
+    const key = modification + "--" + l
+    if (colorOption === "BASE_MODIFICATION_5MC" ||
+        colorOption === "BASE_MODIFICATION_C") {
+
+        if (!modColorMap5MC.has(key)) {
+            const alpha = Math.min(255, Math.floor((l * l / 64 - 4 * l + 256)))    // quadratic
+            if (l >= 128) {
+                const [r, g, b] = IGVColor.rgbComponents(baseColor)
+                modColorMap5MC.set(key, `rgba(${r},${g},${b},${alpha}`)
+            } else {
+                const [r, g, b] = IGVColor.rgbComponents(noModColor5MC)
+                modColorMap5MC.set(key, `rgba(${r},${g},${b},${alpha}`)
+            }
+        }
+
+        return modColorMap5MC.get(key)
+
+    } else {
+        if (l > 250) {
+            return baseColor
+        }
+        const threshold = 256 * 0 //PreferencesManager.getPreferences().getAsFloat("SAM.BASEMOD_THRESHOLD");
+        if (l < threshold) {
+            l = 0
+        }
+        if (!modColorMap.has(key)) {
+            const [r, g, b] = IGVColor.rgbComponents(baseColor)
+            modColorMap.set(key, `rgba(${r},${g},${b},${l}`)
+        }
+        return modColorMap.get(key)
+    }
+}
+
+function getNoModColor(likelihood) {
+
+    // Note the pallete will always return a color, either an initially seeded one if supplied or a random color.
+    const baseColor = noModColor5MC
+
+    const l = byteToUnsignedInt(likelihood)
+    if (l > 255) {
+        return baseColor
+    }
+
+    const key = "NOMOD--" + l
+
+    if (!modColorMap5MC.has(key)) {
+        const alpha = Math.min(255, Math.floor(l * l / 64 - 4 * l + 256))    // quadratic
+        const [r, g, b] = IGVColor.rgbComponents(baseColor)
+        modColorMap5MC.set(key, `rgba(${r},${g},${b},${alpha}`)
+
+    }
+
+    return modColorMap5MC.get(key)
+
+}
+
+function getBaseColor(modification, colorOption) {
+    if ((colorOption === "BASE_MODIFICATION_5MC" || colorOption === "BASE_MODIFICATION_C") && colors5MC.has(modification)) {
+        return colors5MC.get(modification)
+    } else if (colors.has(modification)) {
+        return colors.get(modification)
+    } else {
+        return genericColor
+    }
+}
+
+export {getModColor, getNoModColor}
---
+++ js/bam/mods/baseModificationRenderer.js
@@ -0,0 +1,186 @@
+/*
+            Alignment alignment,
+            double bpStart,
+            double locScale,
+            Rectangle rowRect,
+            Graphics g,
+            AlignmentTrack.ColorOption colorOptio
+ */
+
+import {byteToUnsignedInt} from "./baseModificationUtils.js"
+import {getModColor, getNoModColor} from "./baseModificationColors.js"
+
+function drawModifications(alignment, bpStart, locScale, rowRect, ctx, colorOption) {
+
+    switch (colorOption) {
+        case "BASE_MODIFICATION_5MC":
+            draw5mC(alignment, bpStart, locScale, rowRect, ctx, false)
+            break
+        case "BASE_MODIFICATION_C":
+            draw5mC(alignment, bpStart, locScale, rowRect, ctx, true)
+            break
+
+        //  default:
+        //      draw(alignment, bpStart, locScale, rowRect, g);
+    }
+
+}
+
+/**
+ * Helper function for AlignmentRenderer.  Draw base modifications over alignment.
+ *
+ * @param alignment
+ * @param bpStart
+ * @param locScale
+ * @param rowRect
+ * @param g
+ */
+// private static void draw(
+//         Alignment alignment,
+//         double bpStart,
+//         double locScale,
+//         Rectangle rowRect,
+//         Graphics g) {
+//
+//     List<BaseModificationSet> baseModificationSets = alignment.getBaseModificationSets();
+//
+//     if (baseModificationSets != null) {
+//         for (AlignmentBlock block : alignment.getAlignmentBlocks()) {
+//             // Compute bounds
+//             int pY = (int) rowRect.getY();
+//             int dY = (int) rowRect.getHeight();
+//             int dX = (int) Math.max(1, (1.0 / locScale));
+//
+//             for (int i = block.getBases().startOffset; i < block.getBases().startOffset + block.getBases().length; i++) {
+//
+//                 int blockIdx = i - block.getBases().startOffset;
+//                 int pX = (int) ((block.getStart() + blockIdx - bpStart) / locScale);
+//
+//                 // Don't draw out of clipping rect
+//                 if (pX > rowRect.getMaxX()) {
+//                     break;
+//                 } else if (pX + dX < rowRect.getX()) {
+//                     continue;
+//                 }
+//
+//                 // Search all sets for modifications of this base.  For now keeps mod with > probability
+//                 // TODO -- merge mods in some way
+//                 byte lh = 0;
+//                 String modification = null;
+//                 for (BaseModificationSet bmSet : baseModificationSets) {
+//                     if (bmSet.containsPosition(i)) {
+//                         if (modification == null || Byte.toUnsignedInt(bmSet.getLikelihoods().get(i)) > Byte.toUnsignedInt(lh)) {
+//                             modification = bmSet.getModification();
+//                             lh = bmSet.getLikelihoods().get(i);
+//                         }
+//                     }
+//                 }
+//
+//                 if (modification != null) {
+//
+//                     Color c = BaseModificationColors.getModColor(modification, lh, AlignmentTrack.ColorOption.BASE_MODIFICATION);
+//                     g.setColor(c);
+//
+//                     // Expand narrow width to make more visible
+//                     if (dX < 3) {
+//                         dX = 3;
+//                         pX--;
+//                     }
+//                     g.fillRect(pX, pY, dX, Math.max(1, dY - 2));
+//                 }
+//             }
+//         }
+//     }
+// }
+
+/**
+ * Helper function for AlignmentRenderer.  Draw base modifications over alignment for "5mC" mode.
+ * <p>
+ * Notes:
+ * Designed primarily for visualization of 5mC modifications compatible with existing bisulfite seq viz
+ * - 5mC methylated bases colored red
+ * - Non modified bases colored blue
+ * - Other modificationc colored as defined in BaseModificationColors
+ * <p>
+ * If multiple modifications are specified for a base the modification with the highest probability is
+ * drawn.
+ *
+ * @param alignment
+ * @param bpStart
+ * @param locScale
+ * @param rowRect
+ * @param ctx
+ */
+function draw5mC(alignment, bpStart, locScale, rowRect, ctx, allMods) {
+
+    const baseModificationSets = alignment.getBaseModificationSets()
+    if (baseModificationSets != null) {
+
+        /*
+         start: scPos,
+                seqOffset: seqOffset,
+                len: c.len,
+                type: 'S'
+         */
+
+        for (let block of alignment.blocks) {
+            // Compute bounds
+            let pY = rowRect.y
+            const dY = rowRect.height
+            let dX = Math.max(1, (1.0 / locScale))
+
+            for (let i = block.start; i < block.start + block.len; i++) {
+
+                const blockIdx = i - block.start
+                let pX = ((block.start + blockIdx - bpStart) / locScale)
+
+                // Don't draw out of clipping rect
+                if (pX > rowRect.x + rowRect.width) {
+                    break
+                } else if (pX + dX < rowRect.x) {
+                    continue
+                }
+
+                // Search all sets for modifications of this base, select modification with largest likelihood
+                let lh = -1
+                let modification
+
+                // Compare likelihoods, including likelihood of no modification
+                let noModificationLikelihood = 255
+                for (let bmSet of baseModificationSets) {
+
+                    // This coloring mode is exclusively for "C" modifications, either 5mC or all C mods
+                    if (bmSet.getCanonicalBase() != 'C') continue
+                    if (bmSet.modification === "m" || allMods) {
+                        if (bmSet.containsPosition(i)) {
+                            const l = byteToUnsignedInt(bmSet.likelihoods.get(i))
+                            noModificationLikelihood -= l
+                            if (!modification || l > lh) {
+                                modification = bmSet.modification
+                                lh = l
+                            }
+                        }
+                    }
+                }
+
+                if (modification) {
+
+                    const c = noModificationLikelihood > lh ?
+                        getNoModColor(noModificationLikelihood) :
+                        getModColor(modification, lh, "BASE_MODIFICATION_5MC")
+                    ctx.fillStyle = c
+
+                    // Expand narrow width to make more visible
+                    if (dX < 3) {
+                        dX = 3
+                        pX--
+                    }
+                    ctx.fillRect(pX, pY, dX, Math.max(1, dY - 2))
+                }
+            }
+        }
+    }
+}
+
+export {drawModifications}
+
