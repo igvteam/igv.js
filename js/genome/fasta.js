bd6ac83090fcdcbf4a05abd25c1e3ada8c8408ff
Jim Robinson <933148+jrobinso@users.noreply.github.com>
2023-09-19 19:08:45 -0700

    Implement two-bit reader (#1690)
    Former-commit-id: b5f501688d7aa3f8486a57fb6ecc5d598cbffda2

--- js/genome/fasta.js
+++ js/genome/fasta.js
@@ -2,12 +2,15 @@ import NonIndexedFasta from "./nonIndexedFasta.js"
 import FastaSequence from "./indexedFasta.js"
 import {isDataURL} from "../util/igvUtils.js"
 import ChromSizes from "./chromSizes.js"
+import Twobit from "./twobit.js"
 
 async function loadFasta(reference) {
 
     let fasta
     if ("chromsizes" === reference.format) {
         fasta = new ChromSizes(reference.fastaURL)
+    } else if ("2bit" === reference.format) {
+        fasta = new Twobit(reference.fastaURL)
     } else if (isDataURL(reference.fastaURL) || reference.indexed === false) {
         fasta = new NonIndexedFasta(reference)
     } else {
