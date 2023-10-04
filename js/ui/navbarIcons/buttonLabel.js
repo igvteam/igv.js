26bf8c529669f9cfa71ae5f2840b2b4585c688bf
turner <douglass.turner@gmail.com>
2023-10-04 11:01:50 -0400

    Navbar - Text Button width futzing

--- js/ui/navbarIcons/buttonLabel.js
+++ js/ui/navbarIcons/buttonLabel.js
@@ -1,9 +1,9 @@
 
 const buttonLabel =
-    `<svg width="80px" height="18px" viewBox="0 0 80 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    `<svg width="90px" height="20px" viewBox="0 0 90 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="igv-navbar-button-group">
-            <rect id="Rectangle" x="0.5" y="0.5" width="79" height="17" rx="6"></rect>
+            <rect id="Rectangle" x="0.5" y="0.5" width="89" height="18" rx="6"></rect>
             <text id="Track-Labels" font-family="Helvetica" font-size="12" font-weight="normal" letter-spacing="-0.372">
                 <tspan id="igv-navbar-button-label" x="7.40407031" y="13"></tspan>
             </text>
