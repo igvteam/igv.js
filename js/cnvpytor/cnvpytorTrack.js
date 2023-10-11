b055f06947fb69cc56ca90739acf6fbae3dd4ba1
arpanda <arijpanda@gmail.com>
2023-10-11 12:49:41 -0500

    cnvpytor track update: improve y axis labels

--- js/cnvpytor/cnvpytorTrack.js
+++ js/cnvpytor/cnvpytorTrack.js
@@ -25,7 +25,6 @@
 */
 
 import TrackBase from "../trackBase.js"
-import paintAxis from "../util/paintAxis.js"
 import MenuUtils from "../ui/menuUtils.js"
 import HDF5IndexedReader from "./HDF5IndexedReader.js"
 import {CNVpytorVCF} from "./cnvpytorVCF.js"
@@ -47,7 +46,6 @@ class CNVPytorTrack extends TrackBase {
     constructor(config, browser) {
         super(config, browser)
         this.featureType = 'numeric'
-        this.paintAxis = paintAxis
         
         if (!config.max) {
             this.defaultScale = true
@@ -464,8 +462,96 @@ class CNVPytorTrack extends TrackBase {
                 }
             }
         }
-    }
 
+        let props = {
+            'strokeStyle': 'lightgray',
+            'strokeWidth': 0.5
+        }
+        let y = yScale(2);
+        IGVGraphics.dashedLine(options.context, 0, y, options.pixelWidth, y, 5, props)
+        
+    }
+    paintAxis(ctx, pixelWidth, pixelHeight) {
+
+        var x1,
+            x2,
+            y1,
+            y2,
+            a,
+            b,
+            reference,
+            shim,
+            font = {
+                'font': 'normal 10px Arial',
+                'textAlign': 'right',
+                'strokeStyle': "black"
+            }
+    
+        if (undefined === this.dataRange || undefined === this.dataRange.max || undefined === this.dataRange.min) {
+            return
+        }
+    
+        let flipAxis = (undefined === this.flipAxis) ? false : this.flipAxis
+    
+        IGVGraphics.fillRect(ctx, 0, 0, pixelWidth, pixelHeight, {'fillStyle': "rgb(255, 255, 255)"})
+    
+        reference = 0.95 * pixelWidth
+        x1 = reference - 8
+        x2 = reference
+    
+        //shim = 0.5 * 0.125;
+        shim = .01
+        y1 = y2 = shim * pixelHeight
+    
+        a = {x: x2, y: y1}
+    
+        // tick
+        IGVGraphics.strokeLine(ctx, x1, y1, x2, y2, font)
+        IGVGraphics.fillText(ctx, prettyPrint(flipAxis ? this.dataRange.min : this.dataRange.max), x1 + 4, y1 + 12, font)
+    
+        //shim = 0.25 * 0.125;
+        y1 = y2 = (1.0 - shim) * pixelHeight
+    
+        b = {x: x2, y: y1}
+    
+        // tick
+        IGVGraphics.strokeLine(ctx, x1, y1, x2, y2, font)
+        IGVGraphics.fillText(ctx, prettyPrint(flipAxis ? this.dataRange.max : this.dataRange.min), x1 + 4, y1 - 4, font)
+    
+        IGVGraphics.strokeLine(ctx, a.x, a.y, b.x, b.y, font)
+    
+        function prettyPrint(number) {
+            // if number >= 100, show whole number
+            // if >= 1 show 1 significant digits
+            // if <  1 show 2 significant digits
+    
+            if (number === 0) {
+                return "0"
+            } else if (Math.abs(number) >= 10) {
+                return number.toFixed()
+            } else if (number %1 == 0){
+                return number.toFixed()
+            }
+            else if (Math.abs(number) >= 1) {
+                return number.toFixed(1)
+            } else {
+                return number.toFixed(2)
+            }
+        }
+        const scaleFactor = this.getScaleFactor(this.dataRange.min, this.dataRange.max, pixelHeight, this.logScale)
+        const yScale = (yValue) => this.logScale
+            ? this.computeYPixelValueInLogScale(yValue, scaleFactor)
+            : this.computeYPixelValue(yValue, scaleFactor)
+
+        const n = Math.ceil((this.dataRange.max - this.dataRange.min) /10)
+        for (let p = Math.ceil(this.dataRange.min+1); p < Math.round(this.dataRange.max - 0.4 ); p += n) {
+            const yp = yScale(p)
+            IGVGraphics.strokeLine(ctx, 45, yp, 50, yp, font) // Offset dashes up by 2 pixel
+            IGVGraphics.fillText(ctx, prettyPrint(flipAxis ? this.dataRange.max-p: p), 44, yp+4, font)
+            
+        }
+
+    }
     popupData(clickState, features) {
 
         const featuresArray = features || clickState.viewport.cachedFeatures
