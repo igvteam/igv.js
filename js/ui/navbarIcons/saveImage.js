d70f3e781283ea122431fac3d4a3016f4c93d746
turner <douglass.turner@gmail.com>
2023-11-20 10:39:04 -0500

    discard unused code

--- js/ui/navbarIcons/saveImage.js
+++ js/ui/navbarIcons/saveImage.js
@@ -1,43 +1,11 @@
-// DEPRICATED
-const imageSaveTextSVG =
-    `<svg width="80px" height="44px" viewBox="0 0 80 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>save image text - LATEST</title>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="save-image-text---LATEST">
-            <g id="igv-save-png-group" transform="translate(42, 26)">
-                <rect id="igv-save-image-png-rect" stroke="#737373" fill="#FFFFFF" x="0.5" y="0.5" width="37" height="17" rx="6"></rect>
-                <text id="igv-save-image-png-label" font-family="Helvetica" font-size="12" font-weight="normal" letter-spacing="-0.172" fill="#737373">
-                    <tspan x="6.75604688" y="13">PNG</tspan>
-                </text>
-            </g>
-            <g id="igv-save-svg-group" transform="translate(0, 26)">
-                <rect id="igv-save-image-svg-rect" stroke="#737373" fill="#FFFFFF" x="0.5" y="0.5" width="37" height="17" rx="6"></rect>
-                <text id="igv-save-image-svg-label" font-family="Helvetica" font-size="12" font-weight="normal" letter-spacing="-0.172" fill="#737373">
-                    <tspan x="8.08710156" y="13">SVG</tspan>
-                </text>
-            </g>
-             <g id="igv-save-image-group">
-                <rect id="igv-save-image-rect" stroke="#737373" fill="#FFFFFF" x="0.5" y="0.5" width="79" height="17" rx="6"></rect>
-                <text id="igv-save-image-label" font-family="Helvetica" font-size="12" font-weight="normal" letter-spacing="-0.572" fill="#737373">
-                    <tspan id="igv-save-image-tspan" x="10.3414453" y="13">Sample Names</tspan>
-                </text>
-            </g>
-        </g>
-    </g>
-</svg>`
-// ------------
 
-// Icon Button
 const imageSaveImageSVG =
     `<svg width="625px" height="625px" viewBox="0 0 625 625" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <title>save image</title>
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="save-image">
-            <rect id="Rectangle-Copy-12" stroke="#737373" stroke-width="12" fill="#FFFFFF" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <path d="M194.1875,556 C186.9375,556 180.59375,553.280788 175.15625,547.842365 C169.71875,542.403941 167,536.059113 167,528.807882 L167,464 L194.1875,464 L194.1875,528.807882 L429.8125,528.807882 L429.8125,464 L457,464 L457,528.807882 C457,536.059113 454.28125,542.403941 448.84375,547.842365 C443.40625,553.280788 437.0625,556 429.8125,556 L194.1875,556 Z M312.5,507 L225,419.416838 L244.494819,399.903491 L298.898964,454.359343 L298.898964,286 L326.101036,286 L326.101036,454.359343 L380.505181,399.903491 L400,419.416838 L312.5,507 Z" id="download" fill="#737373" fill-rule="nonzero"></path>
-            <text id="Save" font-family="HelveticaNeue-Bold, Helvetica Neue" font-size="200" font-weight="bold" fill="#737373">
-                <tspan x="80.3" y="240">Save</tspan>
-            </text>
+            <rect id="border" stroke="#737373" stroke-width="12" fill="#FFFFFF" x="6" y="6" width="613" height="613" rx="135"></rect>
+            <path d="M120.75,559 C105.69375,559 92.8046875,553.639062 82.0828125,542.917187 C71.3609375,532.195312 66,519.30625 66,504.25 L66,340 L120.75,340 L120.75,504.25 L285,504.25 L285,559 L120.75,559 Z M340,559 L340,504.25 L504.25,504.25 L504.25,340 L559,340 L559,504.25 C559,519.30625 553.639062,532.195312 542.917187,542.917187 C532.195312,553.639062 519.30625,559 504.25,559 L340,559 Z M148,449 L230.25,339.4 L291.9375,421.6 L374.1875,312 L477,449 L148,449 Z M66,285 L66,120.75 C66,105.69375 71.3609375,92.8046875 82.0828125,82.0828125 C92.8046875,71.3609375 105.69375,66 120.75,66 L285,66 L285,120.75 L120.75,120.75 L120.75,285 L66,285 Z M504.25,285 L504.25,120.75 L340,120.75 L340,66 L504.25,66 C519.30625,66 532.195312,71.3609375 542.917187,82.0828125 C553.639062,92.8046875 559,105.69375 559,120.75 L559,285 L504.25,285 Z M408,258 C396.155556,258 386.361111,254.127778 378.616667,246.383333 C370.872222,238.638889 367,228.844444 367,217 C367,205.155556 370.872222,195.361111 378.616667,187.616667 C386.361111,179.872222 396.155556,176 408,176 C419.844444,176 429.638889,179.872222 437.383333,187.616667 C445.127778,195.361111 449,205.155556 449,217 C449,228.844444 445.127778,238.638889 437.383333,246.383333 C429.638889,254.127778 419.844444,258 408,258 Z" id="Shape" fill="#737373" fill-rule="nonzero"></path>
         </g>
     </g>
 </svg>`
@@ -47,119 +15,10 @@ const imageSaveImageHoverSVG =
     <title>save image hover</title>
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="save-image-hover">
-            <rect id="Rectangle-Copy-19" stroke="#737373" stroke-width="12" fill="#737373" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <path d="M194.1875,556 C186.9375,556 180.59375,553.280788 175.15625,547.842365 C169.71875,542.403941 167,536.059113 167,528.807882 L167,464 L194.1875,464 L194.1875,528.807882 L429.8125,528.807882 L429.8125,464 L457,464 L457,528.807882 C457,536.059113 454.28125,542.403941 448.84375,547.842365 C443.40625,553.280788 437.0625,556 429.8125,556 L194.1875,556 Z M312.5,507 L225,419.416838 L244.494819,399.903491 L298.898964,454.359343 L298.898964,286 L326.101036,286 L326.101036,454.359343 L380.505181,399.903491 L400,419.416838 L312.5,507 Z" id="download" fill="#FFFFFF" fill-rule="nonzero"></path>
-            <text id="Save" font-family="HelveticaNeue-Bold, Helvetica Neue" font-size="200" font-weight="bold" fill="#FFFFFF">
-                <tspan x="80.3" y="240">Save</tspan>
-            </text>
+            <rect id="border-copy" stroke="#737373" stroke-width="12" fill="#737373" x="6" y="6" width="613" height="613" rx="135"></rect>
+            <path d="M120.75,559 C105.69375,559 92.8046875,553.639062 82.0828125,542.917187 C71.3609375,532.195312 66,519.30625 66,504.25 L66,340 L120.75,340 L120.75,504.25 L285,504.25 L285,559 L120.75,559 Z M340,559 L340,504.25 L504.25,504.25 L504.25,340 L559,340 L559,504.25 C559,519.30625 553.639062,532.195312 542.917187,542.917187 C532.195312,553.639062 519.30625,559 504.25,559 L340,559 Z M148,449 L230.25,339.4 L291.9375,421.6 L374.1875,312 L477,449 L148,449 Z M66,285 L66,120.75 C66,105.69375 71.3609375,92.8046875 82.0828125,82.0828125 C92.8046875,71.3609375 105.69375,66 120.75,66 L285,66 L285,120.75 L120.75,120.75 L120.75,285 L66,285 Z M504.25,285 L504.25,120.75 L340,120.75 L340,66 L504.25,66 C519.30625,66 532.195312,71.3609375 542.917187,82.0828125 C553.639062,92.8046875 559,105.69375 559,120.75 L559,285 L504.25,285 Z M408,258 C396.155556,258 386.361111,254.127778 378.616667,246.383333 C370.872222,238.638889 367,228.844444 367,217 C367,205.155556 370.872222,195.361111 378.616667,187.616667 C386.361111,179.872222 396.155556,176 408,176 C419.844444,176 429.638889,179.872222 437.383333,187.616667 C445.127778,195.361111 449,205.155556 449,217 C449,228.844444 445.127778,238.638889 437.383333,246.383333 C429.638889,254.127778 419.844444,258 408,258 Z" id="Shape" fill="#FFFFFF" fill-rule="nonzero"></path>
         </g>
     </g>
 </svg>`
 
-// Child SVG for Icon - PNG
-const pngImage =
-    `<svg width="625px" height="625px" viewBox="0 0 625 625" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>PNG</title>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="PNG">
-            <rect id="Rectangle-Copy-25" stroke="#737373" stroke-width="12" fill="#FFFFFF" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <text font-family="HelveticaNeue-Bold, Helvetica Neue" font-size="200" font-weight="bold" fill="#737373">
-                <tspan x="95.3" y="388">PNG</tspan>
-            </text>
-        </g>
-    </g>
-</svg>`
-
-const pngHoverImage =
-    `<svg width="625px" height="625px" viewBox="0 0 625 625" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>PNG hover</title>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="PNG-hover">
-            <rect id="Rectangle-Copy-20" stroke="#737373" stroke-width="12" fill="#737373" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <text id="PNG" font-family="HelveticaNeue-Bold, Helvetica Neue" font-size="200" font-weight="bold" fill="#FFFFFF">
-                <tspan x="95.3" y="388">PNG</tspan>
-            </text>
-        </g>
-    </g>
-</svg>`
-
-// Child SVG for Icon Button - SVG
-const svgImage =
-    `<svg width="625px" height="625px" viewBox="0 0 625 625" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>SVG</title>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="SVG">
-            <rect id="Rectangle-Copy-22" stroke="#737373" stroke-width="12" fill="#FFFFFF" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <text font-family="HelveticaNeue-Bold, Helvetica Neue" font-size="200" font-weight="bold" fill="#737373">
-                <tspan x="108.2" y="388">SVG</tspan>
-            </text>
-        </g>
-    </g>
-</svg>`
-
-const svgHoverImage =
-    `<svg width="625px" height="625px" viewBox="0 0 625 625" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>SVG hover</title>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="SVG-hover">
-            <rect id="Rectangle-Copy-21" stroke="#737373" stroke-width="12" fill="#737373" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <text id="SVG" font-family="HelveticaNeue-Bold, Helvetica Neue" font-size="200" font-weight="bold" fill="#FFFFFF">
-                <tspan x="108.2" y="388">SVG</tspan>
-            </text>
-        </g>
-    </g>
-</svg>`
-
-// Child SVG for Text Button - PNG
-const pngText =
-    `<svg width="38px" height="18px" viewBox="0 0 38 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>png text</title>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="png-text">
-            <rect id="igv-save-image-png-rect" stroke="#737373" fill="#FFFFFF" x="0.5" y="0.5" width="37" height="17" rx="6"></rect>
-            <text id="igv-save-image-png-label" font-family="Helvetica" font-size="12" font-weight="normal" letter-spacing="-0.172" fill="#737373">
-                <tspan x="6.75604688" y="13">PNG</tspan>
-            </text>
-        </g>
-    </g>
-</svg>`
-
-const pngTextHover =
-    `<svg width="38px" height="18px" viewBox="0 0 38 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>png text hover</title>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="png-text-hover">
-            <rect id="igv-save-image-png-rect" stroke="#737373" fill="#737373" x="0.5" y="0.5" width="37" height="17" rx="6"></rect>
-            <text id="igv-save-image-png-label" font-family="Helvetica" font-size="12" font-weight="normal" letter-spacing="-0.172" fill="#FFFFFF">
-                <tspan x="6.75604688" y="13">PNG</tspan>
-            </text>
-        </g>
-    </g>
-</svg>`
-
-const svgText =
-    `<svg width="38px" height="18px" viewBox="0 0 38 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g>
-            <rect stroke="#737373" fill="#FFFFFF" x="0.5" y="0.5" width="37" height="17" rx="6"></rect>
-            <text font-family="Helvetica" font-size="12" font-weight="normal" letter-spacing="-0.172" fill="#737373">
-                <tspan x="8.08710156" y="13">SVG</tspan>
-            </text>
-        </g>
-    </g>
-</svg>`
-const svgTextHover =
-    `<svg width="38px" height="18px" viewBox="0 0 38 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g>
-            <rect stroke="#737373" fill="#737373" x="0.5" y="0.5" width="37" height="17" rx="6"></rect>
-            <text font-family="Helvetica" font-size="12" font-weight="normal" letter-spacing="-0.172" fill="#FFFFFF">
-                <tspan x="8.08710156" y="13">SVG</tspan>
-            </text>
-        </g>
-    </g>
-</svg>`
-
-export { imageSaveImageSVG, imageSaveImageHoverSVG, imageSaveTextSVG }
-export { pngImage, pngHoverImage, svgImage, svgHoverImage }
-export { pngText, pngTextHover, svgText, svgTextHover }
+export { imageSaveImageSVG, imageSaveImageHoverSVG }
