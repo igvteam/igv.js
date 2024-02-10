e766848f8ce6288e4edc15bf60f390c246bba78d
turner <douglass.turner@gmail.com>
2024-02-09 19:43:12 -0500

    Bug fix for CenterLine and CursorGuide when in whole-genome view.

--- js/ui/centerLineButton.js
+++ js/ui/centerLineButton.js
@@ -49,9 +49,9 @@ class CenterLineButton extends NavbarButton {
 
         const mouseClickHandler = () => {
 
-            if (false === browser.doShowCenterLine && GenomeUtils.isWholeGenomeView(browser.referenceFrameList[0].chr)) {
-                return
-            }
+            // if (false === browser.doShowCenterLine && GenomeUtils.isWholeGenomeView(browser.referenceFrameList[0].chr)) {
+            //     return
+            // }
 
             browser.doShowCenterLine = !browser.doShowCenterLine
             browser.setCenterLineVisibility(browser.doShowCenterLine)
