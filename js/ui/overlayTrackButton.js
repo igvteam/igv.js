450eac4bcd6188cb373055eff77ace439576c839
turner <douglass.turner@gmail.com>
2024-02-29 14:36:09 -0500

    OverlayTrackButton - discard ENABLE_OVERLAY_TRACKS

--- js/ui/overlayTrackButton.js
+++ js/ui/overlayTrackButton.js
@@ -3,24 +3,13 @@ import { overlayTrackImage, overlayTrackImageHover } from "./navbarIcons/overlay
 import { buttonLabel } from "./navbarIcons/buttonLabel.js"
 import {trackOverlayClickHandler} from "./menuUtils.js"
 
-let ENABLE_OVERLAY_TRACKS = false
-
 class OverlayTrackButton extends NavbarButton {
     constructor(browser, parent) {
 
-        super(browser, parent, 'Overlay Tracks', buttonLabel, overlayTrackImage, overlayTrackImageHover, false/*ENABLE_OVERLAY_TRACKS*/)
+        super(browser, parent, 'Overlay Tracks', buttonLabel, overlayTrackImage, overlayTrackImageHover, false)
 
-        this.button.addEventListener('mouseenter', event => {
-            // if (false === ENABLE_OVERLAY_TRACKS) {
-                this.setState(true)
-            // }
-        })
-
-        this.button.addEventListener('mouseleave', event => {
-            // if (false === ENABLE_OVERLAY_TRACKS) {
-                this.setState(false)
-            // }
-        })
+        this.button.addEventListener('mouseenter', () => this.setState(true))
+        this.button.addEventListener('mouseleave', () => this.setState(false))
 
         const mouseClickHandler = () => {
             this.setVisibility(false)
