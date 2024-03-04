ce2615050e524ccb607b37a198246d773adfd691
jrobinso <933148+jrobinso@users.noreply.github.com>
2024-03-04 15:40:55 -0800

    checkpoint

--- js/feature/mergedTrack.js
+++ js/feature/mergedTrack.js
@@ -27,6 +27,7 @@
 import TrackBase from "../trackBase.js"
 import paintAxis from "../util/paintAxis.js"
 import {FeatureUtils} from "../../node_modules/igv-utils/src/index.js"
+import {doAutoscale} from "../util/igvUtils.js"
 
 
 /**
@@ -34,14 +35,16 @@ import {FeatureUtils} from "../../node_modules/igv-utils/src/index.js"
  */
 class MergedTrack extends TrackBase {
 
+    static defaults = {
+        _alpha: 0.5
+    }
+
     constructor(config, browser) {
         super(config, browser)
         this.type = "merged"
-        this.featureType = 'numeric'
+        this.featureType = "numeric"
         this.paintAxis = paintAxis
         this.graphType = config.graphType
-
-        this._alpha = this.config.alpha || 0.5
     }
 
     init(config) {
@@ -59,7 +62,6 @@ class MergedTrack extends TrackBase {
             tconf.isMergedTrack = true
             const t = await this.browser.createTrack(tconf)
             if (t) {
-                t.autoscale = false     // Scaling done from merged track
                 this.tracks.push(t)
             } else {
                 console.warn("Could not create track " + tconf)
@@ -72,16 +74,31 @@ class MergedTrack extends TrackBase {
 
         this.flipAxis = this.config.flipAxis ? this.config.flipAxis : false
         this.logScale = this.config.logScale ? this.config.logScale : false
-        this.autoscale = this.config.autoscale || this.config.max === undefined
-        if (!this.autoscale) {
+
+        // this.autoscale = this.config.autoscale || this.config.max === undefined
+        // if (!this.autoscale) {
+        //     this.dataRange = {
+        //         min: this.config.min || 0,
+        //         max: this.config.max
+        //     }
+        // }
+        // for (let t of this.tracks) {
+        //     t.autoscale = false
+        //     t.dataRange = this.dataRange
+        // }
+
+        // Explicit merged settings -- these will override any individual track settings
+        if (this.config.autoscale) {
+            this.autoscale = this.config.autoscale
+            this.autoscaleGroup = this.config.autoscale     // Autoscale all tracks as a group
+        } else if (this.config.max !== undefined) {
             this.dataRange = {
                 min: this.config.min || 0,
                 max: this.config.max
             }
-        }
-        for (let t of this.tracks) {
-            t.autoscale = false
-            t.dataRange = this.dataRange
+        } else {
+            this.autoscale = true;
+            this.autoscaleGroup = false
         }
 
         this.height = this.config.height || 50
@@ -89,6 +106,10 @@ class MergedTrack extends TrackBase {
         this.resolutionAware = this.tracks.some(t => t.resolutionAware)
 
         return Promise.all(p)
+    }
+
+    doAutoscale(allFeatures) {
+        console.log(allFeatures)
     }
 
     set alpha(alpha) {
@@ -118,7 +139,7 @@ class MergedTrack extends TrackBase {
         if (this.flipAxis !== undefined) {
             items.push({
                 label: "Flip y-axis",
-                click: function flipYAxisHandler(){
+                click: function flipYAxisHandler() {
                     this.flipAxis = !this.flipAxis
                     this.trackView.repaintViews()
                 }
@@ -135,14 +156,13 @@ class MergedTrack extends TrackBase {
      * In addition it contains track names in the same order.
      */
     async getFeatures(chr, bpStart, bpEnd, bpPerPixel) {
-        
+
         const promises = this.tracks.map((t) => t.getFeatures(chr, bpStart, bpEnd, bpPerPixel))
         const featureArrays = await Promise.all(promises)
-        
-        if (featureArrays.every((arr) => arr.length === 0)){
+
+        if (featureArrays.every((arr) => arr.length === 0)) {
             return new MergedFeatureCollection([], [])
-        }
-        else {
+        } else {
             const trackNames = this.tracks.map((t) => t.name)
             return new MergedFeatureCollection(featureArrays, trackNames)
         }
@@ -155,10 +175,14 @@ class MergedTrack extends TrackBase {
         for (let i = 0, len = this.tracks.length; i < len; i++) {
             const trackOptions = Object.assign({}, options)
             trackOptions.features = mergedFeatures.featureArrays[i]
-
             trackOptions.alpha = this.alpha
 
-            this.tracks[i].dataRange = this.dataRange
+            if (this.dataRange) {
+                // Single data scale for all tracks
+                this.tracks[i].dataRange = this.dataRange
+            }
+
+
             this.tracks[i].flipAxis = this.flipAxis
             this.tracks[i].logScale = this.logScale
             if (this.graphType) {
@@ -178,17 +202,16 @@ class MergedTrack extends TrackBase {
             // Flag used to check if there is at least one track with data
             let noData = true
             for (let i = 0; i < clickedFeaturesArray.length; i++) {
-                
-                
+
+
                 if (i > 0) popupData.push('<hr/>')
                 popupData.push(`<div style=background-color:rgb(245,245,245);border-bottom-style:dashed;border-bottom-width:1px;padding-bottom:5px;padding-top:10px;font-weight:bold;font-size:larger >${clickedFeaturesArray[i].trackName}</div>`)
                 if (clickedFeaturesArray[i].features.length > 0) {
                     noData = false
-                    
+
                     const trackPopupData = this.tracks[i].popupData(clickState, clickedFeaturesArray[i].features)
                     popupData.push(...trackPopupData)
-                }
-                else {
+                } else {
                     // Notify user if there is no data or all values are 0 for a specific track
                     popupData.push("Missing or 0 value(s)")
                 }
@@ -198,8 +221,7 @@ class MergedTrack extends TrackBase {
             // If at least one does, we want to display the popup.
             if (noData === true) {
                 return []
-            }
-            else {
+            } else {
                 return popupData
             }
         }
@@ -218,12 +240,12 @@ class MergedTrack extends TrackBase {
 
         const genomicLocation = clickState.genomicLocation
         const clickedFeatures = []
-        
+
         // When zoomed out we need some tolerance around genomicLocation
         const tolerance = (clickState.referenceFrame.bpPerPixel > 0.2) ? 3 * clickState.referenceFrame.bpPerPixel : 0.2
         const ss = genomicLocation - tolerance
         const ee = genomicLocation + tolerance
-        for (let i = 0; i < mergedFeaturesCollection.featureArrays.length; i++){
+        for (let i = 0; i < mergedFeaturesCollection.featureArrays.length; i++) {
             const tmp = (FeatureUtils.findOverlapping(mergedFeaturesCollection.featureArrays[i], ss, ee))
             clickedFeatures.push({
                 trackName: mergedFeaturesCollection.trackNames[i],
@@ -248,29 +270,25 @@ class MergedFeatureCollection {
         this.trackNames = trackNames
     }
 
-    getMax(start, end) {
-        let max = -Number.MAX_VALUE
+    getMaxA(start, end) {
+        let max = -Number.MAX_VALUE;
+
         for (let a of this.featureArrays) {
             for (let f of a) {
-                if (typeof f.value !== 'undefined' && !Number.isNaN(f.value)) {
-                    if (f.end < start) {
-                        continue
-                    }
-                    if (f.start > end) {
-                        break
-                    }
-                    max = Math.max(max, f.value)
+                if (typeof f.value === 'undefined' || Number.isNaN(f.value)) {
+                    continue;
                 }
+                if (f.end < start) {
+                    continue;
+                }
+                if (f.start > end) {
+                    break;
+                }
+                max = Math.max(max, f.value);
             }
         }
-        // We can assume if max has not changed from -Number.MAX_VALUE that there
-        // are no values we can use to determine max value and we set it to 100.
-        if (max === -Number.MAX_VALUE) {
-            return 100
-        }
-        else {
-            return max
-        }
+
+        return max !== -Number.MAX_VALUE ? max : 100;
     }
 
     // Covers cases in which a track has negative values.
