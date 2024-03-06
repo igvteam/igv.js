a1ea2a6873cdd54aa4d6256df80f2d356a3b3dde
turner <douglass.turner@gmail.com>
2024-03-06 14:17:54 -0500

    misc

--- js/ui/navbarIcons/overlayTrack.js
+++ js/ui/navbarIcons/overlayTrack.js
@@ -5,9 +5,9 @@ const overlayTrackImage =
         <g id="Overlay-Tracks" stroke="#737373">
             <rect id="backdrop" stroke-width="12" fill="#FFFFFF" x="6" y="6" width="613" height="613" rx="135"></rect>
             <g id="Group-3" transform="translate(3, 127)" stroke-width="9">
-                <rect id="c" fill="#A1A1A1" x="230.5" y="206.5" width="387" height="154"></rect>
+                <rect id="c" fill="#ECECEC" x="230.5" y="206.5" width="387" height="154"></rect>
                 <rect id="b" fill="#C9C9C9" x="73.5" y="95.5" width="489" height="154"></rect>
-                <rect id="a" fill="#ECECEC" x="4.5" y="4.5" width="347" height="143"></rect>
+                <rect id="a" fill="#A1A1A1" x="4.5" y="4.5" width="347" height="143"></rect>
             </g>
             <rect id="over-border" stroke-width="12" x="6" y="6" width="613" height="613" rx="135"></rect>
         </g>
@@ -20,10 +20,10 @@ const overlayTrackImageHover =
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="Overlay-Tracks-Hover">
             <rect id="backdrop-copy" stroke="#737373" stroke-width="12" fill="#737373" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <g id="Group-3-Copy" transform="translate(3, 127)" stroke="#FFFFFF" stroke-width="9">
-                <rect id="c" fill="#A1A1A1" x="230.5" y="206.5" width="387" height="154"></rect>
+            <g id="Group-3" transform="translate(3, 127)" stroke="#FFFFFF" stroke-width="9">
+                <rect id="c" fill="#ECECEC" x="230.5" y="206.5" width="387" height="154"></rect>
                 <rect id="b" fill="#C9C9C9" x="73.5" y="95.5" width="489" height="154"></rect>
-                <rect id="a" fill="#ECECEC" x="4.5" y="4.5" width="347" height="143"></rect>
+                <rect id="a" fill="#A1A1A1" x="4.5" y="4.5" width="347" height="143"></rect>
             </g>
             <rect id="over-border-copy" stroke="#737373" stroke-width="12" x="6" y="6" width="613" height="613" rx="135"></rect>
         </g>
