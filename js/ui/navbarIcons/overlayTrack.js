37bd2ee394d76603f41e3e0d9d7b79f779b74425
turner <douglass.turner@gmail.com>
2024-03-06 16:45:15 -0500

    more edits

--- js/ui/navbarIcons/overlayTrack.js
+++ js/ui/navbarIcons/overlayTrack.js
@@ -21,9 +21,9 @@ const overlayTrackImageHover =
         <g id="Overlay-Tracks-Hover">
             <rect id="backdrop-copy" stroke="#737373" stroke-width="12" fill="#737373" x="6" y="6" width="613" height="613" rx="135"></rect>
             <g id="Group-3" transform="translate(3, 127)" stroke="#FFFFFF" stroke-width="9">
-                <rect id="c" fill="#ECECEC" x="230.5" y="206.5" width="387" height="154"></rect>
-                <rect id="b" fill="#C9C9C9" x="73.5" y="95.5" width="489" height="154"></rect>
                 <rect id="a" fill="#A1A1A1" x="4.5" y="4.5" width="347" height="143"></rect>
+                <rect id="b" fill="#C9C9C9" x="73.5" y="95.5" width="489" height="154"></rect>
+                <rect id="c" fill="#ECECEC" x="230.5" y="206.5" width="387" height="154"></rect>
             </g>
             <rect id="over-border-copy" stroke="#737373" stroke-width="12" x="6" y="6" width="613" height="613" rx="135"></rect>
         </g>
