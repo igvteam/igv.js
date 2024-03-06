d1aaa68b9d7254b6f5015de06f6662f0eeb60a8a
turner <douglass.turner@gmail.com>
2024-03-06 10:00:26 -0500

    Overlay track icon. Design iteration.

--- js/ui/navbarIcons/overlayTrack.js
+++ js/ui/navbarIcons/overlayTrack.js
@@ -2,11 +2,13 @@ const overlayTrackImage =
     `<svg width="625px" height="625px" viewBox="0 0 625 625" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <title>Overlay Tracks</title>
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="Overlay-Tracks">
-            <rect id="Rectangle-Copy-32" stroke="#737373" stroke-width="12" fill="#FFFFFF" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <g id="Group-3" transform="translate(77, 214)">
-                <rect id="Rectangle" fill-opacity="0.501584353" fill="#0091FF" x="0" y="0" width="402" height="123"></rect>
-                <rect id="Rectangle-Copy-34" fill-opacity="0.503223339" fill="#6DD400" x="70" y="74" width="402" height="123"></rect>
+        <g id="Overlay-Tracks" stroke="#737373">
+            <rect id="backdrop" stroke-width="12" fill="#FFFFFF" x="6" y="6" width="613" height="613" rx="135"></rect>
+            <g id="Group-3">
+                <rect id="c" stroke-width="9" fill="#CFCFCF" x="233.5" y="333.5" width="387" height="154"></rect>
+                <rect id="b" stroke-width="9" fill="#A3A3A3" x="76.5" y="222.5" width="489" height="154"></rect>
+                <rect id="a" stroke-width="9" fill="#DFDFDF" x="7.5" y="131.5" width="347" height="143"></rect>
+                <rect id="over-border" stroke-width="12" x="6" y="6" width="613" height="613" rx="135"></rect>
             </g>
         </g>
     </g>
@@ -17,14 +19,12 @@ const overlayTrackImageHover =
     <title>Overlay Tracks Hover</title>
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="Overlay-Tracks-Hover">
-            <rect id="Rectangle-Copy-33" stroke="#737373" stroke-width="12" fill="#737373" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <g id="Group-3-Copy-2" transform="translate(78, 213)" fill="#FFFFFF">
-                <rect id="Rectangle" x="0" y="0" width="402" height="123"></rect>
-                <rect id="Rectangle-Copy-34" x="70" y="74" width="402" height="123"></rect>
-            </g>
-            <g id="Group-3-Copy" transform="translate(78, 213)">
-                <rect id="Rectangle" fill-opacity="0.501584353" fill="#0091FF" x="0" y="0" width="402" height="123"></rect>
-                <rect id="Rectangle-Copy-34" fill-opacity="0.503223339" fill="#6DD400" x="70" y="74" width="402" height="123"></rect>
+            <rect id="backdrop-copy" stroke="#737373" stroke-width="12" fill="#737373" x="6" y="6" width="613" height="613" rx="135"></rect>
+            <g id="Group-3">
+                <rect id="c" stroke="#FFFFFF" stroke-width="9" fill="#CFCFCF" x="233.5" y="333.5" width="387" height="154"></rect>
+                <rect id="b" stroke="#FFFFFF" stroke-width="9" fill="#A3A3A3" x="76.5" y="222.5" width="489" height="154"></rect>
+                <rect id="a" stroke="#FFFFFF" stroke-width="9" fill="#DFDFDF" x="7.5" y="131.5" width="347" height="143"></rect>
+                <rect id="over-border-copy" stroke="#737373" stroke-width="12" x="6" y="6" width="613" height="613" rx="135"></rect>
             </g>
         </g>
     </g>
