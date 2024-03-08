8598f1b096d41988977ffc477054ff90196f2eb2
turner <douglass.turner@gmail.com>
2024-03-08 13:13:49 -0500

    Overlay button icon edits

--- js/ui/navbarIcons/overlayTrack.js
+++ js/ui/navbarIcons/overlayTrack.js
@@ -2,14 +2,16 @@ const overlayTrackImage =
     `<svg width="625px" height="625px" viewBox="0 0 625 625" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <title>Overlay Tracks</title>
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="Overlay-Tracks" stroke="#737373">
-            <rect id="backdrop" stroke-width="12" fill="#FFFFFF" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <g id="Group-3" transform="translate(3, 127)" stroke-width="9">
-                <rect id="a" fill="#A1A1A1" x="4.5" y="4.5" width="347" height="143"></rect>
-                <rect id="b" fill="#C9C9C9" x="73.5" y="95.5" width="489" height="154"></rect>
-                <rect id="c" fill="#ECECEC" x="230.5" y="206.5" width="387" height="154"></rect>
+        <g id="Overlay-Tracks">
+            <rect id="backdrop" stroke="#737373" stroke-width="12" fill="#FFFFFF" x="6" y="6" width="613" height="613" rx="135"></rect>
+            <g id="layer-group" transform="translate(3, 127)">
+                <rect id="a" stroke="#737373" stroke-width="16" fill="#A1A1A1" x="8" y="8" width="340" height="147"></rect>
+                <rect id="a---hold-out" fill="#A1A1A1" x="9" y="15" width="332" height="133"></rect>
+                <rect id="b" stroke="#737373" stroke-width="16" fill="#C9C9C9" x="77" y="99" width="482" height="147"></rect>
+                <rect id="c" stroke="#737373" stroke-width="16" fill="#ECECEC" x="234" y="210" width="380" height="147"></rect>
+                <rect id="c---hold-out" fill="#ECECEC" x="241" y="217" width="381" height="133"></rect>
             </g>
-            <rect id="over-border" stroke-width="12" x="6" y="6" width="613" height="613" rx="135"></rect>
+            <rect id="over-border" stroke="#737373" stroke-width="12" x="6" y="6" width="613" height="613" rx="135"></rect>
         </g>
     </g>
 </svg>`
@@ -20,10 +22,12 @@ const overlayTrackImageHover =
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="Overlay-Tracks-Hover">
             <rect id="backdrop-copy" stroke="#737373" stroke-width="12" fill="#737373" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <g id="Group-3" transform="translate(3, 127)" stroke="#FFFFFF" stroke-width="9">
-                <rect id="a" fill="#A1A1A1" x="4.5" y="4.5" width="347" height="143"></rect>
-                <rect id="b" fill="#C9C9C9" x="73.5" y="95.5" width="489" height="154"></rect>
-                <rect id="c" fill="#ECECEC" x="230.5" y="206.5" width="387" height="154"></rect>
+            <g id="layer-group-copy" transform="translate(3, 127)">
+                <rect id="a" stroke="#FFFFFF" stroke-width="16" fill="#A1A1A1" x="8" y="8" width="340" height="147"></rect>
+                <rect id="a---hold-out" fill="#A1A1A1" x="9" y="15" width="332" height="133"></rect>
+                <rect id="b" stroke="#FFFFFF" stroke-width="16" fill="#C9C9C9" x="77" y="99" width="482" height="147"></rect>
+                <rect id="c" stroke="#FFFFFF" stroke-width="16" fill="#ECECEC" x="234" y="210" width="380" height="147"></rect>
+                <rect id="c---hold-out" fill="#ECECEC" x="241" y="217" width="381" height="133"></rect>
             </g>
             <rect id="over-border-copy" stroke="#737373" stroke-width="12" x="6" y="6" width="613" height="613" rx="135"></rect>
         </g>
