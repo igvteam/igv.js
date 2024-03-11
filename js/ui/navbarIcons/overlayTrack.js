74953ce9c4d157488d0bec9e68b303325cb818d9
turner <douglass.turner@gmail.com>
2024-03-11 14:40:37 -0400

    icon edits

--- js/ui/navbarIcons/overlayTrack.js
+++ js/ui/navbarIcons/overlayTrack.js
@@ -5,11 +5,11 @@ const overlayTrackImage =
         <g id="Overlay-Tracks">
             <rect id="backdrop" stroke="#737373" stroke-width="12" fill="#FFFFFF" x="6" y="6" width="613" height="613" rx="135"></rect>
             <g id="layer-group" transform="translate(3, 127)">
-                <rect id="a" stroke="#737373" stroke-width="16" fill="#A1A1A1" x="8" y="8" width="340" height="147"></rect>
-                <rect id="a---hold-out" fill="#A1A1A1" x="9" y="15" width="332" height="133"></rect>
-                <rect id="b" stroke="#737373" stroke-width="16" fill="#C9C9C9" x="77" y="99" width="482" height="147"></rect>
-                <rect id="c" stroke="#737373" stroke-width="16" fill="#ECECEC" x="234" y="210" width="380" height="147"></rect>
-                <rect id="c---hold-out" fill="#ECECEC" x="241" y="217" width="381" height="133"></rect>
+                <rect id="a" stroke="#737373" stroke-width="24" fill="#A1A1A1" x="12" y="12" width="332" height="139"></rect>
+                <rect id="a---hold-out" fill="#A1A1A1" x="9" y="25" width="324" height="115"></rect>
+                <rect id="b" stroke="#737373" stroke-width="24" fill="#C9C9C9" x="81" y="103" width="474" height="139"></rect>
+                <rect id="c" stroke="#737373" stroke-width="24" fill="#ECECEC" x="238" y="214" width="372" height="139"></rect>
+                <rect id="c---hold-out" fill="#ECECEC" x="250" y="226" width="372" height="115"></rect>
             </g>
             <rect id="over-border" stroke="#737373" stroke-width="12" x="6" y="6" width="613" height="613" rx="135"></rect>
         </g>
@@ -22,12 +22,12 @@ const overlayTrackImageHover =
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="Overlay-Tracks-Hover">
             <rect id="backdrop-copy" stroke="#737373" stroke-width="12" fill="#737373" x="6" y="6" width="613" height="613" rx="135"></rect>
-            <g id="layer-group-copy" transform="translate(3, 127)">
-                <rect id="a" stroke="#FFFFFF" stroke-width="16" fill="#A1A1A1" x="8" y="8" width="340" height="147"></rect>
-                <rect id="a---hold-out" fill="#A1A1A1" x="9" y="15" width="332" height="133"></rect>
-                <rect id="b" stroke="#FFFFFF" stroke-width="16" fill="#C9C9C9" x="77" y="99" width="482" height="147"></rect>
-                <rect id="c" stroke="#FFFFFF" stroke-width="16" fill="#ECECEC" x="234" y="210" width="380" height="147"></rect>
-                <rect id="c---hold-out" fill="#ECECEC" x="241" y="217" width="381" height="133"></rect>
+            <g id="layer-group" transform="translate(3, 127)">
+                <rect id="a" stroke="#FFFFFF" stroke-width="24" fill="#A1A1A1" x="12" y="12" width="332" height="139"></rect>
+                <rect id="a---hold-out" fill="#A1A1A1" x="9" y="25" width="324" height="115"></rect>
+                <rect id="b" stroke="#FFFFFF" stroke-width="24" fill="#C9C9C9" x="81" y="103" width="474" height="139"></rect>
+                <rect id="c" stroke="#FFFFFF" stroke-width="24" fill="#ECECEC" x="238" y="214" width="372" height="139"></rect>
+                <rect id="c---hold-out" fill="#ECECEC" x="250" y="226" width="372" height="115"></rect>
             </g>
             <rect id="over-border-copy" stroke="#737373" stroke-width="12" x="6" y="6" width="613" height="613" rx="135"></rect>
         </g>
