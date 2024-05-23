a88e39327c902464bb588707193733b364db3f28
jrobinso <933148+jrobinso@users.noreply.github.com>
2024-05-23 14:46:30 -0700

    remove "sort by sample names", not sure why this was added or what it is useful for
    Former-commit-id: ac907f6084c85eb3b5c5e7e6ce9830626dc22577

--- js/variant/variantTrack.js
+++ js/variant/variantTrack.js
@@ -34,7 +34,6 @@ import {attributeNames, emptySpaceReplacement, sampleDictionary} from "../sample
 import {makeVCFChords, sendChords} from "../jbrowse/circularViewUtils.js"
 import {FileUtils, StringUtils, IGVColor} from "../../node_modules/igv-utils/src/index.js"
 import CNVPytorTrack from "../cnvpytor/cnvpytorTrack.js"
-import {sortBySampleName} from "../sample/sampleUtils.js"
 
 const isString = StringUtils.isString
 
@@ -579,8 +578,6 @@ class VariantTrack extends TrackBase {
             }
         }
 
-        menuItems.push(sortBySampleName())
-        menuItems.push('<hr/>')
 
         if (sampleDictionary) {
 
