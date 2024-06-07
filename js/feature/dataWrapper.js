05400b48810e711292c844920cfd359f1b736382
jrobinso <933148+jrobinso@users.noreply.github.com>
2024-06-07 09:19:38 -0700

    Support UTF-8 strings from generic dataWrapper
    Former-commit-id: dcabc28743d72e2f545004d057278cdd6c8b5271

--- js/feature/dataWrapper.js
+++ js/feature/dataWrapper.js
@@ -65,29 +65,76 @@ class StringDataWrapper {
 
 class ByteArrayDataWrapper {
 
+    /**
+     *
+     * @param {Uint8Array} array
+     */
     constructor(array) {
         this.data = array
         this.length = this.data.length
         this.ptr = 0
     }
 
+    /**
+     * Decode the next line as a UTF-8 string.  From: https://gist.github.com/Yaffle/5458286
+     * @returns {undefined|string}
+     */
     nextLine() {
-
-        var c, result
-        result = ""
-
-        if (this.ptr >= this.length) return undefined
-
-        for (var i = this.ptr; i < this.length; i++) {
-            c = String.fromCharCode(this.data[i])
-            if (c === '\r') continue
-            if (c === '\n') break
-            result = result + c
+        if(this.ptr >= this.data.length) {
+            return undefined
         }
+        let i = this.ptr
+        const octets = this.data
+        let string = ""
+        let maybeCR = false
+        while (i < octets.length) {
+            var octet = octets[i]
+            var bytesNeeded = 0
+            var codePoint = 0
+            if (octet <= 0x7F) {
+                bytesNeeded = 0
+                codePoint = octet & 0xFF
+            } else if (octet <= 0xDF) {
+                bytesNeeded = 1
+                codePoint = octet & 0x1F
+            } else if (octet <= 0xEF) {
+                bytesNeeded = 2
+                codePoint = octet & 0x0F
+            } else if (octet <= 0xF4) {
+                bytesNeeded = 3
+                codePoint = octet & 0x07
+            }
+            if (octets.length - i - bytesNeeded > 0) {
+                var k = 0
+                while (k < bytesNeeded) {
+                    octet = octets[i + k + 1]
+                    codePoint = (codePoint << 6) | (octet & 0x3F)
+                    k += 1
+                }
+            } else {
+                codePoint = 0xFFFD
+                bytesNeeded = octets.length - i
+            }
+            i += bytesNeeded + 1
 
-        this.ptr = i + 1
-        return result
+            const c = String.fromCodePoint(codePoint)
+            if(c === '\r') {
+                maybeCR = true
+            } else if( c === '\n') {
+                break
+            } else {
+                if(maybeCR) {
+                    // Add cr's unless immediately followed by a line feed.
+                    string += '\r'
+                    maybeCR = false
+                }
+                string += c
+            }
+        }
+        this.ptr = i
+        return string
     }
+
 
 }
 
