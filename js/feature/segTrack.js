2bc7c78796eb8eee4ac186afdee95f0665af123d
jrobinso <933148+jrobinso@users.noreply.github.com>
2024-08-04 14:44:47 -0700

    * give svg & png images default file names * bug with segTack right click
    Former-commit-id: e199a169d3178b8cf5a0c267f5f00ef85f6b2cd4

--- js/feature/segTrack.js
+++ js/feature/segTrack.js
@@ -520,7 +520,7 @@ class SegTrack extends TrackBase {
         }
 
         // We can't know genomic location intended with precision, define a buffer 5 "pixels" wide in genomic coordinates
-        const bpWidth = referenceFrame.toBP(2.5)
+        const bpWidth = clickState.referenceFrame.toBP(2.5)
 
         return ["DESC", "ASC"].map(direction => {
             const dirLabel = direction === "DESC" ? "descending" : "ascending"
@@ -533,7 +533,7 @@ class SegTrack extends TrackBase {
                     const sort = {
                         option: "VALUE",   // Either VALUE or ATTRIBUTE
                         direction,
-                        chr: clickState.viewport.referenceFrame.chr,
+                        chr: clickState.referenceFrame.chr,
                         start: Math.floor(genomicLocation - bpWidth),
                         end: Math.floor(genomicLocation + bpWidth)
                     }
