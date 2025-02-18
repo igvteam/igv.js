bf7aeb38cc89bce5753ea179181205b9842c3070
Jim Robinson <933148+jrobinso@users.noreply.github.com>
2025-02-18 14:32:07 -0800

    Cram update (#1938)
    * Bump cram-js to 4.0.10.   Add test data.  Fixes #1934

--- js/trackBase.js
+++ js/trackBase.js
@@ -139,11 +139,8 @@ class TrackBase {
 
     async postInit(){
 
-        console.log(`TrackBase - track(${ this.type }) - postInit()`)
-
         this._initialColor = this.color || this.constructor.defaultColor
         this._initialAltColor = this.altColor || this.constructor.defaultColor
-
         return this
     }
 
