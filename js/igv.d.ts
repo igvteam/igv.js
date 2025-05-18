2aedf2fb8dde46a193f9a96a494c938950ff9cfc
eternal-flame-AD <yume@yumechi.jp>
2025-05-17 21:35:46 -0500

    remove custom URL inference case and thenable
    Signed-off-by: eternal-flame-AD <yume@yumechi.jp>

--- js/igv.d.ts
+++ js/igv.d.ts
@@ -42,22 +42,12 @@ export type TypeFormatPair<T extends TrackType> = {
     format: TrackFormatOf<T>;
     // in this case URL can be anything
     url: Deferrable<string>;
-} | {
-    type?: T;
-    format?: TrackFormatOf<T>;
-    // if we can infer the URL, we do not need type or format
-    url: TrackFormatOf<T> extends string ? Deferrable<URLInference.URLWithExtension<TrackFormatOf<T>>> : never;
 } | ({
     // an explicit type and manual reader is allowed, however format and url will not make sense in this context
     type: T;
     format?: never;
     url?: never;
-} & (CustomReaderOf<T> | ManualFeatureOf<T>)) | {
-    type: T;
-    format?: TrackFormatOf<T>;
-    // if there is only one possible format, we permit format to be omitted
-    url: TrackFormatOf<T> extends string ? (string extends TrackFormatOf<T> ? never : Deferrable<URLInference.URLWithExtension<TrackFormatOf<T>>>) : never;
-};
+} & (CustomReaderOf<T> | ManualFeatureOf<T>));
 
 // converts a track type into the file formats that it can accept
 export type TrackFormatOf<T extends TrackType> =
@@ -147,25 +137,6 @@ export type StaticFeatureConfig<T extends Record<string, any>> = {
     mappings: Record<keyof T, string>;
 }
 
-// ref: https://github.com/igvteam/igv-utils/blob/master/src/fileUtils.js
-export namespace URLInference {
-    type StripLast<S extends string, SEP extends string> = S extends `${infer _}${SEP}${infer P}` ? StripLast<P, SEP> : S;
-    type StripQuery<S extends string> = S extends `${infer P}?${infer _}` ? P : S;
-    type StripSuffix<S extends string, T extends string> = S extends `${infer P}.${T}` ? P : S;
-
-    export type FilenameOfURL<U extends string> = StripLast<StripQuery<U>, '/'>
-
-    type AuxExtensions = ".gz" | ".tab" | ".txt" | ".bgz";
-
-    // infer the extension of a URL, with auxillary extensions stripped
-    export type InferExtension<F extends string> = Lowercase<F> extends `${infer B}${AuxExtensions}` ? StripLast<B, '.'> : StripLast<Lowercase<F>, '.'>
-
-    type Query = `?${string}`;
-
-    // matches a URL with an extension
-    export type URLWithExtension<E extends string> =
-        `${string}.${E}${AuxExtensions | ''}${Query | ''}`;
-}
 
 export namespace Tracks {
     export class Track {
@@ -292,7 +263,7 @@ export namespace Tracks {
         supportsWholeGenome?: boolean;
         showGenotypes?: boolean;
         strokecolor?: (variant: VCFItem) => string | void;
-        context_hook?: (variant: VCFItem, ctx: Draw.CanvasContext, x: number, y: number, w: number, h: number) => void;
+        context_hook?: unknown;
     };
 
     export type CnvPyTorFormat = "pytor" | "vcf";
@@ -687,79 +658,6 @@ export type IGV = {
     */
     readonly visibilityChange: () => Promise<void>;
     readonly version: () => string;
-}
-
-declare namespace Draw {
-    // ref: canvas2svg.js
-    export class CanvasContext {
-        public readonly canvas: this;
-        public readonly isSVG: boolean;
-
-        public strokeStyle: string;
-        public lineWidth: number;
-
-        private constructor(config: {
-            ctx?: any;
-            width?: number;
-            height?: number;
-            enableMirroring?: boolean;
-            viewBox?: {
-                x: number;
-                y: number;
-                width: number;
-                height: number;
-            };
-            multiLocusGap?: any;
-            backdropColor?: string;
-            document?: Document;
-        });
-        setWidth(width: number): void;
-        setHeight(height: number): void;
-        getSerializedSvg(fixNamedEntitles?: boolean): string;
-        getSvg(): SVGSVGElement;
-        saveWithTranslationAndClipRect(id: string, tx: number, ty: number, width: number, height: number, clipYOffset: number): void;
-        save(): void;
-        restore(): void;
-        addTrackGroupWithTranslationAndClipRect(
-            id: string,
-            tx: number,
-            ty: number,
-            width: number,
-            height: number,
-            clipYOffset: number,
-        )
-        scale(x: number, y: number): void;
-        rotate(angle: number): void;
-        translate(x: number, y: number): void;
-        transform(a: number, b: number, c: number, d: number, e: number, f: number): void;
-        beginPath(): void;
-        moveTo(x: number, y: number): void;
-        closePath(): void;
-        lineTo(x: number, y: number): void;
-        bezierCurveTo(cp1x: number, cp1y: number, cp2x: number, cp2y: number, x: number, y: number): void;
-        quadraticCurveTo(cp1x: number, cp1y: number, x: number, y: number): void;
-        arcTo(x1: number, y1: number, x2: number, y2: number, radius: number): void;
-        stroke(): void;
-        fill(): void;
-        rect(x: number, y: number, w: number, h: number): void;
-        fillRect(x: number, y: number, w: number, h: number): void;
-        strokeRect(x: number, y: number, w: number, h: number): void;
-        strokeEllipse(cx: number, cy: number, rx: number, ry: number,
-            rotation: number, startAngle: number, endAngle: number,
-            isCCW: boolean): void;
-        fillEllipse(cx: number, cy: number, rx: number, ry: number,
-            rotation: number, startAngle: number, endAngle: number,
-            isCCW: boolean): void;
-        clearRect(x: number, y: number, w: number, h: number): void;
-        createLinearGradient(x0: number, y0: number, x1: number, y1: number): CanvasGradient;
-        createRadialGradient(x0: number, y0: number, r0: number, x1: number, y1: number, r1: number): CanvasGradient;
-        strokeText(text: string, x: number, y: number): void;
-        measureText(text: string): TextMetrics;
-        arc(x: number, y: number, radius: number, startAngle: number, endAngle: number, anticlockwise?: boolean): void;
-        clip(): void;
-        drawImage();
-        createPattern(image: CanvasImageSource, repetition?: string): CanvasPattern;
-    }
 }
 
 declare const igv: IGV;
