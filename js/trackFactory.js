8ff5436b43a8e4aee95c103839ddf84dac3ae8ed
Jim Robinson <933148+jrobinso@users.noreply.github.com>
2025-05-18 20:29:18 -0700

    Support unknown 'type' values on bed trackline -- defauot to "annotat… (#1981)
    * Support unknown 'type' values on bed trackline -- defauot to "annotation".  See https://github.com/igvteam/igv-webapp/issues/332
    
    
    Former-commit-id: a60c144cfecfc7f186dc91d82c437da97c283732

--- js/trackFactory.js
+++ js/trackFactory.js
@@ -45,6 +45,9 @@ const trackFunctions =
         ['image', (config, browser) => new ImageTrack(config, browser)]
     ])
 
+function knownTrackTypes () {
+    return new Set(trackFunctions.keys())
+}
 
 /**
  * Return a track of the given type, passing configuration and a point to the IGV "Browser" object to its constructor function*
@@ -101,5 +104,6 @@ export {
     getTrack,
     trackFunctions,
     registerTrackClass,
-    registerTrackCreatorFunction
+    registerTrackCreatorFunction,
+    knownTrackTypes
 }
