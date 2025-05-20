b05c69711b853358a953b2347064a99fa852eb12
turner <douglass.turner@gmail.com>
2025-05-20 17:35:47 -0400

    refactor

--- js/roi/ROIMenu.js
+++ js/roi/ROIMenu.js
@@ -41,69 +41,94 @@ class ROIMenu {
         if (items.length > 0) items.push(`<hr/>`)
 
         if (roiSet.isUserDefined) {
-            items.push(
-                {
-                    label: 'Set description ...',
-                    click: () => {
-                        const callback = () => {
-                            const value = this.browser.inputDialog.value || ''
-                            feature.name = value.trim()
-                            this.browser.roiManager.repaintTable()
-                        }
-                        const config =
-                            {
-                                label: 'Description',
-                                value: (feature.name || ''),
-                                callback
-                            }
-
-                        this.browser.inputDialog.present(config, event)
-
-                    }
-                }
-            )
+            this.#addDescriptionMenuItem(items, feature, event)
         }
 
         // sequence
 
         // copy
         if (isSecureContext() && feature.end - feature.start < maxSequenceSize) {
-            items.push({
-                label: 'Copy reference sequence',
-                click: async () => {
-                    this.container.style.display = 'none'
-                    let sequence = await this.browser.genome.getSequence(feature.chr, feature.start, feature.end)
-                    if (!sequence) {
-                        sequence = "Unknown sequence"
-                    }
-                    try {
-                        await navigator.clipboard.writeText(sequence)
-                    } catch (e) {
-                        console.error(e)
-                        this.browser.alert.present(`error copying sequence to clipboard ${e}`)
-                    }
-                }
-            })
+            this.#addCopySequenceMenuItem(items, feature)
         }
 
         if (feature.end - feature.start <= maxBlatSize) {
-            // blat
-            items.push({
-                label: 'BLAT reference sequence',
-                click: async () => {
-                    this.container.style.display = 'none'
-                    const {chr, start, end} = feature
-                    let sequence = await this.browser.genome.getSequence(chr, start, end)
-                    if (sequence) {
-                        const name = `blat: ${chr}:${start + 1}-${end}`
-                        const title = `blat: ${chr}:${start + 1}-${end}`
-                        createBlatTrack({sequence, browser: this.browser, name, title})
-                    }
-                }
-            })
+            this.#addBlatMenuItem(items, feature)
         }
 
+        // Add sort menu items
+        this.#addSortMenuItems(items, feature)
 
+        // ROI driven filter
+
+        if (roiSet.isUserDefined) {
+            this.#addDeleteMenuItem(items, feature, roiSet, roiManager, columnContainer, regionElement)
+        }
+
+        return items
+    }
+
+    #addDeleteMenuItem(items, feature, roiSet, roiManager, columnContainer, regionElement) {
+        items.push(
+            '<hr/>',
+            {
+                label: 'Delete',
+                click: async () => {
+                    roiSet.removeFeature(feature)
+                    const userDefinedFeatures = await roiSet.getAllFeatures()
+
+                    // Delete user defined ROI Set if it is empty
+                    if (Object.keys(userDefinedFeatures).length === 0) {
+                        roiManager.deleteUserDefinedROISet()
+                    }
+                    roiManager.deleteRegionWithKey(regionElement.dataset.region, columnContainer)
+                    roiManager.repaintTable()
+                }
+            }
+        )
+    }
+
+    #addCopySequenceMenuItem(items, feature) {
+        items.push({
+            label: 'Copy reference sequence',
+            click: async () => {
+                this.container.style.display = 'none'
+                let sequence = await this.browser.genome.getSequence(feature.chr, feature.start, feature.end)
+                if (!sequence) {
+                    sequence = "Unknown sequence"
+                }
+                try {
+                    await navigator.clipboard.writeText(sequence)
+                } catch (e) {
+                    console.error(e)
+                    this.browser.alert.present(`error copying sequence to clipboard ${e}`)
+                }
+            }
+        })
+    }
+
+    #addDescriptionMenuItem(items, feature, event) {
+        items.push(
+            {
+                label: 'Set description ...',
+                click: () => {
+                    const callback = () => {
+                        const value = this.browser.inputDialog.value || ''
+                        feature.name = value.trim()
+                        this.browser.roiManager.repaintTable()
+                    }
+                    const config = {
+                        label: 'Description',
+                        value: (feature.name || ''),
+                        callback
+                    }
+
+                    this.browser.inputDialog.present(config, event)
+                }
+            }
+        )
+    }
+
+    #addSortMenuItems(items, feature) {
         const found = this.browser.findTracks(track => typeof track.sortByValue === 'function')
         if (found.length > 0) {
             const { chr, start, end } = feature
@@ -112,39 +137,29 @@ class ROIMenu {
                 {
                     label: 'Sort by value (ascending)',
                     click: () => Promise.all(found.map(track => track.sortByValue({ option: 'VALUE', direction: 'ASC', chr, start, end })))
-                })
-
-            items.push(
+                },
                 '<hr/>',
                 {
                     label: 'Sort by value (descending)',
                     click: () => Promise.all(found.map(track => track.sortByValue({ option: 'VALUE', direction: 'DESC', chr, start, end })))
                 })
-
         }
+    }
 
-
-        if (roiSet.isUserDefined) {
-            items.push(
-                '<hr/>',
-                {
-                    label: 'Delete',
-                    click: async () => {
-                        roiSet.removeFeature(feature)
-                        const userDefinedFeatures = await roiSet.getAllFeatures()
-
-                        // Delete user defined ROI Set if it is empty
-                        if (Object.keys(userDefinedFeatures).length === 0) {
-                            roiManager.deleteUserDefinedROISet()
-                        }
-                        roiManager.deleteRegionWithKey(regionElement.dataset.region, columnContainer)
-                        roiManager.repaintTable()
-                    }
+    #addBlatMenuItem(items, feature) {
+        items.push({
+            label: 'BLAT reference sequence',
+            click: async () => {
+                this.container.style.display = 'none'
+                const {chr, start, end} = feature
+                let sequence = await this.browser.genome.getSequence(chr, start, end)
+                if (sequence) {
+                    const name = `blat: ${chr}:${start + 1}-${end}`
+                    const title = `blat: ${chr}:${start + 1}-${end}`
+                    createBlatTrack({sequence, browser: this.browser, name, title})
                 }
-            )
-        }
-
-        return items
+            }
+        })
     }
 
     dispose() {
