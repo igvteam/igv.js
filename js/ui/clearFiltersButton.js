0382e7267fea26b95ff04fee4267f9cafcef0525
turner <douglass.turner@gmail.com>
2025-05-23 08:36:26 -0400

    Clear filters button

--- js/ui/clearFiltersButton.js
+++ js/ui/clearFiltersButton.js
@@ -25,42 +25,41 @@
  */
 
 import NavbarButton from "./navbarButton.js"
-import {centerlineImage, centerlineImageHover} from "./navbarIcons/centerline.js"
+import {clearFiltersImage, clearFiltersImageHover} from "./navbarIcons/clearFilters.js"
 import { buttonLabel } from "./navbarIcons/buttonLabel.js"
 
-class CenterLineButton extends NavbarButton {
+class ClearFiltersButton extends NavbarButton {
 
     constructor(parent, browser) {
 
-        super(parent, browser, 'Center Line', buttonLabel, centerlineImage, centerlineImageHover, browser.config.showCenterGuide)
+        super(parent, browser, 'Clear Filters', buttonLabel, clearFiltersImage, clearFiltersImageHover, false)
 
         this.button.addEventListener('mouseenter', () => {
-            if (false === browser.doShowCenterLine) {
+            // if (false === browser.doShowClearFilters) {
                 this.setState(true)
-            }
+            // }
         })
 
         this.button.addEventListener('mouseleave', () => {
-            if (false === browser.doShowCenterLine) {
+            // if (false === browser.doShowClearFilters) {
                 this.setState(false)
-            }
+            // }
         })
 
         const mouseClickHandler = () => {
-
-            browser.doShowCenterLine = !browser.doShowCenterLine
-            browser.setCenterLineVisibility(browser.doShowCenterLine)
-            this.setState(browser.doShowCenterLine)
+            // browser.doShowClearFilters = !browser.doShowClearFilters
+            // this.setState(browser.doShowClearFilters)
+            browser.clearFilters()
         }
 
         this.boundMouseClickHandler = mouseClickHandler.bind(this)
 
         this.button.addEventListener('click', this.boundMouseClickHandler)
 
-        this.setVisibility(browser.config.showCenterGuideButton)
+        this.setVisibility(true)
 
     }
 
 }
 
-export default CenterLineButton
+export default ClearFiltersButton
