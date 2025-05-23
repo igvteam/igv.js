282d938219b3b2b6e94961f26851629fdc362606
turner <douglass.turner@gmail.com>
2025-05-23 10:45:54 -0400

    tweaks

--- js/ui/navbarIcons/clearFilters.js
+++ js/ui/navbarIcons/clearFilters.js
@@ -1,28 +1,21 @@
 const clearFiltersImage =
     `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#1f1f1f">
     <title>clear filters</title>
-    <g>
-        <rect fill="none" height="24" width="24"/>
+    <g id="clear-filters">
+        <path d="M0,0h24 M24,24H0" fill="none"/>
+        <path d="M7,6h10l-5.01,6.3L7,6z M4.25,5.61C6.27,8.2,10,13,10,13v6c0,0.55,0.45,1,1,1h2c0.55,0,1-0.45,1-1v-6 c0,0,3.72-4.8,5.74-7.39C20.25,4.95,19.78,4,18.95,4H5.04C4.21,4,3.74,4.95,4.25,5.61z"/>
+        <path d="M0,0h24v24H0V0z" fill="none"/>
     </g>
-    <g>
-        <g id="clear-filters" >
-            <path d="M16.95,6l-3.57,4.55l1.43,1.43c1.03-1.31,4.98-6.37,4.98-6.37C20.3,4.95,19.83,4,19,4H6.83l2,2H16.95z"/><path d="M2.81,2.81L1.39,4.22L10,13v6c0,0.55,0.45,1,1,1h2c0.55,0,1-0.45,1-1v-2.17l5.78,5.78l1.41-1.41L2.81,2.81z"/>
-        </g>
-    </g>
-</svg>
-`
+</svg>`
 
 const clearFiltersImageHover =
     `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#1f1f1f">
-<title>clear filters hover</title>
-<g>
-    <rect fill="none" height="24" width="24"/>
-</g>
-<g>
-    <g id="clear-filters-hover" >
-        <path d="M16.95,6l-3.57,4.55l1.43,1.43c1.03-1.31,4.98-6.37,4.98-6.37C20.3,4.95,19.83,4,19,4H6.83l2,2H16.95z"/><path d="M2.81,2.81L1.39,4.22L10,13v6c0,0.55,0.45,1,1,1h2c0.55,0,1-0.45,1-1v-2.17l5.78,5.78l1.41-1.41L2.81,2.81z"/>
+    <title>clear filters hover</title>
+    <g id="clear-filters-hover">
+        <path d="M0,0h24 M24,24H0" fill="none"/>
+        <path d="M7,6h10l-5.01,6.3L7,6z M4.25,5.61C6.27,8.2,10,13,10,13v6c0,0.55,0.45,1,1,1h2c0.55,0,1-0.45,1-1v-6 c0,0,3.72-4.8,5.74-7.39C20.25,4.95,19.78,4,18.95,4H5.04C4.21,4,3.74,4.95,4.25,5.61z"/>
+        <path d="M0,0h24v24H0V0z" fill="none"/>
     </g>
-</g>
 </svg>`
 
 export { clearFiltersImage, clearFiltersImageHover }
