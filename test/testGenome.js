83087134b990d1718a5ec8b462cd19b1c5de0009
Jim Robinson <933148+jrobinso@users.noreply.github.com>
2025-05-30 00:41:11 -0400

    Update reference (#1999)
    * Update deprecated genome reference urls -- s3 urls will be removed at some point in the future, this update translates them to long-term stable UCSC urls
    
    Former-commit-id: c3cbe30217a44d6a5b4d8c50d193c5237574efd1

--- test/testGenome.js
+++ test/testGenome.js
@@ -1,5 +1,6 @@
 import "./utils/mockObjects.js"
 import Genome from "../js/genome/genome.js"
+import {updateReference} from "../js/genome/updateReference.js"
 import {assert} from 'chai'
 import {shortenChromsomeName} from "../js/rulerTrack.js"
 
@@ -11,10 +12,11 @@ suite("testGenome", function () {
         this.timeout(200000)
 
         const reference = {
-            id: "hg19",
-            fastaURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/1kg_v37/human_g1k_v37_decoy.fasta",
-            indexURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/1kg_v37/human_g1k_v37_decoy.fasta.fai",
-            cytobandURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/b37/b37_cytoband.txt",
+            "id": "b37_1kg",
+            "name": "Human (1kg, b37+decoy)",
+            "fastaURL": "https://storage.googleapis.com/genomics-public-data/references/Homo_sapiens_assembly19_1000genomes_decoy/Homo_sapiens_assembly19_1000genomes_decoy.fasta",
+            "indexURL": "https://storage.googleapis.com/genomics-public-data/references/Homo_sapiens_assembly19_1000genomes_decoy/Homo_sapiens_assembly19_1000genomes_decoy.fasta.fai",
+            "cytobandURL": "https://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/cytoBand.txt.gz",
             wholeGenomeView: true
         }
 
@@ -44,7 +46,6 @@ suite("testGenome", function () {
 
 
     test("Shorten name", function() {
-
         const names = ["chr1", "chromosome_1"]
         const expected = ["1", "chromosome_1"]
 
@@ -52,8 +53,37 @@ suite("testGenome", function () {
             const shortened = shortenChromsomeName(names[i])
             assert.equal(shortened, expected[i])
         }
-
     })
 
+    test("update reference", function() {
+
+        const reference = {
+            "id": "hg18",
+            "name": "Human (hg18)",
+            "fastaURL": "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg18/hg18.fasta",
+            "indexURL": "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg18/hg18.fasta.fai",
+            "cytobandURL": "https://hgdownload.soe.ucsc.edu/goldenPath/hg18/database/cytoBandIdeo.txt.gz",
+            "tracks": [
+                {
+                    "name": "Refseq Genes",
+                    "format": "refgene",
+                    "url": "https://hgdownload.soe.ucsc.edu/goldenPath/hg18/database/refGene.txt.gz",
+                    "indexed": false,
+                    "visibilityWindow": -1,
+                    "order": 1000000,
+                    "searchable": true
+                }
+            ],
+            "chromosomeOrder": "chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY"
+        }
+
+        updateReference(reference)
+        assert.isNotOk(reference.fastaURL)
+        assert.isNotOk(reference.indexURL)
+        assert.isOk(reference.twoBitURL)
+        assert.isOk(reference.chromSizesURL)
+
+
+    })
 
 })
