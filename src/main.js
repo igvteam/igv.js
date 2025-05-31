ecf4e90813efe24169be80a974855497ccf885f1
turner <douglass.turner@gmail.com>
2025-05-31 13:54:48 -0400

    created featureSource

--- src/main.js
+++ src/main.js
@@ -1,16 +1,43 @@
+import igv from '../js/index.js';
 import GenomeUtils from '../js/genome/genomeUtils.js';
 import Genome from '../js/genome/genome.js';
 import search from "../js/search.js"
+import TextFeatureSource from "../js/feature/textFeatureSource.js"
 
 document.addEventListener('DOMContentLoaded', async () => {
 
     await GenomeUtils.initializeGenomes({})
 
     const genomeName = 'hg19'
-
     const genomeConfig = await GenomeUtils.expandReference(genomeName)
-
     const genome = await Genome.createGenome(genomeConfig, this)
+
+    const config =
+        {
+            locus: '19:49301000-49305700',
+            genome: "hg19",
+            showTrackLabels: false,
+            showIdeogram: false,
+            showRuler: true,
+            showSequence: false,
+        };
+
+    const browser = await igv.createBrowser(document.getElementById('dat-gene-render-container'), config, genome)
+    console.log(`browser ${browser.guid} is good to go`);
+
+
+    const featureSourceConfig =
+        {
+            "id": "refseqSelect",
+            "url": "https://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/ncbiRefSeqSelect.txt.gz",
+            "format": "refgene",
+            "html": "https://www.ncbi.nlm.nih.gov/refseq/refseq_select/",
+            "type": "annotation"
+        }
+
+    const featureSource = new TextFeatureSource(featureSourceConfig)
+
+    return
 
     const [{ chr, start, end, name }] = await search({ genome }, 'myc')
 
