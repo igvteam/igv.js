7d1b6912fcf82046df43cdf9b83586337c55dc6b
jrobinso <933148+jrobinso@users.noreply.github.com>
2025-08-08 21:04:44 -0700

    dev html

--- js/sample/sampleUtils.js
+++ js/sample/sampleUtils.js
@@ -1,3 +1,7 @@
+import IGVGraphics from "../igv-canvas.js"
+
+const NULL_GROUP = 'None'
+const GROUP_MARGIN_HEIGHT = 16
 
 function doSortByAttributes(sampleInfo, sampleKeys) {
 
@@ -20,4 +24,34 @@ function doSortByAttributes(sampleInfo, sampleKeys) {
     return true
 }
 
-export { doSortByAttributes }
+/**
+ * Draw horizontal lines to separate groups of samples.
+ * @param context
+ * @param pixelTop - the vertical start position in pixels relative to track top in which to draw the dividers.   For deep tracks that are scrolled, this can be non-zero.
+ * @param pixelWidth - viewport width in pixels
+ * @param pixelHeight - the vertical extent of the track to draw in pixels.
+ * @param offset - an offset in pixels from the top of the track to the top of the first sample.  Normally this is zero, but if there is content above the samples, this will be non-zero.
+ * @param sampleHeight - height of each sample in pixels
+ * @param groups - Map of group name to group object, where group object has a count property
+ */
+function drawGroupDividers(context, pixelTop, pixelWidth, pixelHeight, offset, sampleHeight, groups) {
+
+    if (!groups || groups.size < 2) return
+
+    const pixelBottom = pixelTop + pixelHeight
+    context.save()
+    context.fillStyle = 'black'
+    let y = offset - GROUP_MARGIN_HEIGHT / 2
+    for (const group of groups.values()) {
+        y += group.count * sampleHeight + GROUP_MARGIN_HEIGHT
+        if (y > pixelBottom) {
+            break
+        }
+        if (y > pixelTop) {
+            IGVGraphics.dashedLine(context, 0, y, pixelWidth, y)
+        }
+    }
+    context.restore()
+}
+
+export { doSortByAttributes, drawGroupDividers, NULL_GROUP, GROUP_MARGIN_HEIGHT }
