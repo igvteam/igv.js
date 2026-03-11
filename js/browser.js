86a70445b43c67220dbc58c129e9f4d28654d8cd
turner <douglass.turner@gmail.com>
2026-03-11 19:45:03 -0400

    misc
    Former-commit-id: 6684d5ddc45de1aa3f4d61aa774c4f7e72c6e7c9 [formerly e6eb313b0dcd65ce405e023eb9cd7889e1a9f96d]
    Former-commit-id: 02b6ad6a296b921cb342ef1534152e1e8aa9485f

--- js/browser.js
+++ js/browser.js
@@ -132,8 +132,6 @@ class Browser {
 
                 // sample names
                 this.sampleNameViewportWidth = undefined
-                // this.showSampleNames = false
-                // this.sampleNameControl.setState(this.showSampleNames)
                 this.sampleNameControl.hide()
 
                 this.layoutChange()
